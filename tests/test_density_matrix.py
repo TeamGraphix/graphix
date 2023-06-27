0420e66d957f1b4b87311bf8d36e8dcf28067f4f
Yuki Watanabe <yuki.watanabe@fixstars.com>
2023-06-27 11:39:18 +0900

    add 3-qubit QFT test case

--- tests/test_density_matrix.py
+++ tests/test_density_matrix.py
@@ -1,6 +1,5 @@
 import unittest
 import numpy as np
-from numpy.linalg import norm
 from graphix import Circuit
 from graphix.sim.statevec import Statevec, StatevectorBackend, CNOT_TENSOR, SWAP_TENSOR, CZ_TENSOR
 from graphix.sim.density_matrix import DensityMatrix, DensityMatrixBackend
@@ -44,7 +43,7 @@ class TestDensityMatrix(unittest.TestCase):
     def test_init_with_data_success(self):
         for n in range(3):
             data = np.random.rand(2**n, 2**n) + 1j * np.random.rand(2**n, 2**n)
-            data /= norm(data)
+            data /= np.linalg.norm(data)
             dm = DensityMatrix(data=data)
             assert dm.Nqubit == n
             assert dm.rho.shape == (2**n, 2**n)
@@ -87,10 +86,10 @@ class TestDensityMatrix(unittest.TestCase):
     def test_tensor_with_data_success(self):
         for n in range(3):
             data_a = np.random.rand(2**n, 2**n) + 1j * np.random.rand(2**n, 2**n)
-            data_a /= norm(data_a)
+            data_a /= np.linalg.norm(data_a)
             dm_a = DensityMatrix(data=data_a)
             data_b = np.random.rand(2 ** (n + 1), 2 ** (n + 1)) + 1j * np.random.rand(2 ** (n + 1), 2 ** (n + 1))
-            data_b /= norm(data_b)
+            data_b /= np.linalg.norm(data_b)
             dm_b = DensityMatrix(data=data_b)
             dm_a.tensor(dm_b)
             assert dm_a.Nqubit == 2 * n + 1
@@ -207,7 +206,7 @@ class TestDensityMatrix(unittest.TestCase):
         data = np.random.rand(4, 4) + 1j * np.random.rand(4, 4)
         dm = DensityMatrix(data)
         dm.normalize()
-        assert np.allclose(norm(dm.rho), 1)
+        assert np.allclose(np.linalg.norm(dm.rho), 1)
 
     def test_ptrace_fail(self):
         dm = DensityMatrix(nqubit=0)
@@ -305,20 +304,13 @@ class DensityMatrixBackendTest(unittest.TestCase):
         backend.entangle_nodes((1, 2))
         backend.measure(backend.pattern.seq[-4])
 
-        expected_matrix_1 = np.zeros((4, 4))
-        expected_matrix_1[0, 0] = 0.5
-        expected_matrix_1[0, 1] = 0.5
-        expected_matrix_1[1, 0] = 0.5
-        expected_matrix_1[1, 1] = 0.5
-        expected_matrix_2 = np.zeros((4, 4))
-        expected_matrix_2[2, 2] = 0.5
-        expected_matrix_2[2, 3] = -0.5
-        expected_matrix_2[3, 2] = -0.5
-        expected_matrix_2[3, 3] = 0.5
-
+        expected_matrix_1 = np.kron(np.array([[1, 0], [0, 0]]), np.ones((2, 2)) / 2)
+        expected_matrix_2 = np.kron(np.array([[0, 0], [0, 1]]), np.array([[0.5, -0.5], [-0.5, 0.5]]))
         assert np.allclose(backend.state.rho, expected_matrix_1) or np.allclose(backend.state.rho, expected_matrix_2)
 
     def test_correct_byproduct(self):
+        np.random.seed(0)
+
         circ = Circuit(1)
         circ.rx(0, np.pi / 2)
         pattern = circ.transpile()
@@ -348,40 +340,47 @@ class DensityMatrixBackendTest(unittest.TestCase):
         np.testing.assert_allclose(rho, np.outer(psi, psi.conj()))
 
     def test_dephase(self):
-        def simulate(p, pattern):
-            backend = DensityMatrixBackend(pattern)
-            backend.add_nodes([0])
-            backend.dephase(p)
-            backend.add_nodes([1])
-            backend.dephase(p)
-            backend.add_nodes([2])
-            backend.dephase(p)
-            backend.entangle_nodes((0, 1))
-            backend.dephase(p)
-            backend.entangle_nodes((1, 2))
-            backend.dephase(p)
-            backend.measure(pattern.seq[-4])
-            backend.dephase(p)
-            backend.measure(pattern.seq[-3])
-            backend.dephase(p)
-            backend.correct_byproduct(pattern.seq[-2])
-            backend.dephase(p)
-            backend.correct_byproduct(pattern.seq[-1])
-            backend.dephase(p)
-            backend.finalize()
+        def run(p, pattern, max_qubit_num=12):
+            backend = DensityMatrixBackend(pattern, max_qubit_num=max_qubit_num)
+            for cmd in pattern.seq:
+                if cmd[0] == "N":
+                    backend.add_nodes([cmd[1]])
+                elif cmd[0] == "E":
+                    backend.entangle_nodes(cmd[1])
+                    backend.dephase(p)
+                elif cmd[0] == "M":
+                    backend.measure(cmd)
+                    backend.dephase(p)
+                elif cmd[0] == "X":
+                    backend.correct_byproduct(cmd)
+                    backend.dephase(p)
+                elif cmd[0] == "Z":
+                    backend.correct_byproduct(cmd)
+                    backend.dephase(p)
+                elif cmd[0] == "C":
+                    backend.apply_clifford(cmd)
+                    backend.dephase(p)
+                elif cmd[0] == "T":
+                    backend.dephase(p)
+                else:
+                    raise ValueError("invalid commands")
+                if pattern.seq[-1] == cmd:
+                    backend.finalize()
             return backend
 
+        # Test for Rx(pi/4)
         circ = Circuit(1)
         circ.rx(0, np.pi / 4)
         pattern = circ.transpile()
-        backend1 = simulate(0, pattern)
-        backend2 = simulate(1, pattern)
+        backend1 = run(0, pattern)
+        backend2 = run(1, pattern)
         np.testing.assert_allclose(backend1.state.rho, backend2.state.rho)
 
+        # Test for Rz(pi/3)
         circ = Circuit(1)
         circ.rz(0, np.pi / 3)
         pattern = circ.transpile()
-        dm_backend = simulate(1, pattern)
+        dm_backend = run(1, pattern)
         sv_backend = StatevectorBackend(pattern)
         sv_backend.add_nodes([0, 1, 2])
         sv_backend.entangle_nodes((0, 1))
@@ -393,5 +392,69 @@ class DensityMatrixBackendTest(unittest.TestCase):
         sv_backend.finalize()
         np.testing.assert_allclose(dm_backend.state.fidelity(sv_backend.state.psi), 0.25)
 
+        # Test for 3-qubit QFT
+        def cp(circuit, theta, control, target):
+            """Controlled rotation gate, decomposed"""
+            circuit.rz(control, theta / 2)
+            circuit.rz(target, theta / 2)
+            circuit.cnot(control, target)
+            circuit.rz(target, -1 * theta / 2)
+            circuit.cnot(control, target)
 
-unittest.main(argv=[""], verbosity=1, exit=False)
+        def swap(circuit, a, b):
+            """swap gate, decomposed"""
+            circuit.cnot(a, b)
+            circuit.cnot(b, a)
+            circuit.cnot(a, b)
+
+        def qft_circ():
+            circ = Circuit(3)
+            for i in range(3):
+                circ.h(i)
+            circ.x(1)
+            circ.x(2)
+
+            circ.h(2)
+            cp(circ, np.pi / 4, 0, 2)
+            cp(circ, np.pi / 2, 1, 2)
+            circ.h(1)
+            cp(circ, np.pi / 2, 0, 1)
+            circ.h(0)
+            swap(circ, 0, 2)
+            return circ
+
+        # no-noise case
+        circ = qft_circ()
+        pattern = circ.transpile()
+        dm_backend = run(0, pattern)
+        state = circ.simulate_statevector().flatten()
+        np.testing.assert_allclose(dm_backend.state.fidelity(state), 1)
+
+        # noisy case vs exact 3-qubit QFT result
+        circ = qft_circ()
+        pattern = circ.transpile()
+        p = np.random.rand() * 0 + 0.8
+        dm_backend = run(p, pattern)
+        noisy_state = circ.simulate_statevector().flatten()
+
+        sv = Statevec(nqubit=3)
+        omega = np.exp(2j * np.pi / 8)
+        qft_matrix = np.array(
+            [
+                [1, 1, 1, 1, 1, 1, 1, 1],
+                [1, omega, omega**2, omega**3, omega**4, omega**5, omega**6, omega**7],
+                [1, omega**2, omega**4, omega**6, 1, omega**2, omega**4, omega**6],
+                [1, omega**3, omega**6, omega, omega**4, omega**7, omega**2, omega**5],
+                [1, omega**4, 1, omega**4, 1, omega**4, 1, omega**4],
+                [1, omega**5, omega**2, omega**7, omega**4, omega, omega**6, omega**3],
+                [1, omega**6, omega**4, omega**2, 1, omega**6, omega**4, omega**2],
+                [1, omega**7, omega**6, omega**5, omega**4, omega**3, omega**2, omega],
+            ]
+        ) / np.sqrt(8)
+        exact_qft_state = qft_matrix @ sv.psi.flatten()
+        np.testing.assert_allclose(dm_backend.state.fidelity(noisy_state), dm_backend.state.fidelity(exact_qft_state))
+
+
+if __name__ == "__main__":
+    np.random.seed(2)
+    unittest.main()
