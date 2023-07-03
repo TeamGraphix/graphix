38dd24308db0b6bae79a0cea9d4cb76aef0966cc
Yuki Watanabe <yuki.watanabe@fixstars.com>
2023-07-03 17:59:15 +0900

    modify to_kraus

--- tests/test_kraus.py
+++ tests/test_kraus.py
@@ -5,6 +5,9 @@ from graphix.kraus import to_kraus, _is_matrix
 
 class TestKraus(unittest.TestCase):
     def test_to_kraus_fail(self):
+        A_wrong = [[0, 1, 2], [3, 4, 5]]
+        A = [[0, 1], [2, 3]]
+
         # data type is invalid
         with self.assertRaises(TypeError):
             to_kraus(1)
@@ -14,73 +17,78 @@ class TestKraus(unittest.TestCase):
             to_kraus({})
 
         # (i) single unitary matrix A
-        A = [[0, 1, 2], [3, 4, 5]]
         with self.assertRaises(ValueError):
-            to_kraus(np.asarray(A, dtype=complex))
+            to_kraus([np.asarray(A_wrong, dtype=complex), 1])
         with self.assertRaises(ValueError):
-            to_kraus(A)
+            to_kraus([A_wrong, 1])
         with self.assertRaises(ValueError):
-            to_kraus([])
+            to_kraus([A, 1j])
 
         # (ii) single Kraus set
         with self.assertRaises(ValueError):
-            to_kraus([[np.asarray(A, dtype=complex)]])
+            to_kraus([[np.asarray(A_wrong, dtype=complex)]])
         with self.assertRaises(ValueError):
-            to_kraus([[A]])
+            to_kraus([[A_wrong]])
         with self.assertRaises(ValueError):
             to_kraus([[]])
+        with self.assertRaises(ValueError):
+            to_kraus([A, A])
+        with self.assertRaises(ValueError):
+            to_kraus([[A, A], [A, A]])
+        with self.assertRaises(AssertionError):
+            to_kraus([[A, 1], [A, A]])
 
         # (iii) generalized Kraus set
         with self.assertRaises(ValueError):
-            to_kraus([[np.asarray(A, dtype=complex)], [np.asarray(A, dtype=complex)]])
+            to_kraus([[], 1])
         with self.assertRaises(ValueError):
-            to_kraus([[A], [A]])
+            to_kraus([[[A, 1]], [1]])
         with self.assertRaises(ValueError):
-            to_kraus([[A], []])
+            to_kraus(np.array([[[A, 1]], [1]]))
         with self.assertRaises(ValueError):
-            to_kraus([[A], [[]]])
+            to_kraus(np.array([[[A, 1]], [[A, A]]]))
         with self.assertRaises(ValueError):
-            to_kraus([[], [A]])
-        with self.assertRaises(ValueError):
-            to_kraus([[[]], [A]])
-        with self.assertRaises(ValueError):
-            to_kraus([[], []])
-        with self.assertRaises(ValueError):
-            to_kraus([[A], [A, A]])
+            to_kraus(np.array([[[A, A]], [[A, A]]]))
 
     def test_to_kraus_success(self):
         # (i) single unitary matrix A
         A = [[0, 1], [2, 3]]
-        kraus = to_kraus(A)
-        np.testing.assert_array_equal(kraus[0][0], np.asarray(A, dtype=complex))
+        kraus = to_kraus((A, 1))
+        np.testing.assert_array_equal(kraus[0][0].data, np.asarray(A, dtype=complex))
+        self.assertEqual(kraus[0][0].qarg, 1)
         self.assertEqual(kraus[1], None)
 
-        kraus = to_kraus(np.asarray(A, dtype=complex))
-        np.testing.assert_array_equal(kraus[0][0], np.asarray(A, dtype=complex))
+        kraus = to_kraus((np.asarray(A, dtype=complex), 1))
+        np.testing.assert_array_equal(kraus[0][0].data, np.asarray(A, dtype=complex))
+        self.assertEqual(kraus[0][0].qarg, 1)
         self.assertEqual(kraus[1], None)
 
         # (ii) single Kraus set
         B = [[4, 5], [6, 7]]
-        kraus = to_kraus([A, B])
-        np.testing.assert_array_equal(kraus[0][0], np.asarray(A, dtype=complex))
-        np.testing.assert_array_equal(kraus[0][1], np.asarray(B, dtype=complex))
+        kraus = to_kraus([(A, 1), (B, 2)])
+        np.testing.assert_array_equal(kraus[0][0].data, np.asarray(A, dtype=complex))
+        np.testing.assert_array_equal(kraus[0][1].data, np.asarray(B, dtype=complex))
+        self.assertEqual(kraus[0][0].qarg, 1)
+        self.assertEqual(kraus[0][1].qarg, 2)
         self.assertEqual(kraus[1], None)
 
-        kraus = to_kraus([np.asarray(A, dtype=complex), np.asarray(B, dtype=complex)])
-        np.testing.assert_array_equal(kraus[0][0], np.asarray(A, dtype=complex))
-        np.testing.assert_array_equal(kraus[0][1], np.asarray(B, dtype=complex))
+        kraus = to_kraus([(np.asarray(A, dtype=complex), 1), (np.asarray(B, dtype=complex), 2)])
+        np.testing.assert_array_equal(kraus[0][0].data, np.asarray(A, dtype=complex))
+        np.testing.assert_array_equal(kraus[0][1].data, np.asarray(B, dtype=complex))
+        self.assertEqual(kraus[0][0].qarg, 1)
+        self.assertEqual(kraus[0][1].qarg, 2)
         self.assertEqual(kraus[1], None)
 
         # (iii) generalized Kraus set
         C = [[8, 9], [10, 11]]
         D = [[12, 13], [14, 15]]
-        kraus = to_kraus([[A, B], [C, D]])
-        np.testing.assert_array_equal(kraus[0][0], np.asarray(A, dtype=complex))
-        np.testing.assert_array_equal(kraus[0][1], np.asarray(B, dtype=complex))
-        np.testing.assert_array_equal(kraus[1][0], np.asarray(C, dtype=complex))
-        np.testing.assert_array_equal(kraus[1][1], np.asarray(D, dtype=complex))
+        kraus = to_kraus([[(A, 1), (B, 2)], [(C, 3), (D, 4)]])
+        np.testing.assert_array_equal(kraus[0][0].data, np.asarray(A, dtype=complex))
+        np.testing.assert_array_equal(kraus[0][1].data, np.asarray(B, dtype=complex))
+        np.testing.assert_array_equal(kraus[1][0].data, np.asarray(C, dtype=complex))
+        np.testing.assert_array_equal(kraus[1][1].data, np.asarray(D, dtype=complex))
 
-    def test__is_matrix_fial(self):
+    def test__is_matrix_fail(self):
         np.testing.assert_equal(_is_matrix(1), False)
         np.testing.assert_equal(_is_matrix("hello"), False)
         np.testing.assert_equal(_is_matrix([]), False)
