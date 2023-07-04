cadc74933a96d7433dbcadcbeb2f9cb033c0a712
Yuki Watanabe <yuki.watanabe@fixstars.com>
2023-07-04 12:15:36 +0900

    add generate_depahsing_kraus

--- tests/test_kraus.py
+++ tests/test_kraus.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from graphix.kraus import to_kraus, _is_kraus_op
+from graphix.kraus import to_kraus, generate_dephasing_kraus, _is_kraus_op
 
 
 class TestKraus(unittest.TestCase):
@@ -62,6 +62,23 @@ class TestKraus(unittest.TestCase):
         np.testing.assert_array_equal(kraus[1].data, np.asarray(B, dtype=complex))
         self.assertEqual(kraus[0].qarg, 1)
         self.assertEqual(kraus[1].qarg, 2)
+
+    def test_generate_dephasing_kraus_fail(self):
+        with self.assertRaises(AssertionError):
+            generate_dephasing_kraus(2, 1)
+        with self.assertRaises(AssertionError):
+            generate_dephasing_kraus(0.5, "1")
+
+    def test_generate_dephasing_kraus_success(self):
+        p = 0.5
+        qarg = 1
+        dephase = generate_dephasing_kraus(p, qarg)
+        print(dephase)
+        self.assertEqual(len(dephase), 20)
+        np.testing.assert_array_equal(dephase[0].data, np.asarray(np.sqrt(1 - p) * np.eye(2), dtype=complex))
+        np.testing.assert_array_equal(dephase[1].data, np.asarray(np.sqrt(p) * np.diag([1, -1]), dtype=complex))
+        self.assertEqual(dephase[0].qarg, qarg)
+        self.assertEqual(dephase[1].qarg, qarg)
 
     def test__is_kraus_op_fail(self):
         np.testing.assert_equal(_is_kraus_op(1), False)
