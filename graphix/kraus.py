82c9e5d1e36fcc7ef3e23bbc0e4622235d29f66d
Yuki Watanabe <yuki.watanabe@fixstars.com>
2023-07-04 11:52:36 +0900

    modify return value shape of to_kraus

--- graphix/kraus.py
+++ graphix/kraus.py
@@ -9,26 +9,24 @@ class KrausOp:
 
 
 def to_kraus(data):
-    r"""Convert input data into Kraus operator set.
+    r"""Convert input data into Kraus operator set [KrausOp, KrausOp, ...].
+    Each KrausOp has unitary matrix and target qubit index info.
 
     Parameters
     ----------
         data : array_like
             Data to convert into Kraus operator set.
-            Input data must be either (i)single Operator, (ii)single Kraus set or (iii)generalized Kraus set.
+            Input data must be either (i)single Operator or (ii)Kraus set.
             Relation among quantum channel, input data and returned Kraus operator set is as follwos:
                 (i) quantum channel: :math:`E(\rho) = A \rho A^\dagger`
                     input data: [A (2d-array-like), qarg(int)]
-                    returns: ([KrausOp], None)
+                    returns: [KrausOp]
                 (ii) quantum channel: :math:`E(\rho) = \sum_i A_i \rho A_i^\dagger`
                     input data: [(A_1, int), (A_2, int), ...]
-                    returns: ([KrausOp, KrausOp, ...], None)
-                (iii) quantum channel: :math:`E(\rho) = \sum_i A_i \rho B_i^\dagger`
-                    input data: [[(A_1, int), (A_2, int), ...], [(B_1, int), (B_2, int), ...]]
-                    returns: ([KrausOp, KrausOp, ...], [KrausOp, KrausOp, ...])
+                    returns: [KrausOp, KrausOp, ...]
     Returns
     -------
-        kraus : tuple. ([KrausOp, ...], Optional[[KrausOp, ...]])
+        kraus : list. [KrausOp, ...]
             KrausOp set.
     """
     if isinstance(data, (list, tuple, np.ndarray)):
@@ -41,7 +39,7 @@ def to_kraus(data):
         # E(rho) = A * rho * A^\dagger
         # and the second data is target qubit index.
         if _is_kraus_op(data):
-            return ([KrausOp(data[0], data[1])], None)
+            return [KrausOp(data[0], data[1])]
 
         # (ii) If input is list of [2d-array-likes, int], it is a single Kraus set for channel:
         # E(rho) = \sum_i A_i * rho * A_i^\dagger
@@ -50,57 +48,16 @@ def to_kraus(data):
             if isinstance(data, np.ndarray):
                 data = data.tolist()
             kraus = [KrausOp(data[0][0], data[0][1])]
-            shape = kraus[0].data.shape
             for A_i in data[1:]:
                 A_i = KrausOp(A_i[0], A_i[1])
-                if A_i.data.shape != shape:
+                if _is_kraus_op(A_i):
                     raise ValueError("All Kraus operators must have same shape.")
                 kraus.append(A_i)
-            return (kraus, None)
-
-        # (iii) If input is like [[[2d-array-likes, int], ...], [[2d-array-likes, int], ...]],
-        # it is a generalized Kraus set for channel:
-        # E(rho) = \sum_i A_i * rho * B_i^\dagger
-        # with target qubit indices.
-        elif isinstance(data, (list, tuple, np.ndarray)) and len(data) == 2:
-            if isinstance(data, np.ndarray):
-                data = data.tolist()
-            if len(data[0]) == 0:
-                raise ValueError("Input data must be non-empty.")
-            if not _is_kraus_op(data[0][0]):
-                raise ValueError(
-                    "Input data must be either (i)single Operator (2d-array-like), "
-                    "(ii)single Kraus set (list of 2d-array-likes) or "
-                    "(iii)generalized Kraus set (list of lists consisting of same number of 2d-array-likes)."
-                    " with qubit indices for each Operator."
-                )
-            kraus_left = [KrausOp(data[0][0][0], data[0][0][1])]
-            for A_i in data[0][1:]:
-                if not _is_kraus_op(A_i):
-                    raise ValueError(
-                        "Input data must be either (i)single Operator (2d-array-like), "
-                        "(ii)single Kraus set (list of 2d-array-likes) or "
-                        "(iii)generalized Kraus set (list of lists consisting of same number of 2d-array-likes)."
-                        " with qubit indices for each Operator."
-                    )
-                kraus_left.append(KrausOp(A_i[0], A_i[1]))
-            kraus_right = []
-            for B_i in data[1]:
-                if not _is_kraus_op(B_i):
-                    raise ValueError(
-                        "Input data must be either (i)single Operator (2d-array-like), "
-                        "(ii)single Kraus set (list of 2d-array-likes) or "
-                        "(iii)generalized Kraus set (list of lists consisting of same number of 2d-array-likes)."
-                        " with qubit indices for each Operator."
-                    )
-                kraus_right.append(KrausOp(B_i[0], B_i[1]))
-            return (kraus_left, kraus_right)
-
+            return kraus
         else:
             raise ValueError(
-                "Input data must be either (i)single Operator (2d-array-like), "
-                "(ii)single Kraus set (list of 2d-array-likes) or "
-                "(iii)generalized Kraus set (list of lists consisting of same number of 2d-array-likes)"
+                "Input data must be either (i)single Operator (2d-array-like)"
+                " or (ii)single Kraus set (list of 2d-array-likes)"
                 " with qubit indices for each Operator."
             )
     else:
@@ -123,7 +80,10 @@ def generate_amplitude_damping_kraus(p, nqubits):
 
 
 def _is_kraus_op(data):
-    """Check if data is a Kraus operator."""
+    """Check if data is a Kraus operator.
+    Currently, Kraus operator is defined as a list of [2d-array-like, int].
+    This might be changed in the future to support Kraus operator of the form [2^k dim array-like, int] (k <= n).
+    """
     if not isinstance(data, (list, tuple, np.ndarray)):
         return False
     if len(data) != 2:
