e4c8270caf16dbc463ec06c1f03547ee3e705264
mgarnier59 <50111289+mgarnier59@users.noreply.github.com>
2023-07-05 13:59:41 +0200

    NoiseModel via abc

--- graphix/noise_models/noise_model.py
+++ graphix/noise_models/noise_model.py
@@ -1,43 +1,56 @@
 import numpy as np
 from graphix.sim.density_matrix import DensityMatrix
-from graphix.noise_models.noise_model import NoiseModel
+import abc
 
 
-class BaseNoiseModel(NoiseModel):
+class NoiseModel(abc.ABC):
     """No noise"""
 
+    def __init__(self):
+        pass
+    
+    # NOTE is this useful?
+    def assign_simulator(self, simulator):
+        self.simulator = simulator
 
+    @abc.abstractmethod
     def prepare_qubit(self, cmd):
-        """return qubit to be added.
-        in this base model, we return clean qubit in |+> state"""
-        return DensityMatrix(nqubit=1)
-
+        """return qubit to be added with preparation errors.
+        """
+        pass
+    
+    @abc.abstractmethod
     def entangle(self, cmd):
         """apply noise to qubits that happens in the CZ gate process"""
         pass
 
+    @abc.abstractmethod
     def measure(self, cmd):
         """apply noise to qubits that happens in the measurement process"""
         pass
 
+    @abc.abstractmethod
     def confuse_result(self, cmd):
         """assign wrong measurement result"""
-        p = 0.0
-        if np.random.rand() < p:
-            self.simulator.result[cmd[1]] = 1 - self.simulator.result[cmd[1]]
+        pass
 
+    @abc.abstractmethod
     def byproduct_x(self, cmd):
         """apply noise to qubits that happens in the X gate process"""
         pass
 
+    @abc.abstractmethod
     def byproduct_z(self, cmd):
         """apply noise to qubits that happens in the Z gate process"""
         pass
 
+    @abc.abstractmethod
     def clifford(self, cmd):
         """apply noise to qubits that happens in the Clifford gate process"""
+        # NOTE might be different depending on the gate.
         pass
 
+    @abc.abstractmethod
     def tick_clock(self):
         """notion of time in real devices - this is where we apply effect of T1 and T2.
         we assume commands that lie between 'T' commands run simultaneously on the device.
