04e58dc00646e4a67a596876a30c775f4558fe8f
d1ssk <daichi.sasaki.00@gmail.com>
2023-09-04 22:48:16 +0900

    organize methods

--- graphix/visualization.py
+++ graphix/visualization.py
@@ -94,19 +94,9 @@ class GraphVisualizer:
             Filename of the saved plot.
         """
         if figsize is None:
-            width = (max(l_k.values()) + 1) * 0.8
-            height = len(self.v_in)
-            figsize = (width, height)
+            figsize = self.get_figsize(l_k)
         plt.figure(figsize=figsize)
-        pos = nx.spring_layout(self.G)  # Initial layout for the nodes
-
-        n = len(self.v_in)
-        for i in range(n):
-            k = self.v_in[i]
-            pos[k][1] = i
-            while k in f.keys():
-                k = f[k]
-                pos[k][1] = i
+        pos = self.get_pos_from_flow(f, l_k)
 
         # Change the x coordinates of the nodes based on their layer, sort in descending order
         for node, layer in l_k.items():
@@ -117,26 +107,15 @@ class GraphVisualizer:
             nx.draw_networkx_edges(self.G, pos, edgelist=[(a, b)], edge_color="black", arrowstyle="->", arrows=True)
 
         # Draw the dashed edges
-        for edge in self.G.edges():
+        edge_path = self.get_edge_path(f, pos)
+        for edge in edge_path.keys():
             if f.get(edge[0]) != edge[1] and f.get(edge[1]) != edge[0]:  # This edge is not an arrow
-                intersect = False
-                bezier_path = [pos[edge[0]]]
-                for node in self.G.nodes():
-                    if (
-                        node != edge[0]
-                        and node != edge[1]
-                        and self.edge_intersects_node(pos[edge[0]], pos[edge[1]], pos[node])
-                    ):
-                        intersect = True
-                        ctrl_point = self.control_point(pos[edge[0]], pos[edge[1]], pos[node])
-                        bezier_path.append(ctrl_point)
-                if not intersect:
+                if len(edge_path[edge]) == 2:
                     nx.draw_networkx_edges(self.G, pos, edgelist=[edge], style="dashed", alpha=0.7)
-                    continue
-                bezier_path.append(pos[edge[1]])
-                t = np.linspace(0, 1, 100)
-                curve = self.bezier_curve(bezier_path, t)
-                plt.plot(curve[:, 0], curve[:, 1], "k--", linewidth=1, alpha=0.7)
+                else:
+                    t = np.linspace(0, 1, 100)
+                    curve = self.bezier_curve(edge_path[edge], t)
+                    plt.plot(curve[:, 0], curve[:, 1], "k--", linewidth=1, alpha=0.7)
 
         # Draw the nodes with different colors based on their role (input, output, or other)
         for node in self.G.nodes():
@@ -208,62 +187,40 @@ class GraphVisualizer:
             Filename of the saved plot.
         """
 
-        pos = nx.spring_layout(self.G)  # Initial layout for the nodes
-
-        g_edges = []
-
-        for node, node_list in g.items():
-            for n in node_list:
-                g_edges.append((node, n))
-
-        G_prime = self.G.copy()
-        G_prime.add_nodes_from(self.G.nodes())
-        G_prime.add_edges_from(g_edges)
-
-        l_max = max(l_k.values())
-        l_reverse = {v: l_max - l for v, l in l_k.items()}
-
-        nx.set_node_attributes(G_prime, l_reverse, "subset")
-
-        pos = nx.multipartite_layout(G_prime)
-        ys = [value[1] for value in pos.values()]
-        yrange = max(ys) - min(ys)
-
         if figsize is None:
-            width = (max(l_k.values()) + 1) * 0.8
-            height = yrange + 1
-            figsize = (width, height)
-
+            figsize = self.get_figsize(l_k)
         plt.figure(figsize=figsize)
 
-        # Draw the arrows
-        for a, bs in g.items():
-            for b in bs:
-                nx.draw_networkx_edges(self.G, pos, edgelist=[(a, b)], edge_color="black", arrowstyle="->", arrows=True)
+        pos = self.get_pos_from_gflow(g, l_k)
 
-        # Draw the dashed edges
-        for edge in self.G.edges():
-            if edge[1] not in g.get(edge[0], set()) and edge[0] not in g.get(
-                edge[1], set()
-            ):  # This edge is not an arrow
-                intersect = False
-                bezier_path = [pos[edge[0]]]
-                for node in self.G.nodes():
-                    if (
-                        node != edge[0]
-                        and node != edge[1]
-                        and self.edge_intersects_node(pos[edge[0]], pos[edge[1]], pos[node])
-                    ):
-                        intersect = True
-                        ctrl_point = self.control_point(pos[edge[0]], pos[edge[1]], pos[node])
-                        bezier_path.append(ctrl_point)
-                if not intersect:
+        edge_path = self.get_edge_path(g, pos)
+
+        for edge in edge_path.keys():
+            if edge[1] in g.get(edge[0], set()) or edge[0] in g.get(edge[1], set()):  # This edge is an arrow
+                if len(edge_path[edge]) == 2:
+                    nx.draw_networkx_edges(
+                        self.G, pos, edgelist=[edge], edge_color="black", arrowstyle="->", arrows=True
+                    )
+                else:
+                    t = np.linspace(0, 1, 100)
+                    curve = self.bezier_curve(edge_path[edge], t)
+                    start_point = curve[0]
+                    end_point = curve[-1]
+
+                    plt.scatter(curve[:, 0], curve[:, 1], c="k")  # To see the points
+                    plt.annotate(
+                        "",
+                        xy=end_point,
+                        xytext=start_point,
+                        arrowprops=dict(arrowstyle="->", color="k", lw=1, alpha=0.7),
+                    )
+            else:
+                if len(edge_path[edge]) == 2:
                     nx.draw_networkx_edges(self.G, pos, edgelist=[edge], style="dashed", alpha=0.7)
-                    continue
-                bezier_path.append(pos[edge[1]])
-                t = np.linspace(0, 1, 100)
-                curve = self.bezier_curve(bezier_path, t)
-                plt.plot(curve[:, 0], curve[:, 1], "k--", linewidth=1, alpha=0.7)
+                else:
+                    t = np.linspace(0, 1, 100)
+                    curve = self.bezier_curve(edge_path[edge], t)
+                    plt.plot(curve[:, 0], curve[:, 1], "k--", linewidth=1, alpha=0.7)
 
         # Draw the nodes with different colors based on their role (input, output, or other)
         for node in self.G.nodes():
@@ -306,28 +263,176 @@ class GraphVisualizer:
             plt.savefig(filename)
         plt.show()
 
+    def get_figsize(self, l_k):
+        """
+        Returns the figure size of the graph.
+
+        Parameters
+        ----------
+        l_k : dict
+            Layer mapping.
+
+        Returns
+        -------
+        figsize : tuple
+            figure size of the graph.
+        """
+        width = (max(l_k.values()) + 1) * 0.8
+        height = len(self.v_in)
+        figsize = (width, height)
+        return figsize
+
+    def get_edge_path(self, fg, pos):
+        """
+        Returns the path of edges.
+
+        Parameters
+        ----------
+        fg : dict
+            flow or gflow mapping.
+        pos : dict
+            dictionary of node positions.
+
+        Returns
+        -------
+        edge_path : dict
+            dictionary of edge paths.
+        """
+        max_iter = 5
+        edge_path = {}
+        if type(next(iter(fg.values()))) is set:  # fg is gflow
+            edge_set1 = set(self.G.edges())
+            edge_set2 = {(k, v) for k, values in fg.items() for v in values}
+            edge_set = edge_set1.union(edge_set2)
+        else:
+            edge_set = set(self.G.edges())
+        for edge in edge_set:
+            if fg.get(edge[0], set()) == edge[1] and fg.get(edge[1], set()) == edge[0]:
+                edge_path[edge] = [pos[edge[0]], pos[edge[1]]]
+            else:
+                iteration = 0
+                bezier_path = [pos[edge[0]]]
+                bezier_path.append(pos[edge[1]])
+                while True:
+                    iteration += 1
+                    intersect = False
+                    if iteration > max_iter:
+                        break
+                    ctrl_points = []
+                    for i in range(len(bezier_path) - 1):
+                        start = bezier_path[i]
+                        end = bezier_path[i + 1]
+                        for node in self.G.nodes():
+                            if node != edge[0] and node != edge[1] and self.edge_intersects_node(start, end, pos[node]):
+                                intersect = True
+                                ctrl_points.append(
+                                    [
+                                        i,
+                                        self.control_point(
+                                            bezier_path[0], bezier_path[-1], pos[node], distance=0.6 / iteration
+                                        ),
+                                    ]
+                                )
+                    if not intersect:
+                        break
+                    else:
+                        for i, ctrl_point in enumerate(ctrl_points):
+                            bezier_path.insert(ctrl_point[0] + i + 1, ctrl_point[1])
+
+                edge_path[edge] = self.check_path(bezier_path)
+
+        return edge_path
+
+    def get_pos_from_flow(self, f, l_k):
+        """
+        Returns the position of nodes based on the flow.
+
+        Parameters
+        ----------
+        f : dict
+            flow mapping.
+        l_k : dict
+            Layer mapping.
+
+        Returns
+        -------
+        pos : dict
+            dictionary of node positions.
+        """
+        pos = nx.spring_layout(self.G)  # Initial layout for the nodes
+        n = len(self.v_in)
+        for i in range(n):
+            k = self.v_in[i]
+            pos[k][1] = i
+            while k in f.keys():
+                k = f[k]
+                pos[k][1] = i
+
+        lmax = max(l_k.values())
+        # Change the x coordinates of the nodes based on their layer, sort in descending order
+        for node, layer in l_k.items():
+            pos[node][0] = lmax - layer
+        return pos
+
+    def get_pos_from_gflow(self, g, l_k):
+        """
+        Returns the position of nodes based on the gflow.
+
+        Returns
+        -------
+        pos : dict
+            dictionary of node positions.
+        """
+
+        g_edges = []
+
+        for node, node_list in g.items():
+            for n in node_list:
+                g_edges.append((node, n))
+
+        G_prime = self.G.copy()
+        G_prime.add_nodes_from(self.G.nodes())
+        G_prime.add_edges_from(g_edges)
+
+        l_max = max(l_k.values())
+        l_reverse = {v: l_max - l for v, l in l_k.items()}
+
+        nx.set_node_attributes(G_prime, l_reverse, "subset")
+
+        pos = nx.multipartite_layout(G_prime)
+
+        return pos
+
     @staticmethod
     def edge_intersects_node(start, end, node_pos, buffer=0.2):
         """Determine if an edge intersects a node."""
-        dist = np.linalg.norm(
-            np.cross(np.array(end) - np.array(start), np.array(start) - np.array(node_pos))
-        ) / np.linalg.norm(np.array(end) - np.array(start))
-        return (
-            dist < buffer
-            and min(start[0], end[0]) <= node_pos[0] <= max(start[0], end[0])
-            and min(start[1], end[1]) <= node_pos[1] <= max(start[1], end[1])
-        )
+        start = np.array(start)
+        end = np.array(end)
+        node_pos = np.array(node_pos)
+        # Vector from start to end
+        line_vec = end - start
+        # Vector from start to node_pos
+        point_vec = node_pos - start
+        t = np.dot(point_vec, line_vec) / np.dot(line_vec, line_vec)
+
+        if t < 0.0 or t > 1.0:
+            return False
+        # Find the projection point
+        projection = start + t * line_vec
+        distance = np.linalg.norm(projection - node_pos)
+
+        return distance < buffer
 
     @staticmethod
     def control_point(start, end, node_pos, distance=0.6):
         """Generate a control point to bend the edge around a node."""
         edge_vector = np.array(end) - np.array(start)
         # Rotate the edge vector 90 degrees or -90 degrees according to the node position
-        cross = np.cross(np.array(end) - np.array(start), np.array(start) - np.array(node_pos))
+        cross = np.cross(edge_vector, np.array(node_pos) - np.array(start))
         if cross > 0:
-            dir_vector = np.array([-edge_vector[1], edge_vector[0]])  # Rotate the edge vector 90 degrees
+            dir_vector = np.array([edge_vector[1], -edge_vector[0]])  # Rotate the edge vector 90 degrees
         else:
-            dir_vector = np.array([edge_vector[1], -edge_vector[0]])
+            dir_vector = np.array([-edge_vector[1], edge_vector[0]])
         dir_vector = dir_vector / np.linalg.norm(dir_vector)  # Normalize the vector
         control = node_pos + distance * dir_vector
         return control.tolist()
@@ -339,6 +444,31 @@ class GraphVisualizer:
         for i, point in enumerate(bezier_path):
             curve += np.outer(comb(n, i) * ((1 - t) ** (n - i)) * (t**i), np.array(point))
         return curve
+
+    @staticmethod
+    def check_path(path):
+        """
+        if there is an acute angle in the path, merge points
+        """
+        path = np.array(path)
+        while True:
+            fl = False
+            for i in range(len(path) - 2):
+                v1 = path[i + 1] - path[i]
+                v2 = path[i + 2] - path[i + 1]
+                if np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)) < np.cos(3 * np.pi / 4):
+                    fl = True
+                    if i == len(path) - 3:
+                        path = np.delete(path, i + 1, 0)
+                        break
+                    mean = (path[i + 1] + path[i + 2]) / 2
+                    path = np.delete(path, i + 1, 0)
+                    path = np.delete(path, i + 1, 0)
+                    path = np.insert(path, i + 1, mean, 0)
+                    break
+            if not fl:
+                break
+        return path.tolist()
 
 
 def comb(n, r):
