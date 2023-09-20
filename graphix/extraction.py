a1fa23a274df690c3e102ea9228392bb9813d1ee
king-p3nguin <kazukitsuoka@g.ecc.u-tokyo.ac.jp>
2023-09-20 14:27:25 +0900

    add extraction.py

---
+++ graphix/extraction.py
@@ -0,0 +1,157 @@
+from __future__ import annotations
+from enum import Enum
+
+import graphix
+import numpy as np
+import networkx as nx
+
+
+class ClusterType(Enum):
+    GHZ = "GHZ"
+    LINEAR = "LINEAR"
+    NONE = "99"
+
+    def __str__(self):
+        return self.name
+
+
+class Cluster:
+    def __init__(self, type: ClusterType, graph: graphix.GraphState = None):
+        self.graph = graph
+        self.type = type
+
+    def __str__(self):
+        return str(self.type) + str(self.graph.nodes)
+
+    def __repr__(self):
+        return str(self.type) + str(self.graph.nodes)
+
+    def __eq__(self, other):
+        if not isinstance(other, Cluster):
+            raise TypeError("cannot compare Cluster with non-Cluster object")
+
+        return nx.utils.graphs_equal(self.graph, other.graph) and self.type == other.type
+
+
+def extract_clusters_from_graph(
+    graph: graphix.GraphState,
+    max_ghz: int | float = np.inf,
+    max_lin: int | float = np.inf,
+) -> list[Cluster]:
+    """Extract GHZ clusters and Linear clusters circuit from MBQC pattern.
+
+    Parameters
+    ----------
+    graph : :class:`graphix.GraphState` object
+        Graph state corresponding to the pattern.
+    phasedict : dict
+        Dictionary of phases for each node.
+    max_ghz:
+        Maximum size of ghz clusters
+    max_lin:
+        Maximum size of linear clusters
+    """
+    adjdict = nx.to_dict_of_dicts(graph)
+    number_of_edges = graph.number_of_edges()
+    cluster_list = []
+    neighbors_list = []
+
+    # Prepare a list sorted by number of neighbors to get the largest GHZ clusters first.
+    for v in adjdict.keys():
+        if len(adjdict[v]) > 2:
+            neighbors_list.append((v, len(adjdict[v])))
+
+    # Find GHZ clusters in the graph and remove their edges from the graph.
+    # All nodes that have more than 2 edges become the roots of the GHZ clusters.
+    for v, _ in sorted(neighbors_list, key=lambda tup: tup[1], reverse=True):
+        if len(adjdict[v]) > 2:
+            nodes = [v]
+            while len(adjdict[v]) > 0 and len(nodes) < max_ghz:
+                n, _ = adjdict[v].popitem()
+                nodes.append(n)
+                del adjdict[n][v]
+                number_of_edges -= 1
+            cluster_list.append(create_cluster(nodes, root=v))
+
+    # Find Linear clusters in the remaining graph and remove their edges from the graph.
+    while number_of_edges != 0:
+        for v in adjdict.keys():
+            if len(adjdict[v]) == 1:
+                n = v
+                nodes = [n]
+                while len(adjdict[n]) > 0 and len(nodes) < max_lin:
+                    n2, _ = adjdict[n].popitem()
+                    nodes.append(n2)
+                    del adjdict[n2][n]
+                    number_of_edges -= 1
+                    n = n2
+
+                # We define any cluster whose size is smaller than 4, a GHZ cluster
+                if len(nodes) == 3:
+                    cluster_list.append(create_cluster([nodes[1], nodes[0], nodes[2]], root=nodes[1]))
+                elif len(nodes) == 2:
+                    cluster_list.append(create_cluster(nodes, root=nodes[0]))
+                else:
+                    cluster_list.append(create_cluster(nodes))
+
+        # If a cycle exists in the graph, extract one 3-qubit ghz cluster from the cycle.
+        for v in adjdict.keys():
+            if len(adjdict[v]) == 2:
+                neighbors = list(adjdict[v].keys())
+                nodes = [v] + neighbors
+                del adjdict[neighbors[0]][v]
+                del adjdict[neighbors[1]][v]
+                del adjdict[v][neighbors[0]]
+                del adjdict[v][neighbors[1]]
+                number_of_edges -= 2
+
+                cluster_list.append(create_cluster(nodes, root=v))
+                break
+    return cluster_list
+
+
+def create_cluster(node_ids: list[int], root: int | None = None) -> Cluster:
+    """Create a cluster from node ids.
+
+    Parameters
+    ----------
+    node_ids : list
+        List of node ids.
+    root : int
+        Root of the ghz cluster. If None, it's a linear cluster.
+    """
+    cluster_type = None
+    edges = []
+    if root is not None:
+        edges = [(root, i) for i in node_ids if i != root]
+        cluster_type = ClusterType.GHZ
+    else:
+        edges = [(node_ids[i], node_ids[i + 1]) for i in range(len(node_ids)) if i + 1 < len(node_ids)]
+        cluster_type = ClusterType.LINEAR
+    tmp_graph = graphix.GraphState()
+    tmp_graph.add_nodes_from(node_ids)
+    tmp_graph.add_edges_from(edges)
+    return Cluster(type=cluster_type, graph=tmp_graph)
+
+
+def get_fusion_nodes(c1: Cluster, c2: Cluster):
+    """Get the nodes that are fused between two clusters.
+
+    Parameters
+    ----------
+    c1 : :class:`Cluster` object
+        First cluster.
+    c2 : :class:`Cluster` object
+        Second cluster.
+
+    Returns
+    -------
+    list
+        List of nodes that are fused between the two clusters.
+    """
+    if not isinstance(c1, Cluster) or not isinstance(c2, Cluster):
+        raise TypeError("c1 and c2 must be Cluster objects")
+
+    if c1 == c2:
+        return []
+    return [n for n in c1.graph.nodes if n in c2.graph.nodes]
---
+++ tests/test_extraction.py
@@ -0,0 +1,98 @@
+import unittest
+
+import numpy as np
+import networkx as nx
+import graphix
+from graphix_perceval import extraction
+
+
+class TestExtraction(unittest.TestCase):
+    def test_cluster_extraction_one_ghz_cluster(self):
+        gs = graphix.GraphState()
+        nodes = [0, 1, 2, 3, 4]
+        edges = [(0, 1), (0, 2), (0, 3), (0, 4)]
+        gs.add_nodes_from(nodes)
+        gs.add_edges_from(edges)
+        clusters = extraction.extract_clusters_from_graph(gs)
+
+        self.assertEqual(len(clusters), 1)
+        self.assertEqual(clusters[0] == extraction.Cluster(type=extraction.ClusterType.GHZ, graph=gs), True)
+
+    # we consider everything smaller than 4, a GHZ
+    def test_cluster_extraction_small_ghz_cluster_1(self):
+        gs = graphix.GraphState()
+        nodes = [0, 1, 2]
+        edges = [(0, 1), (1, 2)]
+        gs.add_nodes_from(nodes)
+        gs.add_edges_from(edges)
+        clusters = extraction.extract_clusters_from_graph(gs)
+
+        self.assertEqual(len(clusters), 1)
+        self.assertEqual(clusters[0] == extraction.Cluster(type=extraction.ClusterType.GHZ, graph=gs), True)
+
+    # we consider everything smaller than 4, a GHZ
+    def test_cluster_extraction_small_ghz_cluster_2(self):
+        gs = graphix.GraphState()
+        nodes = [0, 1]
+        edges = [(0, 1)]
+        gs.add_nodes_from(nodes)
+        gs.add_edges_from(edges)
+        clusters = extraction.extract_clusters_from_graph(gs)
+
+        self.assertEqual(len(clusters), 1)
+        self.assertEqual(clusters[0] == extraction.Cluster(type=extraction.ClusterType.GHZ, graph=gs), True)
+
+    def test_cluster_extraction_one_linear_cluster(self):
+        gs = graphix.GraphState()
+        nodes = [0, 1, 2, 3, 4, 5, 6]
+        edges = [(0, 1), (1, 2), (2, 3), (5, 4), (4, 6), (6, 0)]
+        gs.add_nodes_from(nodes)
+        gs.add_edges_from(edges)
+        clusters = extraction.extract_clusters_from_graph(gs)
+
+        self.assertEqual(len(clusters), 1)
+        self.assertEqual(clusters[0] == extraction.Cluster(type=extraction.ClusterType.LINEAR, graph=gs), True)
+
+    def test_cluster_extraction_one_ghz_one_linear(self):
+        gs = graphix.GraphState()
+        nodes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        edges = [(0, 1), (0, 2), (0, 3), (0, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9)]
+        gs.add_nodes_from(nodes)
+        gs.add_edges_from(edges)
+        clusters = extraction.extract_clusters_from_graph(gs)
+        self.assertEqual(len(clusters), 2)
+
+        clusters_expected = []
+        lin_cluster = graphix.GraphState()
+        lin_cluster.add_nodes_from([4, 5, 6, 7, 8, 9])
+        lin_cluster.add_edges_from([(4, 5), (5, 6), (6, 7), (7, 8), (8, 9)])
+        clusters_expected.append(extraction.Cluster(extraction.ClusterType.LINEAR, lin_cluster))
+        ghz_cluster = graphix.GraphState()
+        ghz_cluster.add_nodes_from([0, 1, 2, 3, 4])
+        ghz_cluster.add_edges_from([(0, 1), (0, 2), (0, 3), (0, 4)])
+        clusters_expected.append(extraction.Cluster(extraction.ClusterType.GHZ, ghz_cluster))
+
+        self.assertEqual(
+            (clusters[0] == clusters_expected[0] and clusters[1] == clusters_expected[1])
+            or (clusters[0] == clusters_expected[1] and clusters[1] == clusters_expected[0]),
+            True,
+        )
+
+    def test_cluster_extraction_pentagonal_cluster(self):
+        gs = graphix.GraphState()
+        nodes = [0, 1, 2, 3, 4]
+        edges = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 0)]
+        gs.add_nodes_from(nodes)
+        gs.add_edges_from(edges)
+        clusters = extraction.extract_clusters_from_graph(gs)
+        self.assertEqual(len(clusters), 2)
+        self.assertEqual(
+            (clusters[0].type == extraction.ClusterType.GHZ and clusters[1].type == extraction.ClusterType.LINEAR)
+            or (clusters[0].type == extraction.ClusterType.LINEAR and clusters[1].type == extraction.ClusterType.GHZ),
+            True,
+        )
+        self.assertEqual(
+            (len(clusters[0].graph.nodes) == 3 and len(clusters[1].graph.nodes) == 4)
+            or (len(clusters[0].graph.nodes) == 4 and len(clusters[1].graph.nodes) == 3),
+            True,
+        )
