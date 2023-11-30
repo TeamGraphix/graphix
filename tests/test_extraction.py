7a8ff74741a5f825b3091116811903dda64860d1
Kazuki Tsuoka <kazukitsuoka@g.ecc.u-tokyo.ac.jp>
2023-11-30 16:23:09 +0900

    tests: test with rustworkx

--- tests/test_extraction.py
+++ tests/test_extraction.py
@@ -114,9 +114,9 @@ class TestExtraction(unittest.TestCase):
         )
 
 
-class TestExtractionWithNetworkx(unittest.TestCase):
+class TestExtractionWithRustworkX(unittest.TestCase):
     def test_cluster_extraction_one_ghz_cluster(self):
-        gs = graphix.GraphState(use_rustworkx=False)
+        gs = graphix.GraphState(use_rustworkx=True)
         nodes = [0, 1, 2, 3, 4]
         edges = [(0, 1), (0, 2), (0, 3), (0, 4)]
         gs.add_nodes_from(nodes)
@@ -128,7 +128,7 @@ class TestExtractionWithNetworkx(unittest.TestCase):
 
     # we consider everything smaller than 4, a GHZ
     def test_cluster_extraction_small_ghz_cluster_1(self):
-        gs = graphix.GraphState(use_rustworkx=False)
+        gs = graphix.GraphState(use_rustworkx=True)
         nodes = [0, 1, 2]
         edges = [(0, 1), (1, 2)]
         gs.add_nodes_from(nodes)
@@ -140,7 +140,7 @@ class TestExtractionWithNetworkx(unittest.TestCase):
 
     # we consider everything smaller than 4, a GHZ
     def test_cluster_extraction_small_ghz_cluster_2(self):
-        gs = graphix.GraphState(use_rustworkx=False)
+        gs = graphix.GraphState(use_rustworkx=True)
         nodes = [0, 1]
         edges = [(0, 1)]
         gs.add_nodes_from(nodes)
@@ -151,7 +151,7 @@ class TestExtractionWithNetworkx(unittest.TestCase):
         self.assertEqual(clusters[0] == extraction.ResourceGraph(type=extraction.ResourceType.GHZ, graph=gs), True)
 
     def test_cluster_extraction_one_linear_cluster(self):
-        gs = graphix.GraphState(use_rustworkx=False)
+        gs = graphix.GraphState(use_rustworkx=True)
         nodes = [0, 1, 2, 3, 4, 5, 6]
         edges = [(0, 1), (1, 2), (2, 3), (5, 4), (4, 6), (6, 0)]
         gs.add_nodes_from(nodes)
@@ -162,7 +162,7 @@ class TestExtractionWithNetworkx(unittest.TestCase):
         self.assertEqual(clusters[0] == extraction.ResourceGraph(type=extraction.ResourceType.LINEAR, graph=gs), True)
 
     def test_cluster_extraction_one_ghz_one_linear(self):
-        gs = graphix.GraphState(use_rustworkx=False)
+        gs = graphix.GraphState(use_rustworkx=True)
         nodes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         edges = [(0, 1), (0, 2), (0, 3), (0, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9)]
         gs.add_nodes_from(nodes)
@@ -171,11 +171,11 @@ class TestExtractionWithNetworkx(unittest.TestCase):
         self.assertEqual(len(clusters), 2)
 
         clusters_expected = []
-        lin_cluster = graphix.GraphState(use_rustworkx=False)
+        lin_cluster = graphix.GraphState(use_rustworkx=True)
         lin_cluster.add_nodes_from([4, 5, 6, 7, 8, 9])
         lin_cluster.add_edges_from([(4, 5), (5, 6), (6, 7), (7, 8), (8, 9)])
         clusters_expected.append(extraction.ResourceGraph(extraction.ResourceType.LINEAR, lin_cluster))
-        ghz_cluster = graphix.GraphState(use_rustworkx=False)
+        ghz_cluster = graphix.GraphState(use_rustworkx=True)
         ghz_cluster.add_nodes_from([0, 1, 2, 3, 4])
         ghz_cluster.add_edges_from([(0, 1), (0, 2), (0, 3), (0, 4)])
         clusters_expected.append(extraction.ResourceGraph(extraction.ResourceType.GHZ, ghz_cluster))
@@ -187,7 +187,7 @@ class TestExtractionWithNetworkx(unittest.TestCase):
         )
 
     def test_cluster_extraction_pentagonal_cluster(self):
-        gs = graphix.GraphState(use_rustworkx=False)
+        gs = graphix.GraphState(use_rustworkx=True)
         nodes = [0, 1, 2, 3, 4]
         edges = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 0)]
         gs.add_nodes_from(nodes)
@@ -206,7 +206,7 @@ class TestExtractionWithNetworkx(unittest.TestCase):
         )
 
     def test_cluster_extraction_one_plus_two(self):
-        gs = graphix.GraphState(use_rustworkx=False)
+        gs = graphix.GraphState(use_rustworkx=True)
         nodes = [0, 1, 2]
         edges = [(0, 1)]
         gs.add_nodes_from(nodes)
