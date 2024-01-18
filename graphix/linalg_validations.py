2b5cec3d35e5b62205116b2d1a8b7fba8b10438c
mgarnier59 <50111289+mgarnier59@users.noreply.github.com>
2024-01-18 18:16:15 +0100

    minor updates

--- graphix/linalg_validations.py
+++ graphix/linalg_validations.py
@@ -29,26 +29,11 @@ def check_psd(matrix: np.ndarray, tol: float = 1e-15) -> bool:
         matrix to check. Normally already square and 2**n x 2**n
     tol : float
         tolerance on the small negatives. Default 1e-15.
-    method : 'choleski' or 'sylvester' (or 'eigendecomp'). Default 'choleski'
-        if method = 'choleski': attempts a Choleski decomposition. If the matrix is not PSD raises a numpy.linalg.LinAlgError. (but error can be something else...)
-        if method = 'sylvester': computes the determinants of all square matrices of increasing dimension starting from the top left corner. Sylvester : PSD <-> all principal minors are non negative. NOT just leading!!!!!!
-        NOPE. Sylvester for PSD is all principal minors, not just leading !!! So VERY expensive.
     """
 
-    # if matrix is not hermitian, raises an error
-    # if no error: eigenvals are real
-
     evals = np.linalg.eigvalsh(matrix)
-    print(matrix)
-    print(evals)
-    # remove small negatives like -1e-17
-    evals[np.abs(evals) < tol] = 0
 
-    # sort (ascending order)
-    evals.sort()
-
-    # PSD test. Just look
-    if not evals[0] >= 0:
+    if not all(evals >= -tol):
         raise ValueError("The matrix is not positive semi-definite.")
 
     return True
@@ -79,14 +64,14 @@ def check_data_normalization(data: Union[list, tuple, np.ndarray]) -> bool:
     opsu = np.array([i["coef"] * np.conj(i["coef"]) * i["operator"].conj().T @ i["operator"] for i in data])
 
     if not np.allclose(np.sum(opsu, axis=0), np.eye(2 ** int(np.log2(len(data[0]["operator"]))))):
-        raise ValueError(f"The specified channel is not normalized. {np.sum(opsu, axis=0)}")
+        raise ValueError(f"The specified channel is not normalized {np.sum(opsu, axis=0)}.")
     return True
 
 
 def check_data_dims(data: Union[list, tuple, np.ndarray]) -> bool:
 
     # convert to set to remove duplicates
-    dims = list(set([i["operator"].shape for i in data]))
+    dims = set([i["operator"].shape for i in data])
 
     # check all the same dimensions and that they are square matrices
     # TODO replace by using array.ndim
@@ -100,41 +85,23 @@ def check_data_dims(data: Union[list, tuple, np.ndarray]) -> bool:
 
 def check_data_values_type(data: Union[list, tuple, np.ndarray]) -> bool:
 
-    value_types = list(set([isinstance(i, dict) for i in data]))
-
-    if value_types == [True]:
-
-        key0_values = list(set([list(i.keys())[0] == "coef" for i in data]))
-        key1_values = list(set([list(i.keys())[1] == "operator" for i in data]))
-
-        if key0_values == [True] and key1_values == [True]:
-            operator_types = list(set([isinstance(i["operator"], np.ndarray) for i in data]))
-
-            if operator_types == [True]:
-                operator_dtypes = list(
-                    set([i["operator"].dtype in [float, complex, np.float64, np.complex128] for i in data])
-                )
-
-                if operator_dtypes == [True]:
-                    par_types = list(
-                        set([isinstance(i["coef"], (float, complex, np.float64, np.complex128)) for i in data])
-                    )
-
-                    if par_types == [True]:
-                        pass
-                    else:
-                        raise TypeError("All parameters are not scalars")
-
-                else:
-                    raise TypeError(
-                        f"All operators  {list([i['operator'].dtype == (float or complex or np.float64 or np.complex128) for i in data])}."
-                    )
-            else:
-                raise TypeError("All operators don't have the same type and must be np.ndarray.")
-        else:
-            raise KeyError("The keys of the indivudal Kraus operators must be coef and operator.")
-    else:
+    if not all(
+        isinstance(i, dict) for i in data
+    ):  # ni liste ni ensemble mais iterable (lazy) pas stocké, executé au besoin
         raise TypeError("All values are not dictionaries.")
+
+    if not all(set(i.keys()) == {"coef", "operator"} for i in data):
+        raise KeyError("The keys of the indivudal Kraus operators must be coef and operator.")
+
+    if not all(isinstance(i["operator"], np.ndarray) for i in data):
+        raise TypeError("All operators don't have the same type and must be np.ndarray.")
+
+    for i in data:
+        if not i["operator"].dtype in (int, float, complex, np.float64, np.complex128):
+            raise TypeError(f"All operators dtype must be scalar and not {i['operator'].dtype}.")
+
+    if not all(isinstance(i["coef"], (int, float, complex, np.float64, np.complex128)) for i in data):
+        raise TypeError("All coefs dtype must be scalar.")
 
     return True
 
