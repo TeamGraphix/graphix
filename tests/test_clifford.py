473876bfba32f80926cef3a2343992fffa573602
masa10-f <masa1063fuk@gmail.com>
2024-01-18 23:51:19 +0900

    black

--- tests/test_clifford.py
+++ tests/test_clifford.py
@@ -4,9 +4,7 @@ import unittest
 
 import numpy as np
 
-from graphix.clifford import (CLIFFORD, CLIFFORD_CONJ,
-                              CLIFFORD_HSZ_DECOMPOSITION, CLIFFORD_MEASURE,
-                              CLIFFORD_MUL)
+from graphix.clifford import CLIFFORD, CLIFFORD_CONJ, CLIFFORD_HSZ_DECOMPOSITION, CLIFFORD_MEASURE, CLIFFORD_MUL
 
 
 class TestClifford(unittest.TestCase):
