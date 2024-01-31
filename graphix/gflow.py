4008ccb92751616474537c83805d330d7da0c469
d1ssk <daichi.sasaki.00@gmail.com>
2024-01-31 19:45:40 +0900

    add puli flow finder

--- graphix/gflow.py
+++ graphix/gflow.py
@@ -318,6 +318,257 @@ def flowaux(nodes, edges, input, output, v_c, f, l_k, k):
     )
 
 
+def pauliflow(graph, input, output, meas_planes=None, meas_angles=None):
+    """Maximally delayed Pauli flow finding algorithm
+
+    For open graph g with input, output, measurement planes and measurement angles, this returns maximally delayed Pauli flow.
+
+    Pauli flow consist of function p(i) where i is the qubit labels,
+    and strict partial ordering < or layers labels l_k where each element
+    specify the order of qubits to be measured to maintain determinism in MBQC.
+    In practice, we must measure qubits in order specified in array l_k (increasing order
+    of l_k from 1), and for each measurements of qubit i we must perform corrections on
+    qubits in p(i), depending on the measurement outcome.
+
+    For more details of Pauli flow and the finding algorithm used in this method,
+    see Simmons et al., EPTCS 343, 2021, pp. 50-101 (arXiv:2109.05654).
+
+    Parameters
+    ----------
+    graph: nx.Graph
+        graph (incl. in and out)
+    input: set
+        set of node labels for input
+    output: set
+        set of node labels for output
+    meas_planes: dict
+        measurement planes for each qubits. meas_planes[i] is the measurement plane for qubit i.
+    meas_angles: dict
+        measurement angles for each qubits. meas_angles[i] is the measurement angle for qubit i.
+
+    Returns
+    -------
+    p: dict
+        Pauli flow function. p[i] is the set of qubits to be corrected for the measurement of qubit i.
+    l_k: dict
+        layers obtained by  Pauli flow algorithm. l_k[d] is a node set of depth d.
+    """
+    l_k = dict()
+    p = dict()
+    Lx, Ly, Lz = set(), set(), set()
+    for node in graph.nodes:
+        if node in output:
+            l_k[node] = 0
+        elif meas_planes[node] == "XY":
+            if meas_angles[node] == 0:
+                Lx |= {node}
+            elif meas_angles[node] == 1 / 2:
+                Ly |= {node}
+        elif meas_planes[node] == "ZX":
+            if meas_angles[node] == 0:
+                Lz |= {node}
+            elif meas_angles[node] == 1 / 2:
+                Lx |= {node}
+        elif meas_planes[node] == "YZ":
+            if meas_angles[node] == 0:
+                Ly |= {node}
+            elif meas_angles[node] == 1 / 2:
+                Lz |= {node}
+    print("Lx is ", Lx)
+    print("Ly is ", Ly)
+    print("Lz is ", Lz)
+    print("l_k is ", l_k)
+    return pauliflowaux(graph, input, output, meas_planes, 0, set(), output, l_k, p, Lx, Ly, Lz)
+
+
+def pauliflowaux(
+    graph,
+    input: set,
+    output: set,
+    meas_planes: dict,
+    k: int,
+    correction_candidate: set,
+    solved_nodes: set,
+    l_k: dict,
+    p: dict,
+    Lx: set,
+    Ly: set,
+    Lz: set,
+):
+    """Function to find one layer of the Pauli flow.
+
+    Ref: Simmons et al., EPTCS 343, 2021, pp. 50-101 (arXiv:2109.05654).
+
+    Parameters
+    ----------
+    graph: nx.Graph
+        graph (incl. in and out)
+    input: set
+        set of node labels for input
+    output: set
+        set of node labels for output
+    meas_planes: dict
+        measurement planes for each qubits. meas_planes[i] is the measurement plane for qubit i.
+    k: int
+        current layer number.
+    correction_candidate: set
+        set of qubits to be corrected.
+    solved_nodes: set
+        set of qubits whose layers are already determined.
+    l_k: dict
+        layers obtained by gflow algorithm. l_k[d] is a node set of depth d.
+    p: dict
+        Pauli flow function. p[i] is the set of qubits to be corrected for the measurement of qubit i.
+    Lx: set
+        set of qubits whose measurement operator is "X".
+    Ly: set
+        set of qubits whose measurement operator is "Y".
+    Lz: set
+        set of qubits whose measurement operator is "Z".
+
+    Returns
+    -------
+    p: dict
+        Pauli flow function. p[i] is the set of qubits to be corrected for the measurement of qubit i.
+    l_k: dict
+        layers obtained by Pauli flow algorithm. l_k[d] is a node set of depth d.
+    """
+    solved_update = set()
+    nodes = set(graph.nodes)
+    if output == nodes:
+        return p, l_k
+    unsolved_nodes = nodes - solved_nodes
+
+    adj_mat, node_order_list = get_adjacency_matrix(graph)
+    adj_mat_w_id = adj_mat.copy() + MatGF2(np.identity(adj_mat.data.shape[0], dtype=int))
+    node_order_row = node_order_list.copy()
+    node_order_row_lower = node_order_list.copy()
+    node_order_col = node_order_list.copy()
+    print("node_order_row is \n", node_order_row)
+    print("node_order_col is \n", node_order_col)
+    print("adj_mat is \n", adj_mat)
+    print("node_order_row_lower is \n", node_order_row_lower)
+    print("adj_mat_w_id is \n", adj_mat_w_id)
+
+    Pbar = correction_candidate | Ly | Lz
+    P = nodes - Pbar
+    K = (correction_candidate | Lx | Ly) & (nodes - input)
+    Y = Ly - correction_candidate
+    print("P is \n", P)
+    print("K is \n", K)
+    print("Y is \n", Y)
+
+    for node in unsolved_nodes:
+        adj_mat_ = adj_mat.copy()
+        adj_mat_w_id_ = adj_mat_w_id.copy()
+        node_order_row_ = node_order_row.copy()
+        node_order_row_lower_ = node_order_row_lower.copy()
+        node_order_col_ = node_order_col.copy()
+        for node_ in nodes - (P | {node}):
+            adj_mat_.remove_row(node_order_row_.index(node_))
+            node_order_row_.remove(node_)
+        for node_ in nodes - (Y - {node}):
+            adj_mat_w_id_.remove_row(node_order_row_lower_.index(node_))
+            node_order_row_lower_.remove(node_)
+        for node_ in nodes - (K - {node}):
+            adj_mat_.remove_col(node_order_col_.index(node_))
+            adj_mat_w_id_.remove_col(node_order_col_.index(node_))
+            node_order_col_.remove(node_)
+        print("\n -Node is ", node)
+        print("node_order_row_ is \n", node_order_row_)
+        print("node_order_col_ is \n", node_order_col_)
+        print("adj_mat_ is \n", adj_mat_)
+        print("node_order_row_lower_ is \n", node_order_row_lower_)
+        print("adj_mat_w_id_ is \n", adj_mat_w_id_)
+        adj_mat_.concatenate(adj_mat_w_id_, axis=0)
+
+        solved = False
+        if not solved and (meas_planes[node] == "XY" or node in Lx or node in Ly):
+            S = MatGF2(np.zeros((len(node_order_row_), 1), dtype=int))
+            S.data[node_order_row_.index(node), :] = 1
+            print("S_upper is \n", S)
+            S_lower = MatGF2(np.zeros((len(node_order_row_lower_), 1), dtype=int))
+            print("S_lower is \n", S_lower)
+            S.concatenate(S_lower, axis=0)
+            print("S is \n", S)
+            adj_mat_XY, S, _, col_pertumutation_XY = adj_mat_.forward_eliminate(S, copy=True)
+            x_XY, kernels_XY = adj_mat_XY.backward_substitute(S)
+            if x_XY[0, 0] != sp.nan:
+                print("x_XY is \n", x_XY)
+                solved = True
+                solved_update |= {node}
+                p[node] = set()
+                for i in range(x_XY.shape[0]):
+                    if x_XY[i, 0]:
+                        p[node] |= {node_order_col_[col_pertumutation_XY[i]]}
+                l_k[node] = k
+
+        if not solved and (meas_planes[node] == "ZX" or node in Lz or node in Lx):
+            S = MatGF2(np.zeros((len(node_order_row_), 1), dtype=int))
+            S.data[node_order_row_.index(node)] = 1
+            for neighbor in search_neighbor(node, graph.edges):
+                if neighbor in P and neighbor in P | {node}:
+                    S.data[node_order_row_.index(neighbor), :] = 1
+            S_lower = MatGF2(np.zeros((len(node_order_row_lower_), 1), dtype=int))
+            for neighbor in search_neighbor(node, graph.edges):
+                if neighbor in Y and neighbor in Y - {node}:
+                    S_lower.data[node_order_row_lower_.index(neighbor), :] = 1
+            S.concatenate(S_lower, axis=0)
+            adj_mat_ZX, S, _, col_pertumutation_ZX = adj_mat_.forward_eliminate(S)
+            x_ZX, kernels_ZX = adj_mat_ZX.backward_substitute(S)
+            if x_ZX[0, 0] != sp.nan:
+                solved = True
+                solved_update |= {node}
+                p[node] = set()
+                for i in range(x_ZX.shape[0]):
+                    if x_ZX[i, 0]:
+                        p[node] |= {node_order_col_[col_pertumutation_ZX[i]]}
+                l_k[node] = k
+
+        if not solved and (meas_planes[node] == "YZ" or node in Ly or node in Lz):
+            S = MatGF2(np.zeros((len(node_order_row_), 1), dtype=int))
+            for neighbor in search_neighbor(node, graph.edges):
+                if neighbor in P and neighbor in P | {node}:
+                    S.data[node_order_row_.index(neighbor), :] = 1
+            S_lower = MatGF2(np.zeros((len(node_order_row_lower_), 1), dtype=int))
+            for neighbor in search_neighbor(node, graph.edges):
+                if neighbor in Y and neighbor in Y - {node}:
+                    S_lower.data[node_order_row_lower_.index(neighbor), :] = 1
+            S.concatenate(S_lower, axis=0)
+            adj_mat_YZ, S, _, col_pertumutation_YZ = adj_mat_.forward_eliminate(S)
+            x_YZ, kernels_YZ = adj_mat_YZ.backward_substitute(S)
+            if x_YZ[0, 0] != sp.nan:
+                solved = True
+                solved_update |= {node}
+                p[node] = set()
+                for i in range(x_YZ.shape[0]):
+                    if x_YZ[i, 0]:
+                        p[node] |= {node_order_col_[col_pertumutation_YZ[i]]}
+                l_k[node] = k
+
+    if solved_update == set() and k > 0:
+        if solved_nodes == nodes:
+            return p, l_k
+        else:
+            return None, None
+    else:
+        B = solved_nodes | solved_update
+        return pauliflowaux(
+            graph,
+            input,
+            output,
+            meas_planes,
+            k + 1,
+            B,
+            B,
+            l_k,
+            p,
+            Lx,
+            Ly,
+            Lz,
+        )
+
+
 def search_neighbor(node, edges):
     """Function to find neighborhood of node in edges. This is an ancillary method for `flowaux()`.
 
--- graphix/linalg.py
+++ graphix/linalg.py
@@ -75,6 +75,18 @@ class MatGF2:
         array_to_add = array_to_add.reshape((1, self.data.shape[0]))
         self.data = np.insert(self.data, col, array_to_add, axis=1)
 
+    def concatenate(self, other, axis=1):
+        """concatinate two matrices
+
+        Parameters
+        ----------
+        other: MatGF2
+            matrix to concatinate
+        axis: int(optional)
+            axis to concatinate. Defaults to 1.
+        """
+        self.data = np.concatenate((self.data, other.data), axis=axis)
+
     def remove_row(self, row):
         """remove a row from the matrix
 
