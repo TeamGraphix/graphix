350b59cd1ef7e6a2685732b38726c6111689a9c3
Yuki Watanabe <yuki.watanabe@fixstars.com>
2024-04-16 19:56:50 +0900

    add simulation without cotengra optimizer

--- benchmarks/svbenchmark.py
+++ benchmarks/svbenchmark.py
@@ -13,7 +13,7 @@ qu.core._NUM_THREAD_WORKERS = 1
 # %%
 
 # NOTE: to increase n_qubit to more than 11, increase the max_qubit_num argument in the StatevectorBackend class
-n_qubit_list = list(range(2, 10))
+n_qubit_list = list(range(2, 21))
 
 sv_sim = []
 eco_sim = []
@@ -29,14 +29,11 @@ for n_qubit in n_qubit_list:
     pat_original.minimize_space()
     max_sp = pat_original.max_space()
     max_space_ls.append(max_sp)
-    if max_sp >= 20:
-        sv_sim.append(None)
-    else:
-        start = perf_counter()
-        sv = pat_original.simulate_pattern()
-        end = perf_counter()
-        sv_sim.append(end - start)
-        del sv
+    start = perf_counter()
+    sv = pat_original.simulate_pattern()
+    end = perf_counter()
+    sv_sim.append(end - start)
+    del sv
 
     # tn
     tn = pattern.simulate_pattern("tensornetwork")
