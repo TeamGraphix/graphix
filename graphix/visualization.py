989b3646eed2ee2b7f6f765830ccd51a3703293a
EarlMilktea <66886825+EarlMilktea@users.noreply.github.com>
2024-05-07 11:05:48 +0900

    :art: Resolve variable overwrite

--- graphix/visualization.py
+++ graphix/visualization.py
@@ -1014,8 +1014,8 @@ class GraphVisualizer:
         for i, k in enumerate(start_nodes):
             pos[k][1] = i
             while k in f.keys():
-                k = list(f[k])[0]
-                pos[k][1] = i
+                kk = list(f[k])[0]
+                pos[kk][1] = i
 
         lmax = max(l_k.values())
         # Change the x coordinates of the nodes based on their layer, sort in descending order
