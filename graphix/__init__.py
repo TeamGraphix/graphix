8fee1b1faf53d9bb7e6838e53a8b48264d88999b
S.S <66886825+EarlMilktea@users.noreply.github.com>
2024-05-09 13:10:09 +0900

    Update packaging system (#139)
    * :heavy_plus_sign: Add scipy
    
    * :hammer: Migrate to pyproject.toml
    
    * :fire: Remove version.py
    
    * :see_no_evil: Ignore _version.py
    
    Automatically generated by setuptools_scm
    
    * :bug: Fix broken import
    
    * add publishing automation
    
    ---------
    
    Co-authored-by: Shinichi Sunami <33350509+shinich1@users.noreply.github.com>

--- graphix/__init__.py
+++ graphix/__init__.py
@@ -1,6 +1,5 @@
-from graphix.transpiler import Circuit
 from graphix.generator import generate_from_graph
 from graphix.pattern import Pattern
 from graphix.sim.statevec import Statevec
+from graphix.transpiler import Circuit
 from graphix.graphsim import GraphState
-from graphix.version import __version__
