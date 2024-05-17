198006381c7ad46e06c9674ed7b3d5e4307fe2cf
EarlMilktea <66886825+EarlMilktea@users.noreply.github.com>
2024-05-17 09:30:24 +0900

    :wrench: Add patch mode

--- graphix/__init__.py
+++ graphix/__init__.py
@@ -3,3 +3,7 @@ from graphix.graphsim import GraphState
 from graphix.pattern import Pattern
 from graphix.sim.statevec import Statevec
 from graphix.transpiler import Circuit
+
+if False:
+    _ = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+"""
