fae1f4ac032a6e2e80a1d4d2b22f8710150dca38
Yuki Watanabe <yuki.watanabe@fixstars.com>
2024-07-22 20:51:52 +0900

    🎨 Add benchmark for contraction skipped results

--- benchmarks/efficient_contraction_order_statevec.py
+++ benchmarks/efficient_contraction_order_statevec.py
@@ -39,6 +39,7 @@ qu.core._NUM_THREAD_WORKERS = 1
 n_qubit_list = list(range(2, 31))
 
 sv_sim = []
+neco_sim = []
 eco_sim = []
 eco_sim_wo_ctg = []
 max_space_ls = []
@@ -49,14 +50,30 @@ for n_qubit in n_qubit_list:
     pattern = circuit.transpile().pattern
     pattern.standardize()
     pat_original = deepcopy(pattern)
+
+    # statevector simulation: sv
+    if n_qubit > 17:
+        sv_sim.append(-1)
+        max_space_ls.append(-1)
+    else:
+        start = perf_counter()
+        pat_original.minimize_space()
+        max_sp = pat_original.max_space()
+        max_space_ls.append(max_sp)
+        sv = pat_original.simulate_pattern(max_qubit_num=max_sp)
+        end = perf_counter()
+        sv_sim.append(end - start)
+        del sv
+
+    # no-contraction order search simulation: tn
+    tn = pattern.simulate_pattern("tensornetwork")
+    output_inds = [tn._dangling[str(index)] for index in tn.default_output_nodes]
+
     start = perf_counter()
-    pat_original.minimize_space()
-    max_sp = pat_original.max_space()
-    max_space_ls.append(max_sp)
-    sv = pat_original.simulate_pattern(max_qubit_num=max_sp)
+    tn_sv = tn.to_statevector(backend="numpy", skip_tn_simp=True, optimize=None)
     end = perf_counter()
-    sv_sim.append(end - start)
-    del sv
+    neco_sim.append(end - start)
+    del tn_sv
 
     # efficient contraction order simulation (eco-sim): tn
     tn = pattern.simulate_pattern("tensornetwork")
@@ -65,7 +82,7 @@ for n_qubit in n_qubit_list:
     start = perf_counter()
     tn_sv = tn.to_statevector(
         backend="numpy",
-        skip=False,
+        skip_tn_simp=False,
         optimize=ctg.HyperOptimizer(minimize="combo", max_time=600, progbar=True),
     )
     end = perf_counter()
@@ -76,7 +93,7 @@ for n_qubit in n_qubit_list:
     start = perf_counter()
     tn_sv = tn.to_statevector(
         backend="numpy",
-        skip=False,
+        skip_tn_simp=False,
         optimize=None,
     )
     end = perf_counter()
@@ -86,9 +103,9 @@ for n_qubit in n_qubit_list:
 # %%
 # Finally, we save the results to a text file.
 with open("sqrqcresults.txt", "w") as f:
-    f.write("n_qubit, sv_sim, eco_sim, eco_sim_wo_ctg, max_space_ls\n")
+    f.write("n_qubit, neco_sim, sv_sim, eco_sim, eco_sim_wo_ctg, max_space_ls\n")
     for i in range(len(n_qubit_list)):
-        f.write(f"{n_qubit_list[i]}, {sv_sim[i]}, {eco_sim[i]}, {eco_sim_wo_ctg[i]}, {max_space_ls[i]}\n")
+        f.write(f"{n_qubit_list[i]}, {sv_sim[i]}, {neco_sim[i]}, {eco_sim[i]}, {eco_sim_wo_ctg[i]}, {max_space_ls[i]}\n")
 
 # %%
 # We can now plot the simulation time results.
@@ -97,17 +114,19 @@ import numpy as np
 
 data = np.loadtxt("sqrqcresults.txt", delimiter=",", skiprows=1)
 n_qubits = data[:, 0].astype(int)
-sv_sim = data[:, 1].astype(float)
-eco_sim = data[:, 2].astype(float)
-eco_sim_wo_ctg = data[:, 3].astype(float)
-max_sp = data[:, 4].astype(int)
+sv_sim = data[:16, 1].astype(float)
+neco_sim = data[:, 2].astype(float)
+eco_sim = data[:, 3].astype(float)
+eco_sim_wo_ctg = data[:, 4].astype(float)
+max_sp = data[:16, 5].astype(int)
 
 fig, ax1 = plt.subplots(figsize=(8, 5))
 color = "tab:red"
 ax1.set_xlabel("Original Circuit Size [qubit]")
 ax1.set_ylabel("Simulation time [sec]")
 ax1.set_yscale("log")
-ax1.scatter(n_qubits, sv_sim, marker="x", label="MBQC Statevector (minimizing sp)")
+ax1.scatter(n_qubits[:16], sv_sim, marker="x", label="MBQC Statevector (minimizing sp)")
+ax1.scatter(n_qubits, neco_sim, marker="x", label="MBQC TN base (contraction skipped)")
 ax1.scatter(n_qubits, eco_sim, marker="x", label="MBQC TN base (with cotengra)")
 ax1.scatter(n_qubits, eco_sim_wo_ctg, marker="x", label="MBQC TN base (without cotengra)")
 ax1.tick_params(axis="y")
@@ -118,10 +137,12 @@ plt.grid(True, which="Major")
 ax2 = ax1.twinx()
 color = "tab:blue"
 ax2.set_ylabel("Max Space [qubit]")
-ax2.plot(n_qubits, max_sp, color=color, linestyle="--", label="Max Space")
+ax2.plot(n_qubits[:16], max_sp, color=color, linestyle="--", label="Max Space")
 ax2.tick_params(axis="y")
 ax2.legend(loc="lower right")
 
 plt.rcParams["svg.fonttype"] = "none"
 plt.savefig("simulation_time_wo_p.png")
 plt.close()
+
+# %%
