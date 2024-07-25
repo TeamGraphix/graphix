697ff2d3d5ddf2d1dfe894c2cefa229da686b1a9
Thierry Martinez <thierry.martinez@inria.fr>
2024-07-25 22:51:46 +0200

    Split tests
    Shinichi's review: https://github.com/TeamGraphix/graphix/pull/158#discussion_r1686190752

--- tests/test_parameter.py
+++ tests/test_parameter.py
@@ -3,16 +3,25 @@ from graphix.parameter import Placeholder
 from graphix.pattern import Pattern
 
 
-def test_placeholder() -> None:
+def test_pattern_without_parameter_is_not_parameterized() -> None:
     pattern = Pattern(input_nodes=[0, 1])
     pattern.add(graphix.command.M(node=0))
     # A pattern without parameterized angle is not parameterized.
     assert not pattern.is_parameterized()
-    # Substitution in a pattern without parameterized angle is the identity.
+
+
+def test_pattern_without_parameter_subs_is_identity() -> None:
+    pattern = Pattern(input_nodes=[0, 1])
+    pattern.add(graphix.command.M(node=0))
     alpha = Placeholder("alpha")
+    # Substitution in a pattern without parameterized angle is the identity.
     assert list(pattern) == list(pattern.subs(alpha, 0))
-    # A pattern without parameterized angle can be simulated.
-    pattern.simulate_pattern()
+
+
+def test_pattern_substitution() -> None:
+    pattern = Pattern(input_nodes=[0, 1])
+    pattern.add(graphix.command.M(node=0))
+    alpha = Placeholder("alpha")
     pattern.add(graphix.command.M(node=1, angle=alpha))
     assert pattern.is_parameterized()
     # Parameterized patterns can be substituted, even if some angles are not parameterized.
@@ -20,12 +29,27 @@ def test_placeholder() -> None:
     # If all parameterized angles have been instantiated, the pattern is no longer parameterized.
     assert not pattern0.is_parameterized()
     assert list(pattern0) == [graphix.command.M(node=0), graphix.command.M(node=1)]
+
+
+def test_instantiated_pattern_simulation() -> None:
+    pattern = Pattern(input_nodes=[0, 1])
+    pattern.add(graphix.command.M(node=0))
+    alpha = Placeholder("alpha")
+    pattern.add(graphix.command.M(node=1, angle=alpha))
+    pattern0 = pattern.subs(alpha, 0)
     # Instantied patterns can be simulated.
     pattern0.simulate_pattern()
     pattern1 = pattern.subs(alpha, 1)
     assert not pattern1.is_parameterized()
     assert list(pattern1) == [graphix.command.M(node=0), graphix.command.M(node=1, angle=1)]
     pattern1.simulate_pattern()
+
+
+def test_multiple_parameters() -> None:
+    pattern = Pattern(input_nodes=[0, 1])
+    pattern.add(graphix.command.M(node=0))
+    alpha = Placeholder("alpha")
+    pattern.add(graphix.command.M(node=1, angle=alpha))
     beta = Placeholder("beta")
     pattern.add(graphix.command.N(node=2))
     pattern.add(graphix.command.M(node=2, angle=beta))
