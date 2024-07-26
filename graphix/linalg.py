2be1d888852702b9141a600f561dad9607e39572
SS <66886825+EarlMilktea@users.noreply.github.com>
2024-07-27 05:30:36 +0900

    :recycle: Use raw string

--- graphix/linalg.py
+++ graphix/linalg.py
@@ -207,9 +207,9 @@ class MatGF2:
         return len(nonzero_index[0])
 
     def forward_eliminate(self, b=None, copy=False):
-        """forward eliminate the matrix
+        r"""forward eliminate the matrix
 
-        |A B| --\\ |I X|
+        |A B| --\ |I X|
         |C D| --/ |0 0|
         where X is an arbitrary matrix
 
