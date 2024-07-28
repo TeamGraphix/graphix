fa3ef562833e4a8fc749c085b8821b22d6183f5c
SS <66886825+EarlMilktea@users.noreply.github.com>
2024-07-28 09:41:38 +0900

    :coffin: Drop conditionally-defined export

--- graphix/graphsim/__init__.py
+++ graphix/graphsim/__init__.py
@@ -3,4 +3,4 @@ from .graphstate import GraphState
 from .nxgraphstate import NXGraphState
 from .rxgraphstate import RXGraphState
 from .rxgraphviews import EdgeList, NodeList
-from .utils import convert_rustworkx_to_networkx, is_graphs_equal
+from .utils import is_graphs_equal
