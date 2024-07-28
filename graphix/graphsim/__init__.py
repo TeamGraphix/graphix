b9e672b320e410d16a525ea8f216fbd042519c28
SS <66886825+EarlMilktea@users.noreply.github.com>
2024-07-28 09:53:55 +0900

    :bug: Resolve forced import of rx

--- graphix/graphsim/__init__.py
+++ graphix/graphsim/__init__.py
@@ -1,6 +1,4 @@
 from .basegraphstate import BaseGraphState
 from .graphstate import GraphState
 from .nxgraphstate import NXGraphState
-from .rxgraphstate import RXGraphState
-from .rxgraphviews import EdgeList, NodeList
 from .utils import is_graphs_equal
