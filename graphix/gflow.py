f88a1cf3d5b38971a1ad3097519f958eed453cc7
SS <66886825+EarlMilktea@users.noreply.github.com>
2024-07-28 17:13:17 +0900

    :bulb: Note on (p,) =

--- graphix/gflow.py
+++ graphix/gflow.py
@@ -332,6 +332,7 @@ def flowaux(
         N = search_neighbor(q, edges)
         p_set = N & (nodes - oset)
         if len(p_set) == 1:
+            # Iterate over p_set assuming there is only one element p
             (p,) = p_set
             f[p] = {q}
             l_k[p] = k
