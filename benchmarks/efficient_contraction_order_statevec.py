c9f0358d5d8582eb2ef396f6bd59036d6cbf4b88
Yuki Watanabe <yuki.watanabe@fixstars.com>
2024-08-19 20:19:01 +0900

    ✨ Show error bars in benchmarking results

--- benchmarks/efficient_contraction_order_statevec.py
+++ benchmarks/efficient_contraction_order_statevec.py
@@ -22,6 +22,8 @@ from copy import deepcopy
 from time import perf_counter
 
 import cotengra as ctg
+import numpy as np
+import numpy.typing as npt
 import quimb as qu
 from numpy.random import PCG64, Generator
 
@@ -36,8 +38,6 @@ qu.core._NUM_THREAD_WORKERS = 1
 # %%
 # We then run simulations.
 # Let's benchmark the simulation time of the statevector simulator and the efficient contraction order simulator.
-
-n_qubit_list = list(range(2, 25))
 
 
 def prepare_pattern(n_qubit: int) -> tuple[graphix.pattern.Pattern, graphix.pattern.Pattern]:
@@ -82,44 +82,59 @@ def single_iter(n_qubit: int, sv_sim: list, eco_sim: list) -> None:
     efficient_contraction_order_sim(eco_sim, pattern)
 
 
-def benchmark(n_qubit_list: list) -> tuple[list, list]:
+def benchmark(n_qubit_list: list) -> tuple[npt.NDArray, npt.NDArray]:
     sv_sim = []
     eco_sim = []
     for n_qubit in n_qubit_list:
         single_iter(n_qubit, sv_sim, eco_sim)
-    return sv_sim, eco_sim
+    return np.array(sv_sim), np.array(eco_sim)
 
 
-sv_sim, eco_sim = benchmark(n_qubit_list)
+# %%
+# Here, we calculate the simulation multiple times so that we can make error bars.
+n_qubit_list = list(range(2, 25))
+sv_sim_results: list[npt.NDArray] = []
+eco_sim_results: list[npt.NDArray] = []
 
+for _ in range(6):
+    sv_sim, eco_sim = benchmark(n_qubit_list)
+    sv_sim_results.append(sv_sim)
+    eco_sim_results.append(eco_sim)
+
+
+sv_sim_means = np.mean(sv_sim_results, axis=0)
+eco_sim_means = np.mean(eco_sim_results, axis=0)
+sv_sim_std = np.std(sv_sim_results, axis=0)
+eco_sim_std = np.std(eco_sim_results, axis=0)
 
 # %%
 # Finally, we save the results to a text file.
 with open("results.txt", "w") as f:
-    f.write("n_qubit, sv_sim, eco_sim\n")
+    f.write("n_qubit, sv_sim_mean, sv_sim_std, eco_sim_mean, eco_sim_std\n")
     for i in range(len(n_qubit_list)):
-        f.write(f"{n_qubit_list[i]}, {sv_sim[i]}, {eco_sim[i]}\n")
+        f.write(f"{n_qubit_list[i]}, {sv_sim_means[i]}, {sv_sim_std[i]}, {eco_sim_means[i]}, {eco_sim_std[i]}\n")
 
 # %%
 # We can now plot the simulation time results.
 import matplotlib.pyplot as plt  # noqa: E402
-import numpy as np  # noqa: E402
 
 data = np.loadtxt("results.txt", delimiter=",", skiprows=1)
 n_qubits = data[:, 0].astype(int)
-sv_sim = data[:16, 1].astype(float)
-eco_sim = data[:, 2].astype(float)
+sv_sim_mean = data[:16, 1].astype(float)
+sv_sim_std = data[:16, 2].astype(float)
+eco_sim_mean = data[:, 3].astype(float)
+eco_sim_std = data[:, 4].astype(float)
 
 fig, ax = plt.subplots(figsize=(8, 5))
 color = "tab:red"
-ax.set_xlabel("Original Circuit Size [qubit]")
-ax.set_ylabel("Simulation time [sec]")
+ax.set_xlabel("Original Circuit Size [qubit]", fontsize=20)
+ax.set_ylabel("Simulation time [sec]", fontsize=20)
 ax.set_yscale("log")
-ax.scatter(n_qubits[:16], sv_sim, marker="x", label="MBQC Statevector (minimizing sp)")
-ax.scatter(n_qubits, eco_sim, marker="x", label="MBQC TN base")
+ax.errorbar(n_qubits[:16], sv_sim_mean, yerr=sv_sim_std, color=color, fmt="o", label="MBQC Statevector (minimizing sp)")
+ax.errorbar(n_qubits, eco_sim_mean, yerr=eco_sim_std, fmt="x", label="MBQC TN base")
 ax.tick_params(axis="y")
 ax.legend(loc="upper left")
-ax.set_title("Simulation time")
+ax.set_title("Simulation time", fontsize=20)
 plt.grid(True, which="Major")
 plt.rcParams["svg.fonttype"] = "none"
 plt.savefig("simulation_time.png")
