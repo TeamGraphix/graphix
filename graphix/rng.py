5cd6dee4a6ef43cb669ecbdc4b17d9fd641bc5b6
Thierry Martinez <thierry.martinez@inria.fr>
2024-08-24 00:19:02 +0200

    Use thread-local variable for default RNG
    Suggested by EarlMilktea: https://github.com/TeamGraphix/graphix/pull/198#discussion_r1728101563

--- graphix/rng.py
+++ graphix/rng.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import threading
 from typing import TYPE_CHECKING
 
 import numpy as np
@@ -7,8 +8,14 @@ import numpy as np
 if TYPE_CHECKING:
     from numpy.random import Generator
 
+rng_local = threading.local()
+
 
 def ensure_rng(rng: Generator | None = None) -> Generator:
     if rng is not None:
         return rng
-    return np.random.default_rng()
+    if rng := getattr(rng_local, "rng", None):
+        return rng
+    rng = np.random.default_rng()
+    rng_local.rng = rng
+    return rng
