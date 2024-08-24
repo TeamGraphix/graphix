f694ad1a98b3057e5e3f0de40cb12f5d211737fd
Thierry Martinez <thierry.martinez@inria.fr>
2024-08-24 17:36:36 +0200

    Rename `rng_local` into `_rng_local` and add test with new thread

--- graphix/rng.py
+++ graphix/rng.py
@@ -8,14 +8,14 @@ import numpy as np
 if TYPE_CHECKING:
     from numpy.random import Generator
 
-rng_local = threading.local()
+_rng_local = threading.local()
 
 
 def ensure_rng(rng: Generator | None = None) -> Generator:
     if rng is not None:
         return rng
-    if rng := getattr(rng_local, "rng", None):
+    if rng := getattr(_rng_local, "rng", None):
         return rng
     rng = np.random.default_rng()
-    rng_local.rng = rng
+    _rng_local.rng = rng
     return rng
---
+++ tests/test_rng.py
@@ -0,0 +1,7 @@
+from threading import Thread
+
+from graphix.rng import ensure_rng
+
+def test_new_thread() -> None:
+    t = Thread(target=ensure_rng)
+    t.start()
