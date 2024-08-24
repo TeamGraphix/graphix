4d3d685988fe587df7d1e4cdac8c0f2b734cefc6
Thierry Martinez <thierry.martinez@inria.fr>
2024-08-25 01:18:25 +0200

    Add `test_threadpool`
    Suggested by EarlMilktea: https://github.com/TeamGraphix/graphix/pull/198#discussion_r1730040383

--- tests/test_rng.py
+++ tests/test_rng.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 from threading import Thread
 
 from graphix.rng import ensure_rng
@@ -6,3 +7,10 @@ from graphix.rng import ensure_rng
 def test_new_thread() -> None:
     t = Thread(target=ensure_rng)
     t.start()
+
+
+def test_threadpool() -> None:
+    with ThreadPoolExecutor() as executor:
+        tasks = executor.map(lambda _: ensure_rng(), range(100))
+        for _ in tasks:
+            pass
