6e8e0a92431e18c343dfa7ab7699fd69752e1897
thierry-martinez <thierry.martinez@inria.fr>
2024-09-02 10:12:41 +0200

    Update docs/source/conf.py
    Co-authored-by: S.S. <66886825+EarlMilktea@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ author = "Shinichi Sunami"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.intersphinx",
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.autosummary",
