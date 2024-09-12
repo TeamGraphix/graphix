09dd03347a757098759fa7ba8344ad4a6858b1bd
SS <66886825+EarlMilktea@users.noreply.github.com>
2024-09-12 14:54:19 +0900

    :recycle: Use _T

--- graphix/types.py
+++ graphix/types.py
@@ -12,10 +12,10 @@ if typing_extensions.TYPE_CHECKING:
 
 PositiveOrNullInt = typing_extensions.Annotated[int, annotated_types.Ge(0)]  # includes 0
 
-T = TypeVar("T")
+_T = TypeVar("_T")
 
 
-def check_list_elements(l: Iterable[T], ty: type[T]) -> None:
+def check_list_elements(l: Iterable[_T], ty: type[_T]) -> None:
     """Check that every element of the list has the given type."""
     for index, item in enumerate(l):
         if not isinstance(item, ty):
