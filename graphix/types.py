ef3426030055d45df1b42b2483b612a1bf9fe8bf
SS <66886825+EarlMilktea@users.noreply.github.com>
2024-09-12 14:32:30 +0900

    :recycle: Small fix

--- graphix/types.py
+++ graphix/types.py
@@ -2,13 +2,20 @@
 
 from __future__ import annotations
 
+from typing import TypeVar
+
 import annotated_types
 import typing_extensions
 
+if typing_extensions.TYPE_CHECKING:
+    from collections.abc import Iterable
+
 PositiveOrNullInt = typing_extensions.Annotated[int, annotated_types.Ge(0)]  # includes 0
 
+T = TypeVar("T")
 
-def check_list_elements(l, ty):
+
+def check_list_elements(l: Iterable[T], ty: type[T]) -> None:
     """Check that every element of the list has the given type."""
     for index, item in enumerate(l):
         if not isinstance(item, ty):
