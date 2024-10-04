6853e4db22b9a2b0ac3601258e3f810ba664dfef
SS <66886825+EarlMilktea@users.noreply.github.com>
2024-10-05 02:14:54 +0900

    :white_check_mark: Add tests

--- tests/test_clifford.py
+++ tests/test_clifford.py
@@ -1,6 +1,12 @@
 from __future__ import annotations
 
+import itertools
+
+import numpy as np
+import pytest
+
 from graphix.clifford import Clifford
+from graphix.pauli import IXYZ, ComplexUnit, Pauli, Sign
 
 
 class TestClifford:
@@ -20,3 +26,38 @@ class TestClifford:
     def test_index_type(self) -> None:
         for c in Clifford:
             assert isinstance(c.index, int)
+
+    @pytest.mark.parametrize("c", Clifford)
+    def test_repr(self, c: Clifford) -> None:
+        for term in repr(c).split(" @ "):
+            assert term in [
+                "graphix.clifford.I",
+                "graphix.clifford.H",
+                "graphix.clifford.S",
+                "graphix.clifford.Z",
+            ]
+
+    @pytest.mark.parametrize(
+        ("c", "p"),
+        itertools.product(
+            Clifford,
+            (
+                Pauli(sym, u)
+                for sym in IXYZ
+                for u in (
+                    ComplexUnit(Sign.Plus, False),
+                    ComplexUnit(Sign.Minus, False),
+                    ComplexUnit(Sign.Plus, True),
+                    ComplexUnit(Sign.Minus, True),
+                )
+            ),
+        ),
+    )
+    def test_measure(self, c: Clifford, p: Pauli) -> None:
+        cm = c.matrix
+        pm = p.matrix
+        cpc = c.measure(p)
+        if c == Clifford.I:
+            # Prevent aliasing
+            assert cpc is not p
+        assert np.allclose(cpc.matrix, cm.conj().T @ pm @ cm)
