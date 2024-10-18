2561dd6ca4baa9850715b92b612c62f817eef201
SS <66886825+EarlMilktea@users.noreply.github.com>
2024-10-18 23:02:55 +0900

    :white_check_mark: Add tests

--- tests/test_pauli.py
+++ tests/test_pauli.py
@@ -5,11 +5,23 @@ import itertools
 import numpy as np
 import pytest
 
-from graphix.fundamentals import ComplexUnit
+from graphix.fundamentals import Axis, ComplexUnit
 from graphix.pauli import Pauli
 
 
 class TestPauli:
+    def test_from_axis(self) -> None:
+        assert Pauli.from_axis(Axis.X) == Pauli.X
+        assert Pauli.from_axis(Axis.Y) == Pauli.Y
+        assert Pauli.from_axis(Axis.Z) == Pauli.Z
+
+    def test_axis(self) -> None:
+        with pytest.raises(ValueError):
+            _ = Pauli.I.axis
+        assert Pauli.X.axis == Axis.X
+        assert Pauli.Y.axis == Axis.Y
+        assert Pauli.Z.axis == Axis.Z
+
     @pytest.mark.parametrize(
         ("u", "p"),
         itertools.product(ComplexUnit, Pauli.iterate()),
@@ -23,3 +35,43 @@ class TestPauli:
     )
     def test_matmul(self, a: Pauli, b: Pauli) -> None:
         assert np.allclose((a @ b).matrix, a.matrix @ b.matrix)
+
+    def test_str(self) -> None:
+        assert str(Pauli.I) == "IXYZ.I"
+        assert str(1 * Pauli.I) == "IXYZ.I"
+        assert str(1j * Pauli.I) == "1j * IXYZ.I"
+        assert str(-1 * Pauli.I) == "-IXYZ.I"
+        assert str(-1j * Pauli.I) == "-1j * IXYZ.I"
+
+    @pytest.mark.parametrize("p", Pauli.iterate())
+    def test_neg(self, p: Pauli) -> None:
+        pneg = -p
+        assert pneg == -p
+
+    def test_iterate_false(self) -> None:
+        cmp = list(Pauli.iterate(include_unit=False))
+        assert len(cmp) == 4
+        assert cmp[0] == Pauli.I
+        assert cmp[1] == Pauli.X
+        assert cmp[2] == Pauli.Y
+        assert cmp[3] == Pauli.Z
+
+    def test_iterate_true(self) -> None:
+        cmp = list(Pauli.iterate(include_unit=True))
+        assert len(cmp) == 16
+        assert cmp[0] == Pauli.I
+        assert cmp[1] == 1j * Pauli.I
+        assert cmp[2] == -1 * Pauli.I
+        assert cmp[3] == -1j * Pauli.I
+        assert cmp[4] == Pauli.X
+        assert cmp[5] == 1j * Pauli.X
+        assert cmp[6] == -1 * Pauli.X
+        assert cmp[7] == -1j * Pauli.X
+        assert cmp[8] == Pauli.Y
+        assert cmp[9] == 1j * Pauli.Y
+        assert cmp[10] == -1 * Pauli.Y
+        assert cmp[11] == -1j * Pauli.Y
+        assert cmp[12] == Pauli.Z
+        assert cmp[13] == 1j * Pauli.Z
+        assert cmp[14] == -1 * Pauli.Z
+        assert cmp[15] == -1j * Pauli.Z
