95de334ee7fe70c5395108bc94e9078407211a8d
SS <66886825+EarlMilktea@users.noreply.github.com>
2024-11-01 15:55:09 +0900

    :technologist: Move tests to noxfile

--- noxfile.py
+++ noxfile.py
@@ -5,17 +5,28 @@ from __future__ import annotations
 import nox
 from nox import Session
 
+TESTING_LIBS = ["psutil", "pytest", "pytest-mock", "pytest-sugar"]
+
 
 @nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
-def tests_minimal(session: Session) -> None:
+def runtest_mini(session: Session) -> None:
     """Run the test suite with minimal dependencies."""
     session.install("-e", ".")
-    session.install("pytest", "pytest-mock", "psutil")
+    session.install(*TESTING_LIBS)
     session.run("pytest")
 
 
 @nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
-def tests(session: Session) -> None:
+def runtest_full(session: Session) -> None:
     """Run the test suite with full dependencies."""
     session.install("-e", ".[dev]")
+    session.install(*TESTING_LIBS)
     session.run("pytest")
+
+
+@nox.session()
+def runtest_cov(session: Session) -> None:
+    """Measure test coverage."""
+    session.install("-e", ".[dev]")
+    session.install(*TESTING_LIBS, "pytest-cov")
+    session.run("pytest", "--cov=./graphix", "--cov-report=xml", "--cov-report=term")
