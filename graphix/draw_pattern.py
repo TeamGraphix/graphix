af091ab82f10f96ca628e7272c1af8de94d561f3
benjvmin93 <benjamin.guichard@inria.fr>
2025-04-14 14:41:16 +0200

    remove pattern and add newly created draw_pattern file for mypy check

---
+++ graphix/draw_pattern.py
@@ -0,0 +1,83 @@
+from graphix import Pattern
+import PIL
+from pathlib import Path
+import io
+import subprocess
+
+def latex_file_to_image(tmpdirname: Path, tmpfilename: Path) -> PIL.Image.Image:
+    """Convert a latex file located in `tmpdirname/tmpfilename` to an image representation."""
+    try:
+        subprocess.run(
+            [
+                "pdflatex",
+                "-halt-on-error",
+                f"-output-directory={tmpdirname}",
+                f"{tmpfilename}.tex",
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+            check=True,
+        )
+    except OSError as exc:
+        # OSError should generally not occur, because it's usually only triggered if `pdflatex`
+        # doesn't exist as a command, but we've already checked that.
+        raise Exception("`pdflatex` command could not be run.") from exc
+    except subprocess.CalledProcessError as exc:
+        with Path("latex_error.log").open("wb") as error_file:
+            error_file.write(exc.stdout)
+        warnings.warn(
+            "Unable to compile LaTeX. Perhaps you are missing the `qcircuit` package."
+            " The output from the `pdflatex` command is in `latex_error.log`.",
+            stacklevel=2,
+        )
+        raise Exception("`pdflatex` call did not succeed: see `latex_error.log`.") from exc
+    base = Path(tmpdirname) / tmpfilename
+    try:
+        subprocess.run(
+            ["pdftocairo", "-singlefile", "-png", "-q", base.with_suffix(".pdf"), base],
+            check=True,
+        )
+    except (OSError, subprocess.CalledProcessError) as exc:
+        message = "`pdftocairo` failed to produce an image."
+        warnings.warn(message, stacklevel=2)
+        raise Exception(message) from exc
+    
+    def trim(image) -> PIL.Image.Image:
+        """Trim a PIL image and remove white space."""
+        background = PIL.Image.new(image.mode, image.size, image.getpixel((0, 0)))
+        diff = PIL.ImageChops.difference(image, background)
+        diff = PIL.ImageChops.add(diff, diff, 2.0, -100)
+        bbox = diff.getbbox()
+        if bbox:
+            image = image.crop(bbox)
+        return image
+
+    return trim(PIL.Image.open(base.with_suffix(".png")))
+
+def pattern_to_latex_document(pattern: Pattern, left_to_right: bool) -> str:
+        """Generate a latex document with the latex representation of the pattern written in it.
+
+        Parameters
+        ----------
+        left_to_right: bool
+            whether or not represent the pattern from left to right representation. Default is left to right, otherwise it's right to left
+        """
+        header_1 = r"\documentclass[border=2px]{standalone}" + "\n"
+
+        header_2 = r"""
+\usepackage{graphicx}
+
+\begin{document}
+"""
+
+        output = io.StringIO()
+        output.write(header_1)
+        output.write(header_2)
+
+        output.write(pattern.to_latex(left_to_right))
+
+        output.write("\n\\end{document}")
+        contents = output.getvalue()
+        output.close()
+
+        return contents
--- graphix/pattern.py
+++ graphix/pattern.py
@@ -214,59 +214,6 @@ class Pattern:
             and self.output_nodes == other.output_nodes
         )
 
-    def _latex_file_to_image(self, tmpdirname, tmpfilename) -> PIL.Image.Image:
-        """Convert a latex file located in `tmpdirname/tmpfilename` to an image representation."""
-        import PIL
-
-        try:
-            subprocess.run(
-                [
-                    "pdflatex",
-                    "-halt-on-error",
-                    f"-output-directory={tmpdirname}",
-                    f"{tmpfilename}.tex",
-                ],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.DEVNULL,
-                check=True,
-            )
-        except OSError as exc:
-            # OSError should generally not occur, because it's usually only triggered if `pdflatex`
-            # doesn't exist as a command, but we've already checked that.
-            raise Exception("`pdflatex` command could not be run.") from exc
-        except subprocess.CalledProcessError as exc:
-            with Path("latex_error.log").open("wb") as error_file:
-                error_file.write(exc.stdout)
-            warnings.warn(
-                "Unable to compile LaTeX. Perhaps you are missing the `qcircuit` package."
-                " The output from the `pdflatex` command is in `latex_error.log`.",
-                stacklevel=2,
-            )
-            raise Exception("`pdflatex` call did not succeed: see `latex_error.log`.") from exc
-
-        base = Path(tmpdirname) / tmpfilename
-        try:
-            subprocess.run(
-                ["pdftocairo", "-singlefile", "-png", "-q", base.with_suffix(".pdf"), base],
-                check=True,
-            )
-        except (OSError, subprocess.CalledProcessError) as exc:
-            message = "`pdftocairo` failed to produce an image."
-            warnings.warn(message, stacklevel=2)
-            raise Exception(message) from exc
-
-        def _trim(image) -> PIL.Image.Image:
-            """Trim a PIL image and remove white space."""
-            background = PIL.Image.new(image.mode, image.size, image.getpixel((0, 0)))
-            diff = PIL.ImageChops.difference(image, background)
-            diff = PIL.ImageChops.add(diff, diff, 2.0, -100)
-            bbox = diff.getbbox()
-            if bbox:
-                image = image.crop(bbox)
-            return image
-
-        return _trim(PIL.Image.open(base.with_suffix(".png")))
-
     def to_latex(self, left_to_right: bool = True) -> str:
         """Return a string containing the latex representation of the pattern.
 
@@ -285,34 +232,6 @@ class Pattern:
         output.close()
         return contents
 
-    def _to_latex_document(self, left_to_right: bool) -> str:
-        """Generate a latex document with the latex representation of the pattern written in it.
-
-        Parameters
-        ----------
-        left_to_right: bool
-            whether or not represent the pattern from left to right representation. Default is left to right, otherwise it's right to left
-        """
-        header_1 = r"\documentclass[border=2px]{standalone}" + "\n"
-
-        header_2 = r"""
-\usepackage{graphicx}
-
-\begin{document}
-"""
-
-        output = io.StringIO()
-        output.write(header_1)
-        output.write(header_2)
-
-        output.write(self.to_latex(left_to_right))
-
-        output.write("\n\\end{document}")
-        contents = output.getvalue()
-        output.close()
-
-        return contents
-
     def to_png(self, left_to_right: bool = True) -> PIL.Image.Image:
         """Generate a PNG image of the latex representation of the pattern.
 
@@ -321,6 +240,8 @@ class Pattern:
         left_to_right: bool
             whether or not represent the pattern from left to right representation. Default is left to right, otherwise it's right to left
         """
+        from graphix.draw_pattern import pattern_to_latex_document, latex_file_to_image
+
         tmpfilename = "pattern"
 
         with tempfile.TemporaryDirectory() as tmpdirname:
@@ -328,10 +249,10 @@ class Pattern:
             tmppath = tmppath.with_suffix(".tex")
 
             with tmppath.open("w") as latex_file:
-                contents = self._to_latex_document(left_to_right)
+                contents = pattern_to_latex_document(self, left_to_right)
                 latex_file.write(contents)
 
-            return self._latex_file_to_image(tmpdirname, tmpfilename)
+            return latex_file_to_image(tmpdirname, tmpfilename)
 
     def __str__(self) -> str:
         """Return a string representation of the pattern."""
