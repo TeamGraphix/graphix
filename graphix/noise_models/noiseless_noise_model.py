17bca10648b2311324f2ec952bd9e1f74ddd9522
Thierry Martinez <thierry.martinez@inria.fr>
2025-04-22 19:01:36 +0200

    Use typing_extensions.override

--- graphix/noise_models/noiseless_noise_model.py
+++ graphix/noise_models/noiseless_noise_model.py
@@ -4,6 +4,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import typing_extensions
+
 from graphix.noise_models.noise_model import CommandOrNoise, NoiseCommands, NoiseModel
 
 if TYPE_CHECKING:
@@ -18,14 +20,17 @@ class NoiselessNoiseModel(NoiseModel):
     Only return the identity channel.
     """
 
+    @typing_extensions.override
     def input_nodes(self, nodes: Iterable[int]) -> NoiseCommands:
         """Return the noise to apply to input nodes."""
         return []
 
+    @typing_extensions.override
     def command(self, cmd: CommandOrNoise) -> NoiseCommands:
         """Return the noise to apply to the command `cmd`."""
         return [cmd]
 
+    @typing_extensions.override
     def confuse_result(self, cmd: BaseM, result: bool) -> bool:
         """Assign wrong measurement result."""
         return result
