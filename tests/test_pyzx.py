104623fd9722871f54bab0010c921e163329e6b8
Thierry Martinez <thierry.martinez@inria.fr>
2025-05-19 15:16:02 +0200

    Add test random circuit

--- tests/test_pyzx.py
+++ tests/test_pyzx.py
@@ -6,6 +6,11 @@ from copy import deepcopy
 from typing import TYPE_CHECKING
 
 import pytest
+import numpy as np
+from numpy.random import PCG64, Generator
+
+from graphix.opengraph import OpenGraph
+from graphix.random_objects import rand_circuit
 
 if TYPE_CHECKING:
     from pyzx.graph.base import BaseGraph
@@ -67,3 +72,25 @@ def test_random_clifford_t() -> None:
     for _ in range(15):
         g = clifford_t(4, 10, 0.1)
         assert_reconstructed_pyzx_graph_equal(g)
+
+
+@pytest.mark.skipif(_pyzx_notfound(), reason="pyzx not installed")
+@pytest.mark.parametrize("jumps", range(1, 11))
+def test_random_circuit(fx_bg: PCG64, jumps: int) -> None:
+    from graphix.pyzx import from_pyzx_graph, to_pyzx_graph
+    rng = Generator(fx_bg.jumped(jumps))
+    nqubits = 5
+    depth = 5
+    circuit = rand_circuit(nqubits, depth, rng)
+    pattern = circuit.transpile().pattern
+    opengraph = OpenGraph.from_pattern(pattern)
+    zx_graph = to_pyzx_graph(opengraph)
+    opengraph2 = from_pyzx_graph(zx_graph)
+    pattern2 = opengraph2.to_pattern()
+    pattern.perform_pauli_measurements()
+    pattern.minimize_space()
+    state = pattern.simulate_pattern()
+    pattern2.perform_pauli_measurements()
+    pattern2.minimize_space()
+    state2 = pattern2.simulate_pattern()
+    assert np.abs(np.dot(state.flatten().conjugate(), state2.flatten())) == pytest.approx(1)
