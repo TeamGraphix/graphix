fa1e64f8a6e53e43b32de030213bf8fed9f313e3
Thierry Martinez <thierry.martinez@inria.fr>
2025-07-17 23:22:19 +0200

    Fix --doctest-modules

--- noxfile.py
+++ noxfile.py
@@ -13,26 +13,22 @@ def install_pytest(session: Session) -> None:
     session.install("pytest", "pytest-mock", "psutil")
 
 
-def run_pytest(session: Session) -> None:
-    """Run pytest."""
-    session.run("pytest", "--doctest-modules")
-
-
 @nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
 def tests_minimal(session: Session) -> None:
     """Run the test suite with minimal dependencies."""
     session.install("-e", ".")
     install_pytest(session)
-    run_pytest(session)
+    # We cannot run `pytest --doctest-modules` here, since some tests
+    # involve optional dependencies, like pyzx.
+    session.run("pytest")
 
-    # Note that recent types-networkx versions don't support Python 3.9
 
-
+# Note that recent types-networkx versions don't support Python 3.9
 @nox.session(python=["3.10", "3.11", "3.12", "3.13"])
 def tests_dev(session: Session) -> None:
     """Run the test suite with dev dependencies."""
     session.install("-e", ".[dev]")
-    run_pytest(session)
+    session.run("pytest", "--doctest-modules")
 
 
 @nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
@@ -40,14 +36,15 @@ def tests_extra(session: Session) -> None:
     """Run the test suite with extra dependencies."""
     session.install("-e", ".[extra]")
     install_pytest(session)
-    run_pytest(session)
+    session.install("nox")  # needed for --doctest-modules
+    session.run("pytest", "--doctest-modules")
 
 
 @nox.session(python=["3.10", "3.11", "3.12", "3.13"])
 def tests_all(session: Session) -> None:
     """Run the test suite with all dependencies."""
     session.install("-e", ".[dev,extra]")
-    run_pytest(session)
+    session.run("pytest", "--doctest-modules")
 
 
 # TODO: Add 3.13 CI
@@ -56,6 +53,7 @@ def tests_symbolic(session: Session) -> None:
     """Run the test suite of graphix-symbolic."""
     session.install("-e", ".")
     install_pytest(session)
+    session.install("nox")  # needed for --doctest-modules
     # Use `session.cd` as a context manager to ensure that the
     # working directory is restored afterward. This is important
     # because Windows cannot delete a temporary directory while it
@@ -65,4 +63,4 @@ def tests_symbolic(session: Session) -> None:
         # session.run("git", "clone", "-b", "branch-name", "https://github.com/TeamGraphix/graphix-symbolic")
         session.run("git", "clone", "https://github.com/TeamGraphix/graphix-symbolic")
         with session.cd("graphix-symbolic"):
-            run_pytest(session)
+            session.run("pytest", "--doctest-modules")
