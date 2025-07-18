9ce3cad49ad576c661b7f8f49896846f8177cf79
thierry-martinez <thierry.martinez@inria.fr>
2025-07-18 13:01:21 +0200

    Fix #316: pin package version for `mypy` and `types-networkx` (#318)
    This commit pins the package version for `mypy==1.17.0` and
    `types-networkx==3.5.0.20250701`. It's a continuation of PR #243 where
    we pinned the version of `ruff`.
    
    Pinning these package should make the PR review process smoother.
    
    - In #315, CI broke due to a regression introduced in `mypy==1.17`,
      even though the PR was only about updating `ruff`.
    
    - In #309, CI broke due to stricter typing in newer versions of
      `types-networkx`, which required changes in parts of the code not
      touched by the PR.
    
    By pinning these packages and relying on Dependabot to open dedicated
    PRs for updates:
    
    - We avoid unpredicatable CI failures caused by unexpected version
      updates.
    
    - We prevent unrelated changes from being added to PRs just because a
      package update occurred during the review process.
    
    - We get dedicated PRs to manage and test updates.
    
    - We are notified when updates break the CI, even if no new code has
      been pushed since the update was released.
    
    The file `noxfile.yml` is updated to remove Python 3.9 from tested versions for dev, since last versions of `types-networkx` are incompatible with Python 3.9.
    
    The dependencies in `requirements-extra.txt` are now also tested independently of `requirements-dev.txt`.

--- noxfile.py
+++ noxfile.py
@@ -8,17 +8,43 @@ import nox
 from nox import Session
 
 
+def install_pytest(session: Session) -> None:
+    """Install pytest when requirements-dev.txt is not installed."""
+    session.install("pytest", "pytest-mock", "psutil")
+
+
 @nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
 def tests_minimal(session: Session) -> None:
     """Run the test suite with minimal dependencies."""
     session.install("-e", ".")
-    session.install("pytest", "pytest-mock", "psutil")
+    install_pytest(session)
+    # We cannot run `pytest --doctest-modules` here, since some tests
+    # involve optional dependencies, like pyzx.
+    session.run("pytest")
+
+
+# Note that recent types-networkx versions don't support Python 3.9
+@nox.session(python=["3.10", "3.11", "3.12", "3.13"])
+def tests_dev(session: Session) -> None:
+    """Run the test suite with dev dependencies."""
+    session.install("-e", ".[dev]")
+    # We cannot run `pytest --doctest-modules` here, since some tests
+    # involve optional dependencies, like pyzx.
     session.run("pytest")
 
 
 @nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
-def tests(session: Session) -> None:
-    """Run the test suite with full dependencies."""
+def tests_extra(session: Session) -> None:
+    """Run the test suite with extra dependencies."""
+    session.install("-e", ".[extra]")
+    install_pytest(session)
+    session.install("nox")  # needed for `--doctest-modules`
+    session.run("pytest", "--doctest-modules")
+
+
+@nox.session(python=["3.10", "3.11", "3.12", "3.13"])
+def tests_all(session: Session) -> None:
+    """Run the test suite with all dependencies."""
     session.install("-e", ".[dev,extra]")
     session.run("pytest", "--doctest-modules")
 
@@ -27,7 +53,9 @@ def tests(session: Session) -> None:
 @nox.session(python=["3.9", "3.10", "3.11", "3.12"])
 def tests_symbolic(session: Session) -> None:
     """Run the test suite of graphix-symbolic."""
-    session.install("-e", ".[dev]")
+    session.install("-e", ".")
+    install_pytest(session)
+    session.install("nox")  # needed for `--doctest-modules`
     # Use `session.cd` as a context manager to ensure that the
     # working directory is restored afterward. This is important
     # because Windows cannot delete a temporary directory while it
@@ -37,4 +65,4 @@ def tests_symbolic(session: Session) -> None:
         # session.run("git", "clone", "-b", "branch-name", "https://github.com/TeamGraphix/graphix-symbolic")
         session.run("git", "clone", "https://github.com/TeamGraphix/graphix-symbolic")
         with session.cd("graphix-symbolic"):
-            session.run("pytest")
+            session.run("pytest", "--doctest-modules")
