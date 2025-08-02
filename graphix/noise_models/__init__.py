bf9f11adfe40503f48e7e18ca080ec15f241f909
Thierry Martinez <thierry.martinez@inria.fr>
2025-08-02 20:38:45 +0200

    Add tests for noise models

--- graphix/noise_models/__init__.py
+++ graphix/noise_models/__init__.py
@@ -2,14 +2,24 @@
 
 from __future__ import annotations
 
-from graphix.noise_models.depolarising import DepolarisingNoise, DepolarisingNoiseModel
-from graphix.noise_models.noise_model import ComposeNoiseModel, Noise, NoiselessNoiseModel, NoiseModel
+from graphix.noise_models.depolarising import DepolarisingNoise, DepolarisingNoiseModel, TwoQubitDepolarisingNoise
+from graphix.noise_models.noise_model import (
+    A,
+    CommandOrNoise,
+    ComposeNoiseModel,
+    Noise,
+    NoiselessNoiseModel,
+    NoiseModel,
+)
 
 __all__ = [
+    "A",
+    "CommandOrNoise",
     "ComposeNoiseModel",
     "DepolarisingNoise",
     "DepolarisingNoiseModel",
     "Noise",
     "NoiseModel",
     "NoiselessNoiseModel",
+    "TwoQubitDepolarisingNoise",
 ]
