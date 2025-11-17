TeamGraphix/graphix

commit 3758aec8f639b8d97d36939ce22e8a9660bb36a0

Enable type-checking for `pattern.py` and `test_pattern.py` (#302)
This commit adds type annotations to the pattern module and its
corresponding tests, enabling full type-checking with `mypy` while
preserving existing functionality.

This lays the foundations for safer and more ambitious refactoring in
the future.

pyproject.toml
@@ -144,7 +144,6 @@ exclude = [
   '^graphix/device_interface\.py$',
   '^graphix/gflow\.py$',
   '^graphix/linalg\.py$',
-  '^graphix/pattern\.py$',
   '^graphix/random_objects\.py$',
   '^graphix/simulator\.py$',
   '^graphix/sim/base_backend\.py$',
@@ -157,7 +156,6 @@ exclude = [
   '^tests/test_gflow\.py$',
   '^tests/test_linalg\.py$',
   '^tests/test_noisy_density_matrix\.py$',
-  '^tests/test_pattern\.py$',
   '^tests/test_random_utilities\.py$',
   '^tests/test_runner\.py$',
   '^tests/test_statevec\.py$',
@@ -186,7 +184,6 @@ exclude = [
   "graphix/device_interface.py",
   "graphix/gflow.py",
   "graphix/linalg.py",
-  "graphix/pattern.py",
   "graphix/random_objects.py",
   "graphix/simulator.py",
   "graphix/sim/base_backend.py",
@@ -199,7 +196,6 @@ exclude = [
   "tests/test_gflow.py",
   "tests/test_linalg.py",
   "tests/test_noisy_density_matrix.py",
-  "tests/test_pattern.py",
   "tests/test_random_utilities.py",
   "tests/test_runner.py",
   "tests/test_statevec.py",

graphix/generator.py
@@ -16,10 +16,12 @@ if TYPE_CHECKING:
     import numpy as np
     import numpy.typing as npt
 
+    from graphix.parameter import ExpressionOrFloat
+
 
 def generate_from_graph(
     graph: nx.Graph[int],
-    angles: Mapping[int, float] | Sequence[float] | npt.NDArray[np.float64],
+    angles: Mapping[int, ExpressionOrFloat] | Sequence[ExpressionOrFloat] | npt.NDArray[np.float64],
     inputs: Iterable[int],
     outputs: Iterable[int],
     meas_planes: Mapping[int, Plane] | None = None,

graphix/gflow.py
@@ -39,7 +39,7 @@ def check_meas_planes(meas_planes: dict[int, Plane]) -> None:
 
 
 def find_gflow(
-    graph: nx.Graph,
+    graph: nx.Graph[int],
     iset: set[int],
     oset: set[int],
     meas_planes: dict[int, Plane],

graphix/measurements.py
@@ -28,7 +28,7 @@ class Measurement(NamedTuple):
     :param plane: the measurement plane
     """
 
-    angle: float
+    angle: ExpressionOrFloat
     plane: Plane
 
     def isclose(self, other: Measurement, rel_tol: float = 1e-09, abs_tol: float = 0.0) -> bool:
@@ -45,7 +45,11 @@ class Measurement(NamedTuple):
         >>> Measurement(0.1, Plane.XY).isclose(Measurement(0.0, Plane.XY))
         False
         """
-        return math.isclose(self.angle, other.angle, rel_tol=rel_tol, abs_tol=abs_tol) and self.plane == other.plane
+        return (
+            math.isclose(self.angle, other.angle, rel_tol=rel_tol, abs_tol=abs_tol)
+            if isinstance(self.angle, float) and isinstance(other.angle, float)
+            else self.angle == other.angle
+        ) and self.plane == other.plane
 
 
 class PauliMeasurement(NamedTuple):

graphix/parameter.py
@@ -335,7 +335,7 @@ T = TypeVar("T")
 
 
 @overload
-def subs(value: Expression, variable: Parameter, substitute: ExpressionOrSupportsFloat) -> Expression: ...
+def subs(value: ExpressionOrFloat, variable: Parameter, substitute: ExpressionOrSupportsFloat) -> ExpressionOrFloat: ...
 
 
 @overload
@@ -370,6 +370,16 @@ def subs(value: T, variable: Parameter, substitute: ExpressionOrSupportsFloat) -
             return c.real
         return c
     return new_value
+
+
+@overload
+def xreplace(
+    value: ExpressionOrFloat, assignment: Mapping[Parameter, ExpressionOrSupportsFloat]
+) -> ExpressionOrFloat: ...
+
+
+@overload
+def xreplace(value: T, assignment: Mapping[Parameter, ExpressionOrSupportsFloat]) -> T | Expression | complex: ...
 
 
 # The return type could be `T | Expression | complex` since `subs` returns `Expression` only

graphix/pyzx.py
@@ -22,6 +22,14 @@ from graphix.opengraph import OpenGraph
 if TYPE_CHECKING:
     from pyzx.graph.base import BaseGraph
 
+    from graphix.parameter import ExpressionOrFloat
+
+
+def _fraction_of_angle(angle: ExpressionOrFloat) -> Fraction:
+    if not isinstance(angle, SupportsFloat):
+        raise TypeError("Parametric angles are not supported by pyzx")
+    return Fraction(angle)
+
 
 def to_pyzx_graph(og: OpenGraph) -> BaseGraph[int, tuple[int, int]]:
     """Return a :mod:`pyzx` graph corresponding to the open graph.
@@ -95,12 +103,12 @@ def to_pyzx_graph(og: OpenGraph) -> BaseGraph[int, tuple[int, int]]:
     for og_index, meas in og.measurements.items():
         # If it's an X measured node, then we handle it in the next loop
         if meas.plane == Plane.XY:
-            g.set_phase(map_to_pyzx[og_index], Fraction(-meas.angle))
+            g.set_phase(map_to_pyzx[og_index], -_fraction_of_angle(meas.angle))
 
     # Connect the X measured vertices
     for og_index, pyzx_index in zip(x_meas, x_meas_verts):
         g.add_edge((map_to_pyzx[og_index], pyzx_index), EdgeType.HADAMARD)
-        g.set_phase(pyzx_index, Fraction(-og.measurements[og_index].angle))
+        g.set_phase(pyzx_index, -_fraction_of_angle(og.measurements[og_index].angle))
 
     return g
 

graphix/sim/density_matrix.py
@@ -203,7 +203,7 @@ class DensityMatrix(State):
 
         return np.trace(rho_tensor.reshape((2**self.nqubit, 2**self.nqubit)))
 
-    def dims(self) -> list[int]:
+    def dims(self) -> tuple[int, ...]:
         """Return the dimensions of the density matrix."""
         return self.rho.shape
 

commit 305db1f1b8fb9a13d6051e20ff1288226a11b488

Enable type-checking for `random_objects.py`
This commit adds type annotations to `random_objects.py`, along with
its corresponding tests, enabling full type-checking with mypy while
preserving existing functionality.

The exclusion of `device_interface.py` is removed from
`pyproject.toml` since this module was removed in #261.

`scipy-stubs` is added to `requirements-dev.txt` since `scipy` is used
in `random_objects.py`.

**Related issue:**
This PR continues the work started in #302, #308, #312 and #347.

graphix/random_objects.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import functools
+import math
 from typing import TYPE_CHECKING
 
 import numpy as np
@@ -16,21 +17,24 @@ from graphix.rng import ensure_rng
 from graphix.transpiler import Circuit
 
 if TYPE_CHECKING:
-    from collections.abc import Iterable, Iterator
+    from collections.abc import Callable, Iterable, Iterator
+    from typing import Literal, TypeAlias
 
     from numpy.random import Generator
 
     from graphix.parameter import Parameter
 
+    IntLike: TypeAlias = int | np.integer
 
-def rand_herm(sz: int, rng: Generator | None = None) -> npt.NDArray[np.complex128]:
+
+def rand_herm(sz: IntLike, rng: Generator | None = None) -> npt.NDArray[np.complex128]:
     """Generate random hermitian matrix of size sz*sz."""
     rng = ensure_rng(rng)
     tmp = rng.random(size=(sz, sz)) + 1j * rng.random(size=(sz, sz))
     return tmp + tmp.conj().T
 
 
-def rand_unit(sz: int, rng: Generator | None = None) -> npt.NDArray[np.complex128]:
+def rand_unit(sz: IntLike, rng: Generator | None = None) -> npt.NDArray[np.float64]:
     """Generate haar random unitary matrix of size sz*sz."""
     rng = ensure_rng(rng)
     if sz == 1:
@@ -41,7 +45,7 @@ def rand_unit(sz: int, rng: Generator | None = None) -> npt.NDArray[np.complex12
 UNITS = np.array([1, 1j])
 
 
-def rand_dm(dim: int, rng: Generator | None = None, rank: int | None = None) -> npt.NDArray[np.complex128]:
+def rand_dm(dim: IntLike, rng: Generator | None = None, rank: IntLike | None = None) -> npt.NDArray[np.float64]:
     """Generate random density matrices (positive semi-definite matrices with unit trace).
 
     Returns either a :class:`graphix.sim.density_matrix.DensityMatrix` or a :class:`np.ndarray` depending on the parameter *dm_dtype*.
@@ -73,9 +77,20 @@ def rand_dm(dim: int, rng: Generator | None = None, rank: int | None = None) -> 
     return rand_u @ dm @ rand_u.transpose().conj()
 
 
-def rand_gauss_cpx_mat(
-    dim: int, rng: Generator | None = None, sig: float = 1 / np.sqrt(2)
-) -> npt.NDArray[np.complex128]:
+if TYPE_CHECKING:
+    _SIG: TypeAlias = float | Literal["ginibre"] | None
+
+
+def _make_sig(sig: _SIG, dim: IntLike) -> float:
+    if sig is None:
+        # B008 Do not perform function call in argument defaults
+        return 1 / math.sqrt(2)
+    if sig == "ginibre":
+        return 1 / math.sqrt(2 * dim)
+    return sig
+
+
+def rand_gauss_cpx_mat(dim: IntLike, rng: Generator | None = None, sig: _SIG = None) -> npt.NDArray[np.complex128]:
     """Return a square array of standard normal complex random variates.
 
     Code from QuTiP: https://qutip.org/docs/4.0.2/modules/qutip/random_objects.html
@@ -91,14 +106,14 @@ def rand_gauss_cpx_mat(
     """
     rng = ensure_rng(rng)
 
-    if sig == "ginibre":
-        sig = 1.0 / np.sqrt(2 * dim)
-
-    return np.sum(rng.normal(loc=0.0, scale=sig, size=((dim,) * 2 + (2,))) * UNITS, axis=-1)
+    result: npt.NDArray[np.complex128] = np.sum(
+        rng.normal(loc=0.0, scale=_make_sig(sig, dim), size=((dim,) * 2 + (2,))) * UNITS, axis=-1
+    )
+    return result
 
 
 def rand_channel_kraus(
-    dim: int, rng: Generator | None = None, rank: int | None = None, sig: float = 1 / np.sqrt(2)
+    dim: int, rng: Generator | None = None, rank: int | None = None, sig: _SIG = None
 ) -> KrausChannel:
     """Return a random :class:`graphix.sim.channels.KrausChannel` object of given dimension and rank.
 
@@ -122,16 +137,13 @@ def rand_channel_kraus(
     if rank is None:
         rank = dim**2
 
-    if sig == "ginibre":
-        sig = 1.0 / np.sqrt(2 * dim)
-
     if not isinstance(rank, int):
         raise TypeError("The rank of a Kraus expansion must be an integer.")
 
     if not rank >= 1:
         raise ValueError("The rank of a Kraus expansion must be greater or equal than 1.")
 
-    pre_kraus_list = [rand_gauss_cpx_mat(dim=dim, sig=sig) for _ in range(rank)]
+    pre_kraus_list = [rand_gauss_cpx_mat(dim=dim, sig=_make_sig(sig, dim)) for _ in range(rank)]
     h_mat = np.sum([m.transpose().conjugate() @ m for m in pre_kraus_list], axis=0)
     kraus_list = np.array(pre_kraus_list) @ scipy.linalg.inv(scipy.linalg.sqrtm(h_mat))
 
@@ -397,7 +409,7 @@ def rand_circuit(
         for rotation in (circuit.rx, circuit.ry, circuit.rz)
         for parameter in parameters or []
     )
-    gate_choice = (
+    gate_choice: tuple[Callable[[int], None], ...] = (
         functools.partial(circuit.ry, angle=np.pi / 4),
         functools.partial(circuit.rz, angle=-np.pi / 4),
         functools.partial(circuit.rx, angle=-np.pi / 4),

pyproject.toml
@@ -143,16 +143,12 @@ exclude = [
   '^examples/qnn\.py$',
   '^examples/rotation\.py$',
   '^examples/tn_simulation\.py$',
-  '^graphix/device_interface\.py$',
   '^graphix/gflow\.py$',
   '^graphix/linalg\.py$',
-  '^graphix/random_objects\.py$',
   '^tests/test_density_matrix\.py$',
   '^tests/test_gflow\.py$',
   '^tests/test_linalg\.py$',
   '^tests/test_noisy_density_matrix\.py$',
-  '^tests/test_random_utilities\.py$',
-  '^tests/test_runner\.py$',
   '^tests/test_statevec\.py$',
   '^tests/test_statevec_backend\.py$',
   '^tests/test_tnsim\.py$',
@@ -174,16 +170,12 @@ exclude = [
   "examples/qnn.py",
   "examples/rotation.py",
   "examples/tn_simulation.py",
-  "graphix/device_interface.py",
   "graphix/gflow.py",
   "graphix/linalg.py",
-  "graphix/random_objects.py",
   "tests/test_density_matrix.py",
   "tests/test_gflow.py",
   "tests/test_linalg.py",
   "tests/test_noisy_density_matrix.py",
-  "tests/test_random_utilities.py",
-  "tests/test_runner.py",
   "tests/test_statevec.py",
   "tests/test_statevec_backend.py",
   "tests/test_tnsim.py",

tests/test_random_utilities.py
@@ -23,7 +23,7 @@ class TestUtilities:
     # TODO : work on that. Verify an a random vector and not at the operator level...
 
     def test_rand_unit(self, fx_rng: Generator) -> None:
-        d = fx_rng.integers(2, 20)
+        d = fx_rng.integers(2, 20).astype(int)
         tmp = randobj.rand_unit(d, fx_rng)
         print(type(tmp), tmp.dtype)
 
@@ -52,10 +52,6 @@ class TestUtilities:
         assert len(channel) == rk
 
     def test_random_channel_fail(self, fx_rng: Generator) -> None:
-        # incorrect rank type
-        with pytest.raises(TypeError):
-            _ = randobj.rand_channel_kraus(dim=2**2, rank=3.0, rng=fx_rng)
-
         # null rank
         with pytest.raises(ValueError):
             _ = randobj.rand_channel_kraus(dim=2**2, rank=0, rng=fx_rng)
@@ -132,7 +128,7 @@ class TestUtilities:
         assert lv.is_psd(dm.rho)
         assert lv.is_unit_trace(dm.rho)
 
-        evals = np.linalg.eigvalsh(dm.rho)
+        evals = np.linalg.eigvalsh(dm.rho.astype(np.complex128))
 
         evals[np.abs(evals) < 1e-15] = 0
 
@@ -149,10 +145,7 @@ class TestUtilities:
         # or np.apply_along_axis ?
         assert np.all(dims == (2**nqb, 2**nqb))
 
-    def test_pauli_tensor_ops_fail(self, fx_rng: Generator) -> None:
-        with pytest.raises(TypeError):
-            _ = Ops.build_tensor_pauli_ops(fx_rng.integers(2, 6) + 0.5)
-
+    def test_pauli_tensor_ops_fail(self) -> None:
         with pytest.raises(ValueError):
             _ = Ops.build_tensor_pauli_ops(0)
 
@@ -168,12 +161,6 @@ class TestUtilities:
     def test_random_pauli_channel_fail(self, fx_rng: Generator) -> None:
         nqb = 3
         rk = 2
-        with pytest.raises(TypeError):
-            randobj.rand_pauli_channel_kraus(dim=2**nqb, rank=rk + 0.5, rng=fx_rng)
-
-        with pytest.raises(TypeError):
-            randobj.rand_pauli_channel_kraus(dim=2**nqb + 0.5, rank=rk, rng=fx_rng)
-
         with pytest.raises(ValueError):
             randobj.rand_pauli_channel_kraus(dim=2**nqb, rank=-3, rng=fx_rng)
 