85500fb49baaddc3e9dc4cf22196263db66fe324
Emlyn Graham <egraham@PTB-02009984.paris.inria.fr>
2025-11-28 17:42:14 +0100

    fixed mypy checks

--- tests/test_qasm3_exporter.py
+++ tests/test_qasm3_exporter.py
@@ -17,7 +17,7 @@ if TYPE_CHECKING:
     from graphix.instruction import Instruction
 
 try:
-    from graphix_qasm_parser import OpenQASMParser
+    from graphix_qasm_parser import OpenQASMParser  # type: ignore[import-not-found, unused-ignore]
 except ImportError:
     pytestmark = pytest.mark.skip(reason="graphix-qasm-parser not installed")
 
