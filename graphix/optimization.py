7f3038068b38686e8321eddec8ae8a77b550ecf2
matulni <m.uldemolins@gmail.com>
2025-12-11 11:47:26 +0100

    wip

--- graphix/optimization.py
+++ graphix/optimization.py
@@ -550,43 +550,42 @@ class StandardizedPattern(_StandardizedPattern):
         return gf
 
     def extract_xzcorrections(self) -> XZCorrections[Measurement]:
-        nodes = set(self.input_nodes)
-        edges: set[tuple[int, int]] = set()
-        measurements: dict[int, Measurement] = {}
+        """Extract the XZ-corrections from the current measurement pattern.
+
+        Returns
+        -------
+        XZCorrections[Measurement]
+            The XZ-corrections associated with the current pattern.
+
+        Raises
+        ------
+        XZCorrectionsError
+            If the extracted correction dictionaries are not well formed.
+        ValueError
+            If `N` commands in the pattern do not represent a |+⟩ state or if the pattern corrections form closed loops.
+        """
         x_corr: dict[int, set[int]] = defaultdict(set)
         z_corr: dict[int, set[int]] = defaultdict(set)
 
         pre_measured_nodes = set(self.results.keys())  # Not included in the xz-corrections.
 
-        for n in self.n_list:
-            if n.state != BasicStates.PLUS:
-                raise ValueError(
-                    f"Open graph construction in flow extraction requires N commands to represent a |+⟩ state. Error found in {n}."
-                )
-
-        def update_corrections(
-            node: int, domain: AbstractSet[int], corrections: dict[int, set[int]]
-        ) -> dict[int, set[int]]:
-            for measured_node in domain:
-                corrections[measured_node].add(node)
-            return corrections
-
         for m in self.m_list:
-            measurements[m.node] = Measurement(m.angle, m.plane)
-            x_corr = update_corrections(m.node, m.s_domain - pre_measured_nodes, x_corr)
-            z_corr = update_corrections(m.node, m.t_domain - pre_measured_nodes, z_corr)
+            x_corr = _update_corrections(m.node, m.s_domain - pre_measured_nodes, x_corr)
+            z_corr = _update_corrections(m.node, m.t_domain - pre_measured_nodes, z_corr)
 
         for node, domain in self.x_dict.items():
-            x_corr = update_corrections(node, domain - pre_measured_nodes, x_corr)
+            x_corr = _update_corrections(node, domain - pre_measured_nodes, x_corr)
 
         for node, domain in self.z_dict.items():
-            z_corr = update_corrections(node, domain - pre_measured_nodes, z_corr)
+            z_corr = _update_corrections(node, domain - pre_measured_nodes, z_corr)
 
-        graph = nx.Graph(edges)
-        graph.add_nodes_from(nodes)
-        og = OpenGraph(graph, self.input_nodes, self.output_nodes, measurements)
+        og = (
+            self.extract_opengraph()
+        )  # Raises a `ValueError` if `N` commands in the pattern do not represent a |+⟩ state.
 
-        return XZCorrections.from_measured_nodes_mapping(og, dict(x_corr), dict(z_corr))
+        return XZCorrections.from_measured_nodes_mapping(
+            og, dict(x_corr), dict(z_corr)
+        )  # Raises a `XZCorrectionsError` if the input dictionaries are not well formed.
 
 
 def _add_correction_domain(domain_dict: dict[Node, set[Node]], node: Node, domain: set[Node]) -> None:
