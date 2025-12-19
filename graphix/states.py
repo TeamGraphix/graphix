Fix #233: consistent convention, angles expressed in units of π
This commit makes the angle convention consistent across the library:
angles are represented as floats and expressed in units of π. In
particular, angles that appear in parameters of circuit instructions
are now expressed in units of π.

graphix/states.py
@@ -11,7 +11,7 @@ import numpy as np
 import numpy.typing as npt
 import typing_extensions
 
-from graphix.fundamentals import Plane
+from graphix.fundamentals import ANGLE_PI, Angle, Plane, rad_of_angle
 
 
 # generic class State for all States
@@ -45,14 +45,14 @@ class PlanarState(State):
 
     :param plane: One of the three planes (XY, XZ, YZ)
     :type plane: :class:`graphix.pauli.Plane`
-    :param angle: angle IN RADIANS
+    :param angle: Angle (in units of π)
     :type angle: float
     :return: State
     :rtype: :class:`graphix.states.State` object
     """
 
     plane: Plane
-    angle: float
+    angle: Angle
 
     def __repr__(self) -> str:
         """Return a string representation of the planar state."""
@@ -65,13 +65,17 @@ class PlanarState(State):
     def get_statevector(self) -> npt.NDArray[np.complex128]:
         """Return the state vector."""
         if self.plane == Plane.XY:
-            return np.asarray([1 / np.sqrt(2), np.exp(1j * self.angle) / np.sqrt(2)], dtype=np.complex128)
+            return np.asarray([1 / np.sqrt(2), np.exp(1j * rad_of_angle(self.angle)) / np.sqrt(2)], dtype=np.complex128)
 
         if self.plane == Plane.YZ:
-            return np.asarray([np.cos(self.angle / 2), 1j * np.sin(self.angle / 2)], dtype=np.complex128)
+            return np.asarray(
+                [np.cos(rad_of_angle(self.angle) / 2), 1j * np.sin(rad_of_angle(self.angle) / 2)], dtype=np.complex128
+            )
 
         if self.plane == Plane.XZ:
-            return np.asarray([np.cos(self.angle / 2), np.sin(self.angle / 2)], dtype=np.complex128)
+            return np.asarray(
+                [np.cos(rad_of_angle(self.angle) / 2), np.sin(rad_of_angle(self.angle) / 2)], dtype=np.complex128
+            )
         # other case never happens since exhaustive
         typing_extensions.assert_never(self.plane)
 
@@ -81,11 +85,11 @@ class BasicStates:
     """Basic states."""
 
     ZERO: ClassVar[PlanarState] = PlanarState(Plane.XZ, 0)
-    ONE: ClassVar[PlanarState] = PlanarState(Plane.XZ, np.pi)
+    ONE: ClassVar[PlanarState] = PlanarState(Plane.XZ, ANGLE_PI)
     PLUS: ClassVar[PlanarState] = PlanarState(Plane.XY, 0)
-    MINUS: ClassVar[PlanarState] = PlanarState(Plane.XY, np.pi)
-    PLUS_I: ClassVar[PlanarState] = PlanarState(Plane.XY, np.pi / 2)
-    MINUS_I: ClassVar[PlanarState] = PlanarState(Plane.XY, -np.pi / 2)
+    MINUS: ClassVar[PlanarState] = PlanarState(Plane.XY, ANGLE_PI)
+    PLUS_I: ClassVar[PlanarState] = PlanarState(Plane.XY, ANGLE_PI / 2)
+    MINUS_I: ClassVar[PlanarState] = PlanarState(Plane.XY, -ANGLE_PI / 2)
     # remove that in the end
     # need in TN backend
     VEC: ClassVar[list[PlanarState]] = [PLUS, MINUS, ZERO, ONE, PLUS_I, MINUS_I]

tests/test_optimization.py
@@ -6,7 +6,7 @@ from numpy.random import PCG64, Generator
 
 from graphix.clifford import Clifford
 from graphix.command import C, Command, CommandKind, E, M, N, X, Z
-from graphix.fundamentals import Plane
+from graphix.fundamentals import ANGLE_PI, Plane
 from graphix.gflow import gflow_from_pattern
 from graphix.optimization import StandardizedPattern, incorporate_pauli_results, remove_useless_domains
 from graphix.pattern import Pattern
@@ -15,7 +15,7 @@ from graphix.states import PlanarState
 
 
 def test_standardize_clifford_entanglement(fx_rng: Generator) -> None:
-    alpha = 2 * np.pi * fx_rng.random()
+    alpha = 2 * ANGLE_PI * fx_rng.random()
     i_lst = [0]
     o_lst = [0, 1]
 