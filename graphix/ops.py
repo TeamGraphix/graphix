Fix #233: consistent convention, angles expressed in units of π (#399)
This commit makes the angle convention consistent across the library:
angles are represented as floats and expressed in units of π. In
particular, angles that appear in parameters of circuit instructions
are now expressed in units of π.

graphix/ops.py
@@ -9,13 +9,18 @@ from typing import TYPE_CHECKING, ClassVar, overload
 import numpy as np
 import numpy.typing as npt
 
+# assert_never added in Python 3.11
+from typing_extensions import assert_never
+
 from graphix import utils
+from graphix.fundamentals import IXYZ, Axis, I, angle_to_rad
 from graphix.parameter import Expression, cos_sin, exp
 
 if TYPE_CHECKING:
     from collections.abc import Iterable
 
-    from graphix.parameter import ExpressionOrComplex, ExpressionOrFloat
+    from graphix.fundamentals import Angle, ParameterizedAngle
+    from graphix.parameter import ExpressionOrComplex
 
 
 class Ops:
@@ -75,17 +80,17 @@ class Ops:
 
     @overload
     @staticmethod
-    def _cast_array(array: Iterable[Iterable[complex]], theta: float) -> npt.NDArray[np.complex128]: ...
+    def _cast_array(array: Iterable[Iterable[complex]], theta: Angle) -> npt.NDArray[np.complex128]: ...
 
     @overload
     @staticmethod
     def _cast_array(
-        array: Iterable[Iterable[ExpressionOrComplex]], theta: ExpressionOrFloat
+        array: Iterable[Iterable[ExpressionOrComplex]], theta: ParameterizedAngle
     ) -> npt.NDArray[np.complex128] | npt.NDArray[np.object_]: ...
 
     @staticmethod
     def _cast_array(
-        array: Iterable[Iterable[ExpressionOrComplex]], theta: ExpressionOrFloat
+        array: Iterable[Iterable[ExpressionOrComplex]], theta: ParameterizedAngle
     ) -> npt.NDArray[np.complex128] | npt.NDArray[np.object_]:
         if isinstance(theta, Expression):
             return np.asarray(array, dtype=np.object_)
@@ -93,88 +98,85 @@ class Ops:
 
     @overload
     @staticmethod
-    def rx(theta: float) -> npt.NDArray[np.complex128]: ...
+    def rx(theta: Angle) -> npt.NDArray[np.complex128]: ...
 
     @overload
     @staticmethod
     def rx(theta: Expression) -> npt.NDArray[np.object_]: ...
 
     @staticmethod
-    def rx(theta: ExpressionOrFloat) -> npt.NDArray[np.complex128] | npt.NDArray[np.object_]:
+    def rx(theta: ParameterizedAngle) -> npt.NDArray[np.complex128] | npt.NDArray[np.object_]:
         """X rotation.
 
         Parameters
         ----------
-        theta : float
-            rotation angle in radian
+        theta : Angle | Expression
+            rotation angle in units of π
 
         Returns
         -------
         operator : 2*2 np.asarray
         """
-        cos, sin = cos_sin(theta / 2)
-        return Ops._cast_array(
-            [[cos, -1j * sin], [-1j * sin, cos]],
-            theta,
-        )
+        cos, sin = cos_sin(angle_to_rad(theta) / 2)
+        return Ops._cast_array([[cos, -1j * sin], [-1j * sin, cos]], theta)
 
     @overload
     @staticmethod
-    def ry(theta: float) -> npt.NDArray[np.complex128]: ...
+    def ry(theta: Angle) -> npt.NDArray[np.complex128]: ...
 
     @overload
     @staticmethod
     def ry(theta: Expression) -> npt.NDArray[np.object_]: ...
 
     @staticmethod
-    def ry(theta: ExpressionOrFloat) -> npt.NDArray[np.complex128] | npt.NDArray[np.object_]:
+    def ry(theta: ParameterizedAngle) -> npt.NDArray[np.complex128] | npt.NDArray[np.object_]:
         """Y rotation.
 
         Parameters
         ----------
-        theta : float
-            rotation angle in radian
+        theta : Angle | Expression
+            rotation angle in units of π
 
         Returns
         -------
         operator : 2*2 np.asarray
         """
-        cos, sin = cos_sin(theta / 2)
+        cos, sin = cos_sin(angle_to_rad(theta) / 2)
         return Ops._cast_array([[cos, -sin], [sin, cos]], theta)
 
     @overload
     @staticmethod
-    def rz(theta: float) -> npt.NDArray[np.complex128]: ...
+    def rz(theta: Angle) -> npt.NDArray[np.complex128]: ...
 
     @overload
     @staticmethod
     def rz(theta: Expression) -> npt.NDArray[np.object_]: ...
 
     @staticmethod
-    def rz(theta: ExpressionOrFloat) -> npt.NDArray[np.complex128] | npt.NDArray[np.object_]:
+    def rz(theta: ParameterizedAngle) -> npt.NDArray[np.complex128] | npt.NDArray[np.object_]:
         """Z rotation.
 
         Parameters
         ----------
-        theta : float
-            rotation angle in radian
+        theta : Angle | Expression
+            rotation angle in units of π
 
         Returns
         -------
         operator : 2*2 np.asarray
         """
-        return Ops._cast_array([[exp(-1j * theta / 2), 0], [0, exp(1j * theta / 2)]], theta)
+        return Ops._cast_array([[exp(-1j * angle_to_rad(theta) / 2), 0], [0, exp(1j * angle_to_rad(theta) / 2)]], theta)
 
     @overload
     @staticmethod
-    def rzz(theta: float) -> npt.NDArray[np.complex128]: ...
+    def rzz(theta: Angle) -> npt.NDArray[np.complex128]: ...
 
     @overload
     @staticmethod
     def rzz(theta: Expression) -> npt.NDArray[np.object_]: ...
 
     @staticmethod
-    def rzz(theta: ExpressionOrFloat) -> npt.NDArray[np.complex128] | npt.NDArray[np.object_]:
+    def rzz(theta: ParameterizedAngle) -> npt.NDArray[np.complex128] | npt.NDArray[np.object_]:
         """zz-rotation.
 
         Equivalent to the sequence
@@ -184,8 +186,8 @@ class Ops:
 
         Parameters
         ----------
-        theta : float
-            rotation angle in radian
+        theta : Angle | Expression
+            rotation angle in units of π
 
         Returns
         -------
@@ -212,3 +214,21 @@ class Ops:
             return np.kron(lhs, rhs).astype(np.complex128, copy=False)
 
         return np.array([reduce(_reducer, i) for i in product((Ops.I, Ops.X, Ops.Y, Ops.Z), repeat=n_qubits)])
+
+    @staticmethod
+    def from_axis(axis: Axis) -> npt.NDArray[np.complex128]:
+        """Return the matrix representation of an AXIS."""
+        if axis == Axis.X:
+            return Ops.X
+        if axis == Axis.Y:
+            return Ops.Y
+        if axis == Axis.Z:
+            return Ops.Z
+        assert_never(axis)
+
+    @staticmethod
+    def from_ixyz(ixyz: IXYZ) -> npt.NDArray[np.complex128]:
+        """Return the matrix representation of an IXYZ."""
+        if ixyz == I:
+            return Ops.I
+        return Ops.from_axis(ixyz)

tests/test_pattern.py
@@ -16,7 +16,7 @@ from graphix.command import C, Command, CommandKind, E, M, N, X, Z
 from graphix.flow.exceptions import (
     FlowError,
 )
-from graphix.fundamentals import Plane
+from graphix.fundamentals import ANGLE_PI, Plane
 from graphix.measurements import Measurement, Outcome, PauliMeasurement
 from graphix.opengraph import OpenGraph
 from graphix.pattern import Pattern, RunnabilityError, RunnabilityErrorReason, shift_outcomes
@@ -31,6 +31,7 @@ from graphix.transpiler import Circuit
 if TYPE_CHECKING:
     from collections.abc import Sequence
 
+    from graphix.fundamentals import Angle
     from graphix.sim.base_backend import BackendState
 
 
@@ -245,10 +246,10 @@ class TestPattern:
 
         # QFT
         circuit.h(2)
-        cp(circuit, np.pi / 4, 0, 2)
-        cp(circuit, np.pi / 2, 1, 2)
+        cp(circuit, ANGLE_PI / 4, 0, 2)
+        cp(circuit, ANGLE_PI / 2, 1, 2)
         circuit.h(1)
-        cp(circuit, np.pi / 2, 0, 1)
+        cp(circuit, ANGLE_PI / 2, 0, 1)
         circuit.h(0)
         swap(circuit, 0, 2)
 
@@ -273,10 +274,10 @@ class TestPattern:
 
         # QFT
         circuit.h(2)
-        cp(circuit, np.pi / 4, 0, 2)
-        cp(circuit, np.pi / 2, 1, 2)
+        cp(circuit, ANGLE_PI / 4, 0, 2)
+        cp(circuit, ANGLE_PI / 2, 1, 2)
         circuit.h(1)
-        cp(circuit, np.pi / 2, 0, 1)
+        cp(circuit, ANGLE_PI / 2, 0, 1)
         circuit.h(0)
         swap(circuit, 0, 2)
 
@@ -634,7 +635,7 @@ class TestPattern:
         circuit_1.h(0)
         p1 = circuit_1.transpile().pattern  # outputs: [1]
 
-        alpha = 2 * np.pi * fx_rng.random()
+        alpha = 2 * ANGLE_PI * fx_rng.random()
 
         circuit_2 = Circuit(1)
         circuit_2.rz(0, alpha)
@@ -672,7 +673,7 @@ class TestPattern:
 
     # Test warning composition after standardization
     def test_compose_7(self, fx_rng: Generator) -> None:
-        alpha = 2 * np.pi * fx_rng.random()
+        alpha = 2 * ANGLE_PI * fx_rng.random()
 
         circuit_1 = Circuit(1)
         circuit_1.h(0)
@@ -1029,7 +1030,7 @@ class TestPattern:
         assert np.abs(np.dot(s_ref.flatten().conjugate(), s_test.flatten())) == pytest.approx(1)
 
 
-def cp(circuit: Circuit, theta: float, control: int, target: int) -> None:
+def cp(circuit: Circuit, theta: Angle, control: int, target: int) -> None:
     """Controlled rotation gate, decomposed."""  # noqa: D401
     circuit.rz(control, theta / 2)
     circuit.rz(target, theta / 2)
@@ -1071,10 +1072,10 @@ class TestMCOps:
         circuit = Circuit(n)
         for u, v in g.edges:
             circuit.cnot(u, v)
-            circuit.rz(v, np.pi / 4)
+            circuit.rz(v, ANGLE_PI / 4)
             circuit.cnot(u, v)
         for v in g.nodes:
-            circuit.rx(v, np.pi / 9)
+            circuit.rx(v, ANGLE_PI / 9)
 
         pattern = circuit.transpile().pattern
         graph = pattern.extract_graph()
@@ -1207,7 +1208,7 @@ class TestMCOps:
 
     @pytest.mark.parametrize("backend", ["statevector", "densitymatrix"])
     def test_arbitrary_inputs(self, fx_rng: Generator, nqb: int, rand_circ: Circuit, backend: str) -> None:
-        rand_angles = fx_rng.random(nqb) * 2 * np.pi
+        rand_angles = fx_rng.random(nqb) * 2 * ANGLE_PI
         rand_planes = fx_rng.choice(np.array(Plane), nqb)
         states = [PlanarState(plane=i, angle=j) for i, j in zip(rand_planes, rand_angles, strict=True)]
         randpattern = rand_circ.transpile().pattern
@@ -1216,7 +1217,7 @@ class TestMCOps:
         assert compare_backend_result_with_statevec(out, out_circ) == pytest.approx(1)
 
     def test_arbitrary_inputs_tn(self, fx_rng: Generator, nqb: int, rand_circ: Circuit) -> None:
-        rand_angles = fx_rng.random(nqb) * 2 * np.pi
+        rand_angles = fx_rng.random(nqb) * 2 * ANGLE_PI
         rand_planes = fx_rng.choice(np.array(Plane), nqb)
         states = [PlanarState(plane=i, angle=j) for i, j in zip(rand_planes, rand_angles, strict=True)]
         randpattern = rand_circ.transpile().pattern

tests/test_pyzx.py
@@ -8,6 +8,7 @@ import numpy as np
 import pytest
 from numpy.random import PCG64, Generator
 
+from graphix.fundamentals import ANGLE_PI
 from graphix.random_objects import rand_circuit
 from graphix.transpiler import Circuit
 
@@ -95,7 +96,7 @@ def test_random_circuit(fx_bg: PCG64, jumps: int) -> None:
 
 def test_rz() -> None:
     circuit = Circuit(2)
-    circuit.rz(0, np.pi / 4)
+    circuit.rz(0, ANGLE_PI / 4)
     pattern = circuit.transpile().pattern
     # pyzx 0.8 does not support arithmetic expressions such as `pi / 4`.
     circ = zx.qasm(f"qreg q[2]; rz({np.pi / 4}) q[0];")  # type: ignore[attr-defined]