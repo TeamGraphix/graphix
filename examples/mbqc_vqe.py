TeamGraphix/graphix

commit 3758aec8f639b8d97d36939ce22e8a9660bb36a0

Enable type-checking for `pattern.py` and `test_pattern.py` (#302)
This commit adds type annotations to the pattern module and its
corresponding tests, enabling full type-checking with `mypy` while
preserving existing functionality.

This lays the foundations for safer and more ambitious refactoring in
the future.

graphix/simulator.py
@@ -22,7 +22,11 @@ from graphix.sim.tensornet import TensorNetworkBackend
 from graphix.states import BasicStates
 
 if TYPE_CHECKING:
+    from typing import Any
+
+    from graphix.noise_models.noise_model import NoiseModel
     from graphix.pattern import Pattern
+    from graphix.sim.density_matrix import Data
 
 
 class MeasureMethod(abc.ABC):
@@ -160,7 +164,12 @@ class PatternSimulator:
     """
 
     def __init__(
-        self, pattern, backend="statevector", measure_method: MeasureMethod | None = None, noise_model=None, **kwargs
+        self,
+        pattern: Pattern,
+        backend: Backend | str = "statevector",
+        measure_method: MeasureMethod | None = None,
+        noise_model: NoiseModel | None = None,
+        **kwargs: Any,
     ) -> None:
         """
         Construct a pattern simulator.
@@ -223,7 +232,7 @@ class PatternSimulator:
             raise ValueError(f"The backend {self.backend} doesn't support noise but noisemodel was provided.")
         self.noise_model = model
 
-    def run(self, input_state=BasicStates.PLUS) -> None:
+    def run(self, input_state: Data = BasicStates.PLUS) -> None:
         """Perform the simulation.
 
         Returns

pyproject.toml
@@ -144,7 +144,6 @@ exclude = [
   '^graphix/device_interface\.py$',
   '^graphix/gflow\.py$',
   '^graphix/linalg\.py$',
-  '^graphix/pattern\.py$',
   '^graphix/random_objects\.py$',
   '^graphix/simulator\.py$',
   '^graphix/sim/base_backend\.py$',
@@ -157,7 +156,6 @@ exclude = [
   '^tests/test_gflow\.py$',
   '^tests/test_linalg\.py$',
   '^tests/test_noisy_density_matrix\.py$',
-  '^tests/test_pattern\.py$',
   '^tests/test_random_utilities\.py$',
   '^tests/test_runner\.py$',
   '^tests/test_statevec\.py$',
@@ -186,7 +184,6 @@ exclude = [
   "graphix/device_interface.py",
   "graphix/gflow.py",
   "graphix/linalg.py",
-  "graphix/pattern.py",
   "graphix/random_objects.py",
   "graphix/simulator.py",
   "graphix/sim/base_backend.py",
@@ -199,7 +196,6 @@ exclude = [
   "tests/test_gflow.py",
   "tests/test_linalg.py",
   "tests/test_noisy_density_matrix.py",
-  "tests/test_pattern.py",
   "tests/test_random_utilities.py",
   "tests/test_runner.py",
   "tests/test_statevec.py",

tests/test_pattern.py
@@ -17,6 +17,8 @@ from graphix.measurements import PauliMeasurement
 from graphix.pattern import Pattern, shift_outcomes
 from graphix.random_objects import rand_circuit, rand_gate
 from graphix.sim.density_matrix import DensityMatrix
+from graphix.sim.statevec import Statevec
+from graphix.sim.tensornet import MBQCTensorNet
 from graphix.simulator import PatternSimulator
 from graphix.states import PlanarState
 from graphix.transpiler import Circuit
@@ -24,17 +26,17 @@ from graphix.transpiler import Circuit
 if TYPE_CHECKING:
     import collections.abc
     from collections.abc import Sequence
+    from typing import Literal
 
-    from graphix.sim.base_backend import Backend
-    from graphix.sim.statevec import Statevec
+    from graphix.sim.base_backend import Backend, State
 
 
-def compare_backend_result_with_statevec(backend: str, backend_state, statevec: Statevec) -> float:
-    if backend == "statevector":
-        return np.abs(np.dot(backend_state.flatten().conjugate(), statevec.flatten()))
-    if backend == "densitymatrix":
-        return np.abs(np.dot(backend_state.rho.flatten().conjugate(), DensityMatrix(statevec).rho.flatten()))
-    raise NotImplementedError(backend)
+def compare_backend_result_with_statevec(backend_state: State, statevec: Statevec) -> float:
+    if isinstance(backend_state, Statevec):
+        return float(np.abs(np.dot(backend_state.flatten().conjugate(), statevec.flatten())))
+    if isinstance(backend_state, DensityMatrix):
+        return float(np.abs(np.dot(backend_state.rho.flatten().conjugate(), DensityMatrix(statevec).rho.flatten())))
+    raise NotImplementedError(backend_state)
 
 
 Outcome = typing.Literal[0, 1]
@@ -131,13 +133,14 @@ class TestPattern:
         def simulate_and_measure() -> int:
             sim = PatternSimulator(pattern, backend_type)
             sim.run()
-            if backend_type == "statevector":
-                assert sim.backend.state.dims() == ()
-            elif backend_type == "densitymatrix":
-                assert sim.backend.state.dims() == (1, 1)
-            elif backend_type == "tensornetwork":
-                assert sim.backend.state.to_statevector().shape == (1,)
-            return sim.measure_method.results[0]
+            state = sim.backend.state
+            if isinstance(state, Statevec):
+                assert state.dims() == ()
+            elif isinstance(state, DensityMatrix):
+                assert state.dims() == (1, 1)
+            elif isinstance(state, MBQCTensorNet):
+                assert state.to_statevector().shape == (1,)
+            return sim.measure_method.get_measure_result(0)
 
         nb_shots = 1000
         nb_ones = sum(1 for _ in range(nb_shots) if simulate_and_measure())
@@ -194,7 +197,7 @@ class TestPattern:
         pattern.minimize_space()
         state = circuit.simulate_statevector().statevec
         state_mbqc = pattern.simulate_pattern(backend, rng=rng)
-        assert compare_backend_result_with_statevec(backend, state_mbqc, state) == pytest.approx(1)
+        assert compare_backend_result_with_statevec(state_mbqc, state) == pytest.approx(1)
 
     @pytest.mark.parametrize("jumps", range(1, 11))
     @pytest.mark.parametrize("ignore_pauli_with_deps", [False, True])
@@ -217,7 +220,7 @@ class TestPattern:
     @pytest.mark.parametrize("angle", [0.0, 0.5, 1.0, 1.5])
     def test_pauli_measurement_single(self, plane: Plane, angle: float) -> None:
         pattern = Pattern(input_nodes=[0, 1])
-        pattern.add(E(nodes=[0, 1]))
+        pattern.add(E(nodes=(0, 1)))
         pattern.add(M(node=0, plane=plane, angle=angle))
         pattern_ref = pattern.copy()
         pattern.perform_pauli_measurements()
@@ -292,7 +295,7 @@ class TestPattern:
 
         isolated_nodes = pattern.get_isolated_nodes()
         # There is no isolated node.
-        isolated_nodes_ref = set()
+        isolated_nodes_ref: set[int] = set()
 
         assert isolated_nodes == isolated_nodes_ref
 
@@ -341,7 +344,9 @@ class TestPattern:
         pattern.standardize(method="mc")
         signal_dict = pattern.shift_signals(method=method)
         # Test for every possible outcome of each measure
-        for outcomes_ref in itertools.product(*([[0, 1]] * 3)):
+        zero_one: list[Literal[0, 1]] = [0, 1]
+        outcomes_ref: tuple[Literal[0, 1], ...]
+        for outcomes_ref in itertools.product(*([zero_one] * 3)):
             state_ref = pattern_ref.simulate_pattern(pr_calc=False, rng=IterGenerator(iter(outcomes_ref)))
             outcomes_p = shift_outcomes(dict(enumerate(outcomes_ref)), signal_dict)
             state_p = pattern.simulate_pattern(
@@ -390,7 +395,7 @@ class TestPattern:
         pattern.minimize_space()
         state = circuit.simulate_statevector().statevec
         state_mbqc = pattern.simulate_pattern()
-        assert compare_backend_result_with_statevec("statevector", state_mbqc, state) == pytest.approx(1)
+        assert compare_backend_result_with_statevec(state_mbqc, state) == pytest.approx(1)
 
     @pytest.mark.parametrize("jumps", range(1, 11))
     def test_standardize_two_cliffords(self, fx_bg: PCG64, jumps: int) -> None:
@@ -473,7 +478,7 @@ class TestMCOps:
         pattern = circuit.transpile().pattern
         nodes, edges = pattern.get_graph()
 
-        nodes_ref = list(np.arange(27))
+        nodes_ref = list(range(27))
         edges_ref = [
             (1, 3),
             (0, 3),
@@ -632,7 +637,7 @@ class TestMCOps:
         randpattern = rand_circ.transpile().pattern
         out = randpattern.simulate_pattern(backend=backend, input_state=states, rng=fx_rng)
         out_circ = rand_circ.simulate_statevector(input_state=states).statevec
-        assert compare_backend_result_with_statevec(backend, out, out_circ) == pytest.approx(1)
+        assert compare_backend_result_with_statevec(out, out_circ) == pytest.approx(1)
 
     def test_arbitrary_inputs_tn(self, fx_rng: Generator, nqb: int, rand_circ: Circuit) -> None:
         rand_angles = fx_rng.random(nqb) * 2 * np.pi

graphix/generator.py
@@ -16,10 +16,12 @@ if TYPE_CHECKING:
     import numpy as np
     import numpy.typing as npt
 
+    from graphix.parameter import ExpressionOrFloat
+
 
 def generate_from_graph(
     graph: nx.Graph[int],
-    angles: Mapping[int, float] | Sequence[float] | npt.NDArray[np.float64],
+    angles: Mapping[int, ExpressionOrFloat] | Sequence[ExpressionOrFloat] | npt.NDArray[np.float64],
     inputs: Iterable[int],
     outputs: Iterable[int],
     meas_planes: Mapping[int, Plane] | None = None,

graphix/gflow.py
@@ -39,7 +39,7 @@ def check_meas_planes(meas_planes: dict[int, Plane]) -> None:
 
 
 def find_gflow(
-    graph: nx.Graph,
+    graph: nx.Graph[int],
     iset: set[int],
     oset: set[int],
     meas_planes: dict[int, Plane],

graphix/measurements.py
@@ -28,7 +28,7 @@ class Measurement(NamedTuple):
     :param plane: the measurement plane
     """
 
-    angle: float
+    angle: ExpressionOrFloat
     plane: Plane
 
     def isclose(self, other: Measurement, rel_tol: float = 1e-09, abs_tol: float = 0.0) -> bool:
@@ -45,7 +45,11 @@ class Measurement(NamedTuple):
         >>> Measurement(0.1, Plane.XY).isclose(Measurement(0.0, Plane.XY))
         False
         """
-        return math.isclose(self.angle, other.angle, rel_tol=rel_tol, abs_tol=abs_tol) and self.plane == other.plane
+        return (
+            math.isclose(self.angle, other.angle, rel_tol=rel_tol, abs_tol=abs_tol)
+            if isinstance(self.angle, float) and isinstance(other.angle, float)
+            else self.angle == other.angle
+        ) and self.plane == other.plane
 
 
 class PauliMeasurement(NamedTuple):

graphix/parameter.py
@@ -335,7 +335,7 @@ T = TypeVar("T")
 
 
 @overload
-def subs(value: Expression, variable: Parameter, substitute: ExpressionOrSupportsFloat) -> Expression: ...
+def subs(value: ExpressionOrFloat, variable: Parameter, substitute: ExpressionOrSupportsFloat) -> ExpressionOrFloat: ...
 
 
 @overload
@@ -370,6 +370,16 @@ def subs(value: T, variable: Parameter, substitute: ExpressionOrSupportsFloat) -
             return c.real
         return c
     return new_value
+
+
+@overload
+def xreplace(
+    value: ExpressionOrFloat, assignment: Mapping[Parameter, ExpressionOrSupportsFloat]
+) -> ExpressionOrFloat: ...
+
+
+@overload
+def xreplace(value: T, assignment: Mapping[Parameter, ExpressionOrSupportsFloat]) -> T | Expression | complex: ...
 
 
 # The return type could be `T | Expression | complex` since `subs` returns `Expression` only

commit cca93695a4122c84cf40f43589158bc45188b44b

Enable type-checking for examples
This commit adds type annotations in all example modules, and refines
the type of `PatternSimulator` constructor, so that the type parameter
for states is automatically inferred from the `backend` argument.

It enables full type-checking with mypy and pyright while preserving
existing functionality across the entire code base. No files are
ignored by the typers anymore.

**Related issue:**
This commit continues the work started in #302, #308, #312, #347, and #414.

examples/mbqc_vqe.py
@@ -37,8 +37,11 @@ from graphix.simulator import PatternSimulator
 if TYPE_CHECKING:
     from collections.abc import Iterable
 
+    from scipy.optimize import OptimizeResult
+
+    from graphix.fundamentals import ParameterizedAngle
     from graphix.pattern import Pattern
-    from graphix.transpiler import Angle
+    from graphix.sim.tensornet import MBQCTensorNet
 
 Z = np.array([[1, 0], [0, -1]])
 X = np.array([[0, 1], [1, 0]])
@@ -46,7 +49,7 @@ X = np.array([[0, 1], [1, 0]])
 
 # %%
 # Define the Hamiltonian for the VQE problem (Example: H = Z0Z1 + X0 + X1)
-def create_hamiltonian() -> npt.NDArray[np.complex128]:
+def create_hamiltonian() -> npt.NDArray[np.float64]:
     return np.kron(Z, Z) + np.kron(X, np.eye(2)) + np.kron(np.eye(2), X)
 
 
@@ -65,7 +68,7 @@ else:
 
 # %%
 # Function to build the VQE circuit
-def build_vqe_circuit(n_qubits: int, params: Iterable[Angle]) -> Circuit:
+def build_vqe_circuit(n_qubits: int, params: Iterable[ParameterizedAngle]) -> Circuit:
     circuit = Circuit(n_qubits)
     for i, (x, y, z) in enumerate(batched(params, n=3)):
         circuit.rx(i, x)
@@ -78,13 +81,13 @@ def build_vqe_circuit(n_qubits: int, params: Iterable[Angle]) -> Circuit:
 
 # %%
 class MBQCVQE:
-    def __init__(self, n_qubits: int, hamiltonian: npt.NDArray):
+    def __init__(self, n_qubits: int, hamiltonian: npt.NDArray[np.float64]):
         self.n_qubits = n_qubits
         self.hamiltonian = hamiltonian
 
     # %%
     # Function to build the MBQC pattern
-    def build_mbqc_pattern(self, params: Iterable[Angle]) -> Pattern:
+    def build_mbqc_pattern(self, params: Iterable[ParameterizedAngle]) -> Pattern:
         circuit = build_vqe_circuit(self.n_qubits, params)
         pattern = circuit.transpile().pattern
         pattern.standardize()
@@ -95,34 +98,36 @@ class MBQCVQE:
 
     # %%
     # Function to simulate the MBQC circuit
-    def simulate_mbqc(self, params: Iterable[float], backend="tensornetwork"):
+    def simulate_mbqc(
+        self,
+        params: Iterable[float],
+    ) -> MBQCTensorNet:
         pattern = self.build_mbqc_pattern(params)
-        simulator = PatternSimulator(pattern, backend=backend)
-        if backend == "tensornetwork":
-            simulator.run()  # Simulate the MBQC circuit using tensor network
-            tn = simulator.backend.state
-            tn.default_output_nodes = pattern.output_nodes  # Set the default_output_nodes attribute
-            if tn.default_output_nodes is None:
-                raise ValueError("Output nodes are not set for tensor network simulation.")
-            return tn
-        return simulator.run()  # Simulate the MBQC circuit using other backends
+        simulator = PatternSimulator(pattern, backend="tensornetwork")
+        state = simulator.backend.state
+        simulator.run()  # Simulate the MBQC circuit using tensor network
+        tn = simulator.backend.state
+        tn.default_output_nodes = pattern.output_nodes  # Set the default_output_nodes attribute
+        if tn.default_output_nodes is None:
+            raise ValueError("Output nodes are not set for tensor network simulation.")
+        return state
 
     # %%
     # Function to compute the energy
-    def compute_energy(self, params: Iterable[float]):
+    def compute_energy(self, params: Iterable[float]) -> float:
         # Simulate the MBQC circuit using tensor network backend
-        tn = self.simulate_mbqc(params, backend="tensornetwork")
+        tn = self.simulate_mbqc(params)
         # Compute the expectation value using MBQCTensornet.expectation_value
-        return tn.expectation_value(self.hamiltonian, qubit_indices=range(self.n_qubits))
+        return tn.expectation_value(self.hamiltonian.astype(np.complex128), qubit_indices=range(self.n_qubits))
 
 
 class MBQCVQEWithPlaceholders(MBQCVQE):
-    def __init__(self, n_qubits: int, hamiltonian) -> None:
+    def __init__(self, n_qubits: int, hamiltonian: npt.NDArray[np.float64]) -> None:
         super().__init__(n_qubits, hamiltonian)
         self.placeholders = tuple(Placeholder(f"{r}[{q}]") for q in range(n_qubits) for r in ("X", "Y", "Z"))
         self.pattern = super().build_mbqc_pattern(self.placeholders)
 
-    def build_mbqc_pattern(self, params):
+    def build_mbqc_pattern(self, params: Iterable[ParameterizedAngle]) -> Pattern:
         return self.pattern.xreplace(dict(zip(self.placeholders, params, strict=True)))
 
 
@@ -133,12 +138,12 @@ hamiltonian = create_hamiltonian()
 
 # %%
 # Instantiate the MBQCVQE class
-mbqc_vqe = MBQCVQEWithPlaceholders(n_qubits, hamiltonian)
+mbqc_vqe: MBQCVQE = MBQCVQEWithPlaceholders(n_qubits, hamiltonian)
 
 
 # %%
 # Define the cost function
-def cost_function(params):
+def cost_function(params: Iterable[float]) -> float:
     return mbqc_vqe.compute_energy(params)
 
 
@@ -150,7 +155,7 @@ initial_params = rng.random(n_qubits * 3)
 
 # %%
 # Perform the optimization using COBYLA
-def compute():
+def compute() -> OptimizeResult:
     return minimize(cost_function, initial_params, method="COBYLA", options={"maxiter": 100})
 
 

examples/qaoa.py
@@ -17,9 +17,9 @@ from graphix import Circuit
 rng = np.random.default_rng()
 
 n = 4
-xi = rng.random(6)
-theta = rng.random(4)
-g = nx.complete_graph(n)
+xi: np.ndarray[tuple[int], np.dtype[np.float64]] = rng.random(6)
+theta: np.ndarray[tuple[int], np.dtype[np.float64]] = rng.random(4)
+g: nx.Graph[int] = nx.complete_graph(n)
 circuit = Circuit(n)
 for i, (u, v) in enumerate(g.edges):
     circuit.cnot(u, v)

examples/qft_with_tn.py
@@ -10,11 +10,16 @@ Firstly, let us import relevant modules and define the circuit:
 # %%
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from graphix import Circuit
 from graphix.fundamentals import ANGLE_PI
 
+if TYPE_CHECKING:
+    from graphix.fundamentals import Angle
 
-def cp(circuit, theta, control, target):
+
+def cp(circuit: Circuit, theta: Angle, control: int, target: int) -> None:
     circuit.rz(control, theta / 2)
     circuit.rz(target, theta / 2)
     circuit.cnot(control, target)
@@ -22,19 +27,18 @@ def cp(circuit, theta, control, target):
     circuit.cnot(control, target)
 
 
-def qft_rotations(circuit, n):
+def qft_rotations(circuit: Circuit, n: int) -> None:
     circuit.h(n)
     for qubit in range(n + 1, circuit.width):
         cp(circuit, ANGLE_PI / 2 ** (qubit - n), qubit, n)
 
 
-def swap_registers(circuit, n):
+def swap_registers(circuit: Circuit, n: int) -> None:
     for qubit in range(n // 2):
         circuit.swap(qubit, n - qubit - 1)
-    return circuit
 
 
-def qft(circuit, n):
+def qft(circuit: Circuit, n: int) -> None:
     for i in range(n):
         qft_rotations(circuit, i)
     swap_registers(circuit, n)

examples/tn_simulation.py
@@ -11,17 +11,25 @@ Firstly, let's import the relevant modules:
 from __future__ import annotations
 
 from functools import reduce
+from typing import TYPE_CHECKING
 
 import cotengra as ctg
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
-import opt_einsum as oe
 import quimb.tensor as qtn
+from cotengra.oe import PathOptimizer
 from scipy.optimize import minimize
 
 from graphix import Circuit
 from graphix.fundamentals import ANGLE_PI
+
+if TYPE_CHECKING:
+    from collections.abc import Collection, Sequence
+
+    import numpy.typing as npt
+
+    from graphix.fundamentals import Angle
 
 rng = np.random.default_rng()
 
@@ -34,7 +42,13 @@ rng = np.random.default_rng()
 # Let's start with defining a helper function for buidling the circuit.
 
 
-def ansatz(circuit, n, gamma, beta, iterations):
+def ansatz(
+    circuit: Circuit,
+    n: int,
+    gamma: npt.NDArray[np.float64] | Sequence[Angle],
+    beta: npt.NDArray[np.float64] | Sequence[Angle],
+    iterations: int,
+) -> None:
     for j in range(iterations):
         for i in range(1, n):
             circuit.cnot(i, 0)
@@ -109,8 +123,8 @@ t1 = qtn.rand_tensor([2, 2], ["a", "b"])
 t2 = qtn.rand_tensor([1, 2], ["b", "c"])
 t1.add_tag("T1")
 t2.add_tag("T2")
-t = qtn.TensorNetwork([t1, t2])
-t.draw(ax=ax[1], title="MPS", legend=False, color=["T1", "T2"])
+tn = qtn.TensorNetwork([t1, t2])
+tn.draw(ax=ax[1], title="MPS", legend=False, color=["T1", "T2"])
 plt.show()
 
 # %%
@@ -149,7 +163,7 @@ print(f"Probability for {0} is {value}")
 # It is also possible to change the path contraction algorithm.
 # Let's explore that too and define a custom optimizer for contraction, that we can use later.
 
-opt = ctg.HyperOptimizer(
+opt: PathOptimizer = ctg.HyperOptimizer(
     minimize="combo",
     reconf_opts={},
     progbar=True,
@@ -172,7 +186,14 @@ print("Expectation value for Z^n: ", exp_val)
 # Create a cost function using the elements of graphix, which were already discussed above.
 
 
-def cost(params, n, ham, quantum_iter, slice_index, opt=None):
+def cost(
+    params: npt.NDArray[np.float64],
+    n: int,
+    ham: npt.NDArray[np.float64],
+    quantum_iter: int,
+    slice_index: int,
+    opt: str | PathOptimizer | None = None,
+) -> float:
     circuit = Circuit(n)
     gamma = params[:slice_index]
     beta = params[slice_index:]
@@ -184,7 +205,7 @@ def cost(params, n, ham, quantum_iter, slice_index, opt=None):
     pattern.remove_input_nodes()
     pattern.perform_pauli_measurements()
     mbqc_tn = pattern.simulate_pattern(backend="tensornetwork", graph_prep="parallel")
-    exp_val = 0
+    exp_val: float = 0
     for op in ham:
         exp_val += np.real(mbqc_tn.expectation_value(op, range(n), optimize=opt))
     return exp_val
@@ -195,7 +216,7 @@ def cost(params, n, ham, quantum_iter, slice_index, opt=None):
 
 ham = [reduce(np.kron, [pauli_z] * n)]
 for i in range(1, n):
-    op = [identity] * n
+    op = np.array([identity] * n)
     op[0] = pauli_z
     op[i] = pauli_z
     op = reduce(np.kron, op)
@@ -203,8 +224,10 @@ for i in range(1, n):
 
 
 # Use yet again another optimizer for path contraction.
-class MyOptimizer(oe.paths.PathOptimizer):
-    def __call__(self, inputs, output, size_dict, memory_limit=None):
+class MyOptimizer(PathOptimizer):
+    def __call__(
+        self, inputs: Collection[object], output: object, size_dict: object, memory_limit: object = None
+    ) -> list[tuple[int, int]]:
         return [(0, 1)] * (len(inputs) - 1)
 
 
@@ -258,7 +281,7 @@ plt.show()
 
 fig, ax = plt.subplots(ncols=2, figsize=(8, 6))
 ax = ax.flatten()
-g = nx.Graph()
+g: nx.Graph[int] = nx.Graph()
 for i in range(1, n):
     g.add_edge(0, i)
 color = ["blue"] * n

graphix/pattern.py
@@ -1383,7 +1383,7 @@ class Pattern:
 
         .. seealso:: :class:`graphix.simulator.PatternSimulator`
         """
-        sim: PatternSimulator[_StateT_co] = PatternSimulator(self, backend=backend, **kwargs)
+        sim = PatternSimulator(self, backend=backend, **kwargs)
         sim.run(input_state, rng=rng)
         return sim.backend.state
 

graphix/simulator.py
@@ -36,11 +36,14 @@ if TYPE_CHECKING:
     from graphix.command import BaseN
     from graphix.noise_models.noise_model import CommandOrNoise, NoiseModel
     from graphix.pattern import Pattern
-    from graphix.sim import Data
+    from graphix.sim import Data, DensityMatrix, MBQCTensorNet, Statevec
 
 
 _BuiltinBackend = DensityMatrixBackend | StatevectorBackend | TensorNetworkBackend
 _BackendLiteral = Literal["statevector", "densitymatrix", "tensornetwork", "mps"]
+
+if TYPE_CHECKING:
+    _BuiltinBackendState = DensityMatrix | MBQCTensorNet | Statevec
 
 
 class PrepareMethod(abc.ABC):
@@ -216,10 +219,62 @@ class PatternSimulator(Generic[_StateT_co]):
     """
 
     noise_model: NoiseModel | None
-    backend: Backend[_StateT_co] | _BuiltinBackend
+    backend: Backend[_StateT_co]
 
+    @overload
+    def __init__(
+        self: PatternSimulator[Statevec],
+        pattern: Pattern,
+        backend: Literal["statevector"] = ...,
+        prepare_method: PrepareMethod | None = None,
+        measure_method: MeasureMethod | None = None,
+        noise_model: NoiseModel | None = None,
+        branch_selector: BranchSelector | None = None,
+        graph_prep: str | None = None,
+        symbolic: bool = False,
+    ) -> None: ...
+
+    @overload
+    def __init__(
+        self: PatternSimulator[DensityMatrix],
+        pattern: Pattern,
+        backend: Literal["densitymatrix"] = ...,
+        prepare_method: PrepareMethod | None = None,
+        measure_method: MeasureMethod | None = None,
+        noise_model: NoiseModel | None = None,
+        branch_selector: BranchSelector | None = None,
+        graph_prep: str | None = None,
+        symbolic: bool = False,
+    ) -> None: ...
+
+    @overload
+    def __init__(
+        self: PatternSimulator[MBQCTensorNet],
+        pattern: Pattern,
+        backend: Literal["tensornetwork", "mps"] = ...,
+        prepare_method: PrepareMethod | None = None,
+        measure_method: MeasureMethod | None = None,
+        noise_model: NoiseModel | None = None,
+        branch_selector: BranchSelector | None = None,
+        graph_prep: str | None = None,
+        symbolic: bool = False,
+    ) -> None: ...
+
+    @overload
     def __init__(
         self,
+        pattern: Pattern,
+        backend: Backend[_StateT_co] = ...,
+        prepare_method: PrepareMethod | None = None,
+        measure_method: MeasureMethod | None = None,
+        noise_model: NoiseModel | None = None,
+        branch_selector: BranchSelector | None = None,
+        graph_prep: str | None = None,
+        symbolic: bool = False,
+    ) -> None: ...
+
+    def __init__(
+        self: PatternSimulator[_StateT_co | _BuiltinBackendState],
         pattern: Pattern,
         backend: Backend[_StateT_co] | _BackendLiteral = "statevector",
         prepare_method: PrepareMethod | None = None,

pyproject.toml
@@ -19,6 +19,7 @@ classifiers = [
   "Programming Language :: Python :: 3.11",
   "Programming Language :: Python :: 3.12",
   "Programming Language :: Python :: 3.13",
+  "Programming Language :: Python :: 3.14",
   "Operating System :: OS Independent",
   "Topic :: Scientific/Engineering :: Physics",
 ]
@@ -133,16 +134,6 @@ filterwarnings = ["ignore:Couldn't import `kahypar`"]
 [tool.mypy]
 # Keep in sync with pyright
 files = ["*.py", "examples", "graphix", "tests"]
-exclude = [
-  '^examples/deutsch_jozsa\.py$',
-  '^examples/ghz_with_tn\.py$',
-  '^examples/mbqc_vqe\.py$',
-  '^examples/qaoa\.py$',
-  '^examples/qft_with_tn\.py$',
-  '^examples/qnn\.py$',
-  '^examples/rotation\.py$',
-  '^examples/tn_simulation\.py$',
-]
 follow_imports = "silent"
 follow_untyped_imports = true # required for qiskit, requires mypy >=1.14
 strict = true
@@ -151,16 +142,6 @@ mypy_path = "./stubs"
 [tool.pyright]
 # Keep in sync with mypy
 include = ["*.py", "examples", "graphix", "tests"]
-exclude = [
-  "examples/deutsch_jozsa.py",
-  "examples/ghz_with_tn.py",
-  "examples/mbqc_vqe.py",
-  "examples/qaoa.py",
-  "examples/qft_with_tn.py",
-  "examples/qnn.py",
-  "examples/rotation.py",
-  "examples/tn_simulation.py",
-]
 reportUnknownArgumentType = "information"
 reportUnknownLambdaType = "information"
 reportUnknownMemberType = "information"

stubs/quimb/tensor/__init__.pyi
@@ -4,6 +4,7 @@ from typing import Literal
 import numpy as np
 import numpy.typing as npt
 from cotengra.oe import PathOptimizer
+from matplotlib.axes import Axes
 from quimb import oset
 from typing_extensions import Self
 
@@ -17,6 +18,8 @@ class Tensor:
         tags: Sequence[str] | None = None,
         left_inds: Sequence[str] | None = None,
     ) -> None: ...
+    def add_tag(self, tag: str) -> None: ...
+    def draw(self, legend: bool | str = "auto", title: str | None = None, ax: Axes | None = None) -> None: ...
     def reindex(self, retag_map: Mapping[str, str], inplace: bool = False) -> Tensor: ...
     def retag(self, retag_map: Mapping[str, str], inplace: bool = False) -> Tensor: ...
     def split(
@@ -63,6 +66,18 @@ class TensorNetwork:
         optimize: str | PathOptimizer | None = None,
     ) -> float: ...
     def copy(self, virtual: bool = False, deep: bool = False) -> TensorNetwork: ...
+    def draw(
+        self,
+        color: list[str] | None = None,
+        *,
+        show_inds: bool | None = None,
+        show_tags: bool | None = None,
+        legend: bool | str = "auto",
+        iterations: int | Literal["auto"] = "auto",
+        k: float | None = None,
+        title: str | None = None,
+        ax: Axes | None = None,
+    ) -> None: ...
     def full_simplify(
         self,
         seq: str = "ADCR",
@@ -83,3 +98,4 @@ class TensorNetwork:
     def tensors(self) -> tuple[Tensor, ...]: ...
 
 def rand_uuid(base: str = "") -> str: ...
+def rand_tensor(shape: Sequence[int], inds: Sequence[str] | str) -> Tensor: ...

tests/test_pattern.py
@@ -167,7 +167,7 @@ class TestPattern:
         pattern.add(M(node=0, angle=0.5))
 
         def simulate_and_measure() -> int:
-            sim: PatternSimulator[Statevec | DensityMatrix | MBQCTensorNet] = PatternSimulator(pattern, backend_type)
+            sim = PatternSimulator(pattern, backend_type)
             sim.run()
             state = sim.backend.state
             if isinstance(state, Statevec):