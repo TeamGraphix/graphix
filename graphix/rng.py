f8287dbd7c4cb800ec8f0253fd0853c6b348da43
Thierry Martinez <thierry.martinez@inria.fr>
2026-03-11 15:28:09 +0100

    Add `ensure_rng` to the documentation and fix syntax

--- graphix/rng.py
+++ graphix/rng.py
@@ -15,13 +15,13 @@ _rng_local = threading.local()
 
 
 def ensure_rng(rng: Generator | None = None, *, stacklevel: int = 1) -> Generator:
-    """Return a default random-number generator if `None` is given.
+    """Return a default random-number generator if ``None`` is given.
 
     Parameters
     ----------
     rng: Generator | None, optional
-        If set and not `None`, this value is returned.
-        If `None` (the default), the default random-number generator is returned.
+        If set and not ``None``, this value is returned.
+        If ``None`` (the default), the default random-number generator is returned.
     stacklevel : int, optional
         Stack level to use for warnings. Defaults to 1, meaning that warnings
         are reported at this function's call site.
