6d3e54f333185c1176c1848f984d35caac5ab2db
Emlyn Graham <egraham@PTB-02009984.paris.inria.fr>
2026-04-28 16:26:33 +0200

    Revert "removing unnecessary local imports"
    This reverts commit 94257711ee681b6c7e75b1b33475ecb4a124aa51.

--- graphix/optimization.py
+++ graphix/optimization.py
@@ -15,7 +15,7 @@ import networkx as nx
 # assert_never added in Python 3.11
 from typing_extensions import assert_never
 
-from graphix import Pattern, command
+from graphix import command
 from graphix.clifford import Clifford, Domains
 from graphix.command import CommandKind, Node
 from graphix.flow._partial_order import compute_topological_generations
@@ -35,6 +35,7 @@ if TYPE_CHECKING:
     from collections.abc import Set as AbstractSet
     from typing import Self
 
+    from graphix import Pattern
     from graphix.space_minimization import SpaceMinimizationHeuristic
 
 
@@ -388,6 +389,8 @@ class StandardizedPattern(_StandardizedPattern):
 
     def to_pattern(self) -> Pattern:
         """Return the standardized pattern."""
+        from graphix import Pattern  # noqa: PLC0415
+
         pattern = Pattern(input_nodes=self.input_nodes)
         pattern.results = dict(self.results)
         pattern.extend(
@@ -723,6 +726,8 @@ def _update_corrections(node: Node, domain: AbstractSet[Node], correction: dict[
 
 def incorporate_pauli_results(pattern: Pattern) -> Pattern:
     """Return an equivalent pattern where results from Pauli presimulation are integrated in corrections."""
+    from graphix import Pattern  # noqa: PLC0415
+
     result = Pattern(input_nodes=pattern.input_nodes)
     for cmd in pattern:
         match cmd.kind:
@@ -768,6 +773,8 @@ def incorporate_pauli_results(pattern: Pattern) -> Pattern:
 
 def remove_useless_domains(pattern: Pattern) -> Pattern:
     """Return an equivalent pattern where measurement domains that are not used given the specific measurement angles and planes are removed."""
+    from graphix import Pattern  # noqa: PLC0415
+
     new_pattern = Pattern(input_nodes=pattern.input_nodes)
     new_pattern.results = pattern.results
     for cmd in pattern:
@@ -788,6 +795,8 @@ def remove_useless_domains(pattern: Pattern) -> Pattern:
 
 def single_qubit_domains(pattern: Pattern) -> Pattern:
     """Return an equivalent pattern where domains contains at most one qubit."""
+    from graphix import Pattern  # noqa: PLC0415
+
     new_pattern = Pattern(input_nodes=pattern.input_nodes)
     new_pattern.results = pattern.results
 
