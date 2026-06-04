4ea0cd5644c288376bf76540b4f67c98a428a52a
Thierry Martinez <thierry.martinez@inria.fr>
2026-06-04 09:58:21 +0200

    Parametrize tests
    ---------
    
    Co-authored-by: matulni <m.uldemolins@gmail.com>

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,29 +1,42 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+import pytest
+
 from graphix import Clifford
 from graphix.command import BaseM, BaseN, Command
 
+if TYPE_CHECKING:
+    from graphix.command import BaseCommand
 
-def test_reindex() -> None:
+
+@pytest.mark.parametrize(
+    ("cmd1", "cmd2"),
+    [
+        (BaseN(0), BaseN(1)),
+        (BaseN(2), BaseN(2)),
+        (Command.N(0), Command.N(1)),
+        (Command.N(2), Command.N(2)),
+        (BaseM(0), BaseM(1)),
+        (BaseM(2), BaseM(2)),
+        (Command.M(0), Command.M(1)),
+        (Command.M(2), Command.M(2)),
+        (Command.E((0, 2)), Command.E((1, 2))),
+        (Command.E((2, 0)), Command.E((2, 1))),
+        (Command.C(0, Clifford.H), Command.C(1, Clifford.H)),
+        (Command.C(2, Clifford.S), Command.C(2, Clifford.S)),
+        (Command.X(0, {2}), Command.X(1, {2})),
+        (Command.X(2, {0}), Command.X(2, {1})),
+        (Command.Z(0, {2}), Command.Z(1, {2})),
+        (Command.Z(2, {0}), Command.Z(2, {1})),
+        (Command.S(0, {2}), Command.S(1, {2})),
+        (Command.S(2, {0}), Command.S(2, {1})),
+        (Command.T(), Command.T()),
+    ],
+)
+def test_reindex(cmd1: BaseCommand, cmd2: BaseCommand) -> None:
     def reindex(node: int) -> int:
         return 1 if node == 0 else node
 
-    assert BaseN(0).reindex(reindex) == BaseN(1)
-    assert BaseN(2).reindex(reindex) == BaseN(2)
-    assert Command.N(0).reindex(reindex) == Command.N(1)
-    assert Command.N(2).reindex(reindex) == Command.N(2)
-    assert BaseM(0).reindex(reindex) == BaseM(1)
-    assert BaseM(2).reindex(reindex) == BaseM(2)
-    assert Command.M(0).reindex(reindex) == Command.M(1)
-    assert Command.M(2).reindex(reindex) == Command.M(2)
-    assert Command.E((0, 2)).reindex(reindex) == Command.E((1, 2))
-    assert Command.E((2, 0)).reindex(reindex) == Command.E((2, 1))
-    assert Command.C(0, Clifford.H).reindex(reindex) == Command.C(1, Clifford.H)
-    assert Command.C(2, Clifford.S).reindex(reindex) == Command.C(2, Clifford.S)
-    assert Command.X(0, {2}).reindex(reindex) == Command.X(1, {2})
-    assert Command.X(2, {0}).reindex(reindex) == Command.X(2, {1})
-    assert Command.Z(0, {2}).reindex(reindex) == Command.Z(1, {2})
-    assert Command.Z(2, {0}).reindex(reindex) == Command.Z(2, {1})
-    assert Command.S(0, {2}).reindex(reindex) == Command.S(1, {2})
-    assert Command.S(2, {0}).reindex(reindex) == Command.S(2, {1})
-    assert Command.T().reindex(reindex) == Command.T()
+    assert cmd1.reindex(reindex) == cmd2
