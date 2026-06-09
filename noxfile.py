f2f2da443cdf15a3d147531081b2008e1b944308
Thierry Martinez <thierry.martinez@inria.fr>
2026-06-09 08:05:00 +0200

    Pin graphix-stim-backend

--- noxfile.py
+++ noxfile.py
@@ -110,7 +110,7 @@ class ReverseDependency:
 @nox.parametrize(
     "package",
     [
-        ReverseDependency("https://github.com/thierry-martinez/graphix-stim-backend"),
+        ReverseDependency("https://github.com/thierry-martinez/graphix-stim-backend", branch="fix/add_jcz"),
         ReverseDependency("https://github.com/TeamGraphix/graphix-symbolic"),
         ReverseDependency("https://github.com/TeamGraphix/graphix-qasm-parser"),
         ReverseDependency(
