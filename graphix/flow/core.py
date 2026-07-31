0ac1052b0bf1a132fdcc3e6d5946b33a5e04acfc
thierry-martinez <thierry.martinez@inria.fr>
2026-07-31 12:55:44 +0200

    Update graphix/flow/core.py
    Co-authored-by: matulni <m.uldemolins@gmail.com>

--- graphix/flow/core.py
+++ graphix/flow/core.py
@@ -906,7 +906,7 @@ class PauliFlow(Parameterizable, Generic[_AM_co]):
 
         Notes
         -----
-        See notes and examples in :func:`OpenGraph.xreplace`.
+        See notes and examples in :meth:`OpenGraph.with_parameters`.
         """
         new_og = self.og.with_parameters(assignment)
         return dataclasses.replace(self, og=new_og)
