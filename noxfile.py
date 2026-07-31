151c9e7e2c1e4eca4b525b66df9228d29df2670b
Thierry Martinez <thierry.martinez@inria.fr>
2026-07-31 15:45:35 +0200

    Fix `graphix-stim-backend` reverse dependency

--- noxfile.py
+++ noxfile.py
@@ -96,7 +96,7 @@ class ReverseDependency:
     "package",
     [
         ReverseDependency("https://github.com/thierry-martinez/graphix-symbolic", branch="in-place_methods"),
-        ReverseDependency("https://github.com/matulni/graphix-stim-backend", branch="rename_methods"),
+        ReverseDependency("https://github.com/thierry-martinez/graphix-stim-backend", branch="rename-simulate"),
         ReverseDependency("https://github.com/TeamGraphix/graphix-qasm-parser"),
         ReverseDependency(
             "https://github.com/thierry-martinez/graphix-ibmq", doctest_modules=False, branch="rename-simulate"
