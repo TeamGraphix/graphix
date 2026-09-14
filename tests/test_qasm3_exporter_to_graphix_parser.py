39f6e5bd1507c8b001e899654917faa6ad9ef1e7
thierry-martinez <thierry.martinez@inria.fr>
2026-09-14 12:49:02 +0200

    Add all standard OpenQASM gates (#592)
    This commit adds the following gates specified in the OpenQASM 3.0
    specification: SDG, T, TDG, SX, SXDG, CY, P, U, CP, CRX, CRY, CRZ,
    CU, CSWAP, and GPHASE.
    
    Support for GPHASE is currently limited: the gate is ignored by the
    circuit simulator and the transpiler. It is useful, however, to have
    it internally in order to add control to the decompositions of the
    other gates.
    
    Additionally, this commit adds the CJ gate, which is useful internally
    for adding control to decompositions that involve J gates.
    
    These additions were motivated by the issue
    TeamGraphix/graphix-qasm-parser#14. The OpenQASM parser plugin can now
    parse all the gates and map them to native Graphix gates without
    translation, except for `u1`, `u2`, and `u3`, which are maintained for
    compatibility with OpenQASM 2.0 and are translated.
    
    ---------
    
    Co-authored-by: matulni <m.uldemolins@gmail.com>
    Co-authored-by: Pranav Nair <61292693+pranav97nair@users.noreply.github.com>

--- tests/test_qasm3_exporter_to_graphix_parser.py
+++ tests/test_qasm3_exporter_to_graphix_parser.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+import dataclasses
+import math
 from typing import TYPE_CHECKING
 
 import pytest
@@ -12,11 +14,18 @@ from graphix.fundamentals import ANGLE_PI, Axis
 from graphix.instruction import InstructionKind
 from graphix.qasm3_exporter import circuit_to_qasm3
 from graphix.random_objects import rand_circuit
-from tests.test_instruction import ALL_INSTRUCTIONS
+from tests.test_instruction import INSTRUCTION_TEST_CASES
 
-if TYPE_CHECKING:
-    from graphix.instruction import InstructionType
-
+# `graphix-qasm-parser` depends on the `graphix` package, so we cannot have
+# `graphix-qasm-parser` as a dependency of `graphix`, as this would create
+# a dependency loop, which is disallowed by PyPI.
+# Instead, `graphix-qasm-parser` is made optional and installed separately
+# in `noxfile.py` (for `tests_all` and as a reverse dependency) and in the
+# `cov` pipeline.
+# The version of `graphix-qasm-parser` to use is defined in two places:
+# - `nox`'s `tests_all` session and `cov` pipeline rely on the requirement
+#   defined in `.github/qasm-parser-requirements.txt`;
+# - `nox`'s reverse dependency check is declared in `noxfile.py` itself.
 try:
     from graphix_qasm_parser import OpenQASMParser  # type: ignore[import-not-found, unused-ignore]
 except ImportError:
@@ -30,13 +39,22 @@ except ImportError:
         # tests are skipped in this case.
         sys.exit(1)
 
+if TYPE_CHECKING:
+    from tests.test_instruction import InstructionTestCase
+
 
 def check_round_trip(circuit: Circuit) -> None:
     qasm = circuit_to_qasm3(circuit)
-    check_circuit = circuit.transpile_j_to_rzh()
+    check_circuit = circuit.transpile_to_qasm_gates()
     parser = OpenQASMParser()
     parsed_circuit = parser.parse_str(qasm)
-    assert parsed_circuit.instruction == check_circuit.instruction
+    for parsed_instr, instr in zip(parsed_circuit.instruction, check_circuit.instruction, strict=True):
+        assert parsed_instr.kind == instr.kind
+        assert all(
+            math.isclose(x, y) if isinstance(x, float) and isinstance(y, float) else x == y
+            for field in dataclasses.fields(parsed_instr)
+            for x, y in [(getattr(parsed_instr, field.name), getattr(instr, field.name))]
+        )
 
 
 @pytest.mark.parametrize("jumps", range(1, 11))
@@ -48,25 +66,51 @@ def test_circuit_to_qasm3(fx_bg: PCG64, jumps: int) -> None:
     check_round_trip(rand_circuit(nqubits, depth, rng, use_j=True, use_cz=True))
 
 
-@pytest.mark.parametrize("instruction", ALL_INSTRUCTIONS)
-def test_instruction_to_qasm3(instruction: InstructionType) -> None:
-    if instruction.kind in {InstructionKind.RZZ, InstructionKind.M}:
+@pytest.mark.parametrize("test_case", INSTRUCTION_TEST_CASES)
+def test_instruction_to_qasm3(fx_rng: Generator, test_case: InstructionTestCase) -> None:
+    instruction = test_case.instruction(fx_rng)
+    if instruction.kind in {InstructionKind.CJ, InstructionKind.RZZ, InstructionKind.M}:
         pytest.skip()
     check_round_trip(Circuit(3, instr=[instruction]))
 
 
 def test_j_to_qasm3() -> None:
-    circuit = Circuit(3, instr=[Instruction.J(target=0, angle=ANGLE_PI / 4)])
+    circuit = Circuit(1, instr=[Instruction.J(target=0, angle=ANGLE_PI / 4)])
     qasm = circuit_to_qasm3(circuit)
     parser = OpenQASMParser()
     parsed_circuit = parser.parse_str(qasm)
-    assert parsed_circuit.instruction == circuit.transpile_j_to_rzh().instruction
+    assert parsed_circuit.instruction == circuit.transpile_to_qasm_gates().instruction
 
 
 def test_j_to_qasm3_failure() -> None:
     circuit = Circuit(3, instr=[Instruction.J(target=0, angle=ANGLE_PI / 4)])
     with pytest.raises(ValueError):
         circuit_to_qasm3(circuit, transpile=False)
+
+
+def test_cj_to_qasm3() -> None:
+    circuit = Circuit(2, instr=[Instruction.CJ(control=0, target=1, angle=ANGLE_PI / 4)])
+    qasm = circuit_to_qasm3(circuit)
+    parser = OpenQASMParser()
+    parsed_circuit = parser.parse_str(qasm)
+    assert parsed_circuit.instruction == circuit.transpile_to_qasm_gates().instruction
+
+
+def test_rzz_to_qasm3() -> None:
+    circuit = Circuit(2, instr=[Instruction.RZZ(control=0, target=1, angle=ANGLE_PI / 4)])
+    qasm = circuit_to_qasm3(circuit)
+    parser = OpenQASMParser()
+    parsed_circuit = parser.parse_str(qasm)
+    assert parsed_circuit.instruction == circuit.transpile_to_qasm_gates().instruction
+
+
+def test_gphase_to_qasm3() -> None:
+    instr = Instruction.GPHASE(ANGLE_PI / 4)
+    circuit = Circuit(1, instr=[instr])
+    qasm = circuit_to_qasm3(circuit)
+    parser = OpenQASMParser()
+    parsed_circuit = parser.parse_str(qasm)
+    assert parsed_circuit.instruction == [instr]
 
 
 def test_measurement() -> None:
